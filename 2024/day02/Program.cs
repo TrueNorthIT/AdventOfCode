39200feb8b31c2aba2540afcdedec76b7e7aa7be
Christian <cwaters@truenorthit.co.uk>
2024-12-02 21:34:48 +0000

    smaller

--- 2024/day02/Program.cs
+++ 2024/day02/Program.cs
@@ -1,39 +1,15 @@
-﻿namespace Day02
+﻿var reports = File.ReadAllLines("input.txt").Select(line => line.Split(" ").Select(int.Parse).ToArray()).ToArray();
+
+Console.WriteLine($"Part1: {reports.Count(isSafe)}");
+Console.WriteLine($"Part2: {reports.Count(report => Enumerable.Range(0, report.Length).Select(i => report[0..i].Concat(report[(i + 1)..]).ToArray()).Any(isSafe))}");
+
+bool isSafe(int[] report)
 {
-    internal class Program
+    int sign = Math.Sign(report[1] - report[0]);
+    for (int i = 1; i < report.Length; i++)
     {
-        static void Main(string[] args)
-        {
-            var reports = File.ReadAllLines("input.txt").Select(line => line.Split(" ").Select(int.Parse).ToArray()).ToArray();
-
-            Console.WriteLine($"Part1: {reports.Count(isSafe)}");
-
-            Console.WriteLine($"Part2: {reports.Count(isSafe2)}");
-        }
-
-        static bool isSafe2(int[] report)
-        {
-            for (int i = 0; i < report.Length; i++)
-            {
-                var r2 = report.ToList();
-                r2.RemoveAt(i);
-                if (isSafe(r2.ToArray()) == true)
-                    return true;
-            }
-
-            return false;
-        }
-
-        static bool isSafe(int[] report)
-        {
-            int sign = Math.Sign(report[1] - report[0]);
-            for (int i = 1; i < report.Length; i++)
-            {
-                int diff = Math.Abs(report[i] - report[i - 1]);
-                if (diff < 1 || diff > 3) return false;
-                if (Math.Sign(report[i] - report[i - 1]) != sign) return false;
-            }
-            return true;
-        }
+        int diff = Math.Abs(report[i] - report[i - 1]);
+        if (diff < 1 || diff > 3 || Math.Sign(report[i] - report[i - 1]) != sign) return false;
     }
+    return true;
 }
