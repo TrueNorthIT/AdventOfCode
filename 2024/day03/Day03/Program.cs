62896e7dde7afb61648f018cda0b54a609fe302e
Christian Waters <cwaters@truenorthit.co.uk>
2024-12-03 15:50:13 +0000

    .

--- 2024/day03/Day03/Program.cs
+++ 2024/day03/Day03/Program.cs
@@ -4,9 +4,9 @@ Console.WriteLine($"Part 1: {part2(line, "don't()")}");
 
 int part1(string segment) => new System.Text.RegularExpressions.Regex(@"(mul\((\d{1,3}),(\d{1,3})\))").Matches(segment).Sum(match => int.Parse(match.Groups[2].Value) * int.Parse(match.Groups[3].Value));
 
-int part2(string segment, string token) => segment.IndexOf(token) switch
+int part2(string segment, string token) => (segment.IndexOf(token), token == "don't()") switch
 {
-    _ when segment is null => 0,
-    -1 => token == "don't()" ? part1(segment) : 0,
-    var index => (token == "don't()" ? part1(segment[0..(index - 1 + token.Length)]) : 0) + part2(segment[index..], token == "do()" ? "don't()" : "do()")
+    (_ ,_) when segment is null => 0,
+    (-1, var enabled) => enabled ? part1(segment) : 0,
+    (var index, var enabled) => (enabled ? part1(segment[0..index]) : 0) + part2(segment[index..], enabled ? "do()" : "don't()")
 };
