55675eff0dd88d340733a19b0ca9fd275870f43d
Christian Waters <cwaters@truenorthit.co.uk>
2024-12-03 13:08:36 +0000

    golfing

--- 2024/day03/Day03/Program.cs
+++ 2024/day03/Day03/Program.cs
@@ -1,24 +1,12 @@
 ﻿var line = File.ReadAllText("input.txt");
 Console.WriteLine($"Part 1: {part1(line)}");
-
-(bool enabled, int total, string remainder) = (true, 0, line); 
-while (remainder != null)
-{
-    (remainder, int score, enabled) = processSegment(remainder, enabled);
-    total += score;
-}
-
-Console.WriteLine($"Part 2: {total}");
+Console.WriteLine($"Part 1: {part2(line, "don't()")}");
 
 int part1(string segment) => new System.Text.RegularExpressions.Regex(@"(mul\((\d{1,3}),(\d{1,3})\))").Matches(segment).Sum(match => int.Parse(match.Groups[2].Value) * int.Parse(match.Groups[3].Value));
 
-(string remainder, int score, bool enabled) processSegment(string segment, bool enabled)
+int part2(string segment, string token) => segment.IndexOf(token) switch
 {
-    string token = enabled ? "don't()" : "do()";
-    var indexOf = segment.IndexOf(token);
-    if (indexOf == -1)
-    {
-        return (null, enabled ? part1(segment) : 0, !enabled);
-    }
-    return (segment[indexOf..], enabled ? part1(segment[0..(indexOf - 1 + token.Length)]) : 0, !enabled);
-}
+    _ when segment is null => 0,
+    -1 => token == "don't()" ? part1(segment) : 0,
+    var index => (token == "don't()" ? part1(segment[0..(index - 1 + token.Length)]) : 0) + part2(segment[index..], token == "do()" ? "don't()" : "do()")
+};
