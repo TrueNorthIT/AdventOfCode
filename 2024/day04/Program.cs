894849cf237a5604f9f0fc777d9cef06a6bc9d06
Christian <cwaters@truenorthit.co.uk>
2024-12-04 19:23:51 +0000

    .

--- 2024/day04/Program.cs
+++ 2024/day04/Program.cs
@@ -4,8 +4,8 @@ var grid = File.ReadAllLines("input.txt").SelectMany((line, r) => line.Select((c
         .ToDictionary(tp => new Complex(tp.r, tp.c), tp => tp.ch);
 
 Complex[][] Xs = [
-    [ new (-1,-1), new(0, 0), new(1,1) ],
-    [new (-1,1), new(0, 0), new (1,-1) ],
+    [ new (-1,-1), new (0, 0), new(1,1) ],
+    [new (-1,1), new (0, 0), new (1,-1) ],
 ];
 
 var lines = from or in new[] { -1, 0, 1 }
@@ -13,7 +13,9 @@ var lines = from or in new[] { -1, 0, 1 }
             where !(or == 0 && oc == 0)
             select (from i in Enumerable.Range(0, 4) select new Complex(or * i, oc * i)).ToArray();
 
-var part1 = grid.Select(kvp => lines.Count(line => Enumerable.Range(0, 4).All(i => "XMAS"[i] == grid.GetValueOrDefault(kvp.Key + line[i])))).Sum();
+var part1 = grid.SelectMany(kvp => 
+    lines.Where(line => Enumerable.Range(0, 4).All(i => "XMAS"[i] == grid.GetValueOrDefault(kvp.Key + line[i]))))
+.Count();
 
 var part2 = grid.Where(kvp => 
                     (   from arr in Xs
