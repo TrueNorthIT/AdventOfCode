e91de6e3b074bbaa823c901585d733c1cadaeaf4
Joe Pitts <jpitts@truenorthit.co.uk>
2024-12-04 11:01:45 +0000

    Tidy day 4

--- 2024/day04/README.md
+++ 2024/day04/README.md
@@ -1,1 +1,92 @@
 # Day 04
+
+## Solutions:
+
+### Part 1:
+
+#### Solution
+
+```python
+       def parse_input(self):
+        data = {
+            (x, y): char
+            for y, row in enumerate(self.input_data.splitlines())
+            for x, char in enumerate(row.strip())
+        }
+
+        reverse_data = collections.defaultdict(list)
+        for pos, char in data.items():
+            reverse_data[char].append(pos)
+
+        return data, reverse_data
+
+    def search_for_xmas(self, data, search_pos):
+        m_neighbours = [
+            (search_pos[0] + x, search_pos[1] + y)
+            for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+            if (search_pos[0] + x, search_pos[1] + y) in data["M"]
+        ]
+
+        found = []
+        for m_pos in m_neighbours:
+            # Let's get the direction we are going in, we want to stick to the same direction for the next two steps
+            delta = [(m_pos[0] - search_pos[0], m_pos[1] - search_pos[1])]
+            potential_a_pos_neighbour = (m_pos[0] + delta[0][0], m_pos[1] + delta[0][1])
+            if potential_a_pos_neighbour in data["A"]:
+                potential_s_pos_neighbour = (potential_a_pos_neighbour[0] + delta[0][0], potential_a_pos_neighbour[1] + delta[0][1])
+                if potential_s_pos_neighbour in data["S"]:
+                    found.append((search_pos, m_pos, potential_a_pos_neighbour, potential_s_pos_neighbour))
+
+        return len(found)
+    
+    def part1(self):
+        _, reverse_data = self.parse_input()
+        return sum(self.search_for_xmas(reverse_data, x_pos) for x_pos in reverse_data["X"])
+```
+
+I parse my input in two ways, first just a grid but then a dict of "X": [(x, y)...], "M": [(x,y)...] etc
+I then for every X pos run search_for_xmas which finds all neighbouring M's then checks for A and S
+
+ 
+### Part 2:
+
+#### Solution
+```python
+    def search_for_x_mas_from_middle(self, data, search_pos):
+
+        top_left = (search_pos[0]-1, search_pos[1]-1)
+        top_right = (search_pos[0]+1, search_pos[1]-1)
+        middle = (search_pos[0], search_pos[1])
+        bottom_left = (search_pos[0]-1, search_pos[1] + 1)
+        bottom_right = (search_pos[0]+1, search_pos[1] + 1)
+
+        try:
+            return f"{data[(top_left)]}.{data[(top_right)]}.{data[(middle)]}.{data[(bottom_left)]}.{data[(bottom_right)]}" in [
+                "M.S.A.M.S",
+                "M.M.A.S.S",
+                "S.S.A.M.M",
+                "S.M.A.S.M"
+            ]          
+        except KeyError:
+            return False
+
+
+    def part2(self):
+
+        data, reverse_data = self.parse_input()
+        # Thank's Alex for the searching from the middle idea! I originally searched from the top left corner which could either be a M or S so i had to run the search twice
+        return [self.search_for_x_mas_from_middle(data, m_pos) for m_pos in reverse_data["A"]].count(True)
+```
+For all the A's check if the four corners make a valid X of MAS! Thank's Alex for the middle search idea, i was searching from top left before which led me to search twice, once for M and once for S
+
+##### GOTCHYAS! 
+
+The input has newlines in it, since my disregard regex looks for the end of line ($) this was returrning many false positives. Took me ages to figure it out but the `.replace("\n", "")` fixed it
+
+
+## Benchmark 100 Iterations
+
+| Part   | Avg Time  | Median    | 95th %ile | 99th %ile | Unit |
+|--------|-----------|-----------|-----------|-----------|------|
+| Part 1 | 2.716922  | 2.258717  | 6.530397  | 7.802292  | s    |
+| Part 2 | 6.679797  | 6.608963  | 7.748628  | 10.149164 | ms   |
--- 2024/day04/solve.py
+++ 2024/day04/solve.py
@@ -1,82 +1,8 @@
 import collections
-import re
 import sys; from pathlib import Path; sys.path.append(str(Path(__file__).resolve().parent.parent)); from JoesAoCSolver import JoesAoCSolver
-from JoesAOCHelpers import read_input_as_grid
 class Day04Solver(JoesAoCSolver):
 
-
-    def search_for_xmas(self, data, search_pos):
-        search_pos_neighbours = [
-            (search_pos[0] + x, search_pos[1] + y)
-            for x, y in
-            [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
-        ]
-
-        m_neighbours = [pos for pos in search_pos_neighbours if pos in data["M"]]
-        found = []
-        for m_pos in m_neighbours:
-            delta = [(m_pos[0] - search_pos[0], m_pos[1] - search_pos[1])]
-            potential_a_pos_neighbour = (m_pos[0] + delta[0][0], m_pos[1] + delta[0][1])
-            if potential_a_pos_neighbour in data["A"]:
-                potential_s_pos_neighbour = (potential_a_pos_neighbour[0] + delta[0][0], potential_a_pos_neighbour[1] + delta[0][1])
-                if potential_s_pos_neighbour in data["S"]:
-                    found.append((search_pos, m_pos, potential_a_pos_neighbour, potential_s_pos_neighbour))
-
-        return found
-    
-    def pprint(self, grid):
-        for row in grid:
-            print(''.join(row))
-    
-    def search_for_x_mas_from_top_left(self, data, search_pos):
-
-        def pprint():
-            print(data[(top_left)], sep="", end="")
-            print(".", sep="", end="")
-            print(data[(top_right)])
-            print(".", sep="", end="")
-            print(data[(middle)], sep="", end="")
-            print(".")
-            print(data[(bottom_left)], sep="", end="")
-            print(".", sep="", end="")
-            print(data[(bottom_right)])                        
-
-        def to_string():
-            return f"{data[(top_left)]}.{data[(top_right)]}.{data[(middle)]}.{data[(bottom_left)]}.{data[(bottom_right)]}"
-
-        # We are always going to asume we are starting from the top left corner
-
-        top_left = search_pos
-        top_right = (search_pos[0]+2, search_pos[1])
-        middle = (search_pos[0]+1, search_pos[1] + 1)
-        bottom_left = (search_pos[0], search_pos[1] + 2)
-        bottom_right = (search_pos[0] + 2, search_pos[1] + 2)
-
-        try:
-            str_repr = to_string()
-        except KeyError:
-            return False
-
- 
-        valid_strs = [
-            "M.S.A.M.S",
-            "M.M.A.S.S",
-            "S.S.A.M.M",
-            "S.M.A.S.M"
-        ]        
-
-        if str_repr in valid_strs:
- 
-            return True
-
-
-
-
-        
-
-
-    def part1(self):
-
+    def parse_input(self):
         data = {
             (x, y): char
             for y, row in enumerate(self.input_data.splitlines())
@@ -87,17 +13,30 @@ class Day04Solver(JoesAoCSolver):
         for pos, char in data.items():
             reverse_data[char].append(pos)
 
+        return data, reverse_data
 
-        xmas_pos = []
-        for x_pos in reverse_data["X"]:
-            xmas = self.search_for_xmas(reverse_data, x_pos)
-            if xmas:
-                xmas_pos.extend(xmas)
-        print(f"Found {len(xmas_pos)} XMAS positions")
+    def search_for_xmas(self, data, search_pos):
+        m_neighbours = [
+            (search_pos[0] + x, search_pos[1] + y)
+            for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+            if (search_pos[0] + x, search_pos[1] + y) in data["M"]
+        ]
 
+        found = []
+        for m_pos in m_neighbours:
+            # Let's get the direction we are going in, we want to stick to the same direction for the next two steps
+            delta = [(m_pos[0] - search_pos[0], m_pos[1] - search_pos[1])]
+            potential_a_pos_neighbour = (m_pos[0] + delta[0][0], m_pos[1] + delta[0][1])
+            if potential_a_pos_neighbour in data["A"]:
+                potential_s_pos_neighbour = (potential_a_pos_neighbour[0] + delta[0][0], potential_a_pos_neighbour[1] + delta[0][1])
+                if potential_s_pos_neighbour in data["S"]:
+                    found.append((search_pos, m_pos, potential_a_pos_neighbour, potential_s_pos_neighbour))
 
-        return len(xmas_pos)
-
+        return len(found)
+    
+    def part1(self):
+        _, reverse_data = self.parse_input()
+        return sum(self.search_for_xmas(reverse_data, x_pos) for x_pos in reverse_data["X"])    
 
 
     def part1_examples(self):
@@ -113,57 +52,31 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX
 """, 18)]
-    
+
+    def search_for_x_mas_from_middle(self, data, search_pos):
+
+        top_left = (search_pos[0]-1, search_pos[1]-1)
+        top_right = (search_pos[0]+1, search_pos[1]-1)
+        middle = (search_pos[0], search_pos[1])
+        bottom_left = (search_pos[0]-1, search_pos[1] + 1)
+        bottom_right = (search_pos[0]+1, search_pos[1] + 1)
+
+        try:
+            return f"{data[(top_left)]}.{data[(top_right)]}.{data[(middle)]}.{data[(bottom_left)]}.{data[(bottom_right)]}" in [
+                "M.S.A.M.S",
+                "M.M.A.S.S",
+                "S.S.A.M.M",
+                "S.M.A.S.M"
+            ]          
+        except KeyError:
+            return False
+
+
     def part2(self):
 
-        data = {
-            (x, y): char
-            for y, row in enumerate(self.input_data.splitlines())
-            for x, char in enumerate(row.strip())
-        }
-
-
-        reverse_data = collections.defaultdict(list)
-        for pos, char in data.items():
-            reverse_data[char].append(pos)
-
-
-        x_mas_pos = []
-
-        for m_pos in reverse_data["M"]:
-            x_mas = self.search_for_x_mas_from_top_left(data, m_pos)
-            if x_mas:                
-                x_mas_pos.append(x_mas)
-            
-        print("Finished searching for M's")
-        
-        for s_pos in reverse_data["S"]:
-            x_mas = self.search_for_x_mas_from_top_left(data, s_pos)
-            if x_mas:                
-                x_mas_pos.append(x_mas)
-
-        print("Finished searching for S's")    
-
-
-        # # Let's find the bounds of the data 
-        # x_min = min(x for x, y in data.keys())
-        # x_max = max(x for x, y in data.keys())
-        # y_min = min(y for x, y in data.keys())
-        # y_max = max(y for x, y in data.keys())
-
-        # print(f"Found {len(x_mas_pos)} X-MAS positions, {x_mas_pos}")
-
-
-        # # let's replace all of the NON found positions with a . 
-        # for y in range(y_min, y_max + 1):
-        #     for x in range(x_min, x_max + 1):
-        #         if (x, y) not in x_mas_pos:
-        #             data[(x, y)] = "."
-
-        # for y in range(y_min, y_max + 1):
-        #     print(''.join(data[(x, y)] for x in range(x_min, x_max + 1)))
-
-        return len(x_mas_pos)
+        data, reverse_data = self.parse_input()
+        # Thank's Alex for the searching from the middle idea! I originally searched from the top left corner which could either be a M or S so i had to run the search twice
+        return [self.search_for_x_mas_from_middle(data, m_pos) for m_pos in reverse_data["A"]].count(True)
 
 
     def part2_examples(self):
@@ -183,6 +96,6 @@ MXMXAXMASX
 if __name__ == "__main__":
     solver = Day04Solver()
     # solver.run("assertions")
-    solver.run("real")
-    # solver.benchmark(1000)
+    # solver.run("real")
+    solver.benchmark(100)
     
