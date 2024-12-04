0024f949b509c77326be4963898be0e92bdc3ff8
Christian <cwaters@truenorthit.co.uk>
2024-12-04 13:27:23 +0000

    now with less expressions

--- 2024/day04/Program.cs
+++ 2024/day04/Program.cs
@@ -1,52 +1,21 @@
-﻿    var grid = File.ReadAllLines("input.txt").SelectMany((line, r) => line.Select((ch, c) => (r, c, ch)))
+﻿var grid = File.ReadAllLines("input.txt").SelectMany((line, r) => line.Select((ch, c) => (r, c, ch)))
         .ToDictionary(tp => (tp.r, tp.c), tp => tp.ch);
 
-    var R = grid.Max(tp => tp.Key.r);
-    var C = grid.Max(tp => tp.Key.c);
-
-    (int r, int c)[][] validLines =
-        (   from or in new[] { -1, 0, 1 }
-            from oc in new[] { -1, 0, 1 }
-            where !(or == 0 && oc == 0)
-            select (from i in Enumerable.Range(0, 4) 
-                    select (or * i, oc * i))
-                    .ToArray()
-        ).ToArray();
-
-    var Xs = new (int r, int c)[][] {
-        new [] { (-1,-1),(1,1) },
-        new [] { (-1,1),(1,-1) },
-    };
-    (int part1, int part2) = (0, 0);
-    for (int r = 0; r <= R; r++)
-    {
-        for (int c = 0; c <= C; c++)
-        {
-            if (grid.ContainsKey((r, c)) && grid[(r, c)] == 'A')
-            {
-                if (Xs.Select(arr => new String(
-                        arr.Select(tp => (tp.r + r, tp.c + c)).Select(tp => grid.ContainsKey(tp) ? grid[tp] : '.').ToArray()))
-                    .All(str => str == "MS" || str == "SM"))
-                {
-                    part2++;
-                }
-            }
-            foreach (var line in validLines)
-            {
-                bool allCorrect = true;
-                for (int i = 0; i < line.Length; i++)
-                {
-                    var key = (r + line[i].r, c + line[i].c);
-                    if (!grid.ContainsKey(key) || grid[key] != "XMAS"[i])
-                    {
-                        allCorrect = false;
-                        break;
-                    }
-                }
-                if (allCorrect) part1++;
-            }
-        }
-    }
-
-    Console.WriteLine($"Part 1: {part1}");
-    Console.WriteLine($"Part 2: {part2}");
+Console.WriteLine(grid.Keys.Select( key => (grid.TryGetValue((key.r, key.c), out char first), first) switch
+{
+    (true, 'A') => (0,  new (int or, int oc)[][] {
+                            new [] { (-1,-1),(1,1) },
+                            new [] { (-1,1),(1,-1) },
+                        }.Select(arr => new String(arr.Select(tp => (tp.or + key.r, tp.oc + key.c))
+                                                    .Select(tp => grid.ContainsKey(tp) ? grid[tp] : '.').ToArray()))
+                        .Count(str => str == "MS" || str == "SM")),
+    (true, 'X') => ((   from or in new[] { -1, 0, 1 }
+                        from oc in new[] { -1, 0, 1 }
+                        where !(or == 0 && oc == 0)
+                        select (from i in Enumerable.Range(0, 4)
+                                select (or * i, oc * i))
+                                .ToArray<(int r, int c)>()
+                        ).Count(line => Enumerable.Range(0, 4).Select(i => (i, (key.r + line[i].r, key.c + line[i].c)))
+                            .All(tp => grid.ContainsKey(tp.Item2) && grid[tp.Item2] == "XMAS"[tp.Item1])), 0),
+    (_, _) => (0, 0),
+}).Aggregate((0,0), (acc, tp) => (acc.Item1 + tp.Item1, acc.Item2 + tp.Item2)));
