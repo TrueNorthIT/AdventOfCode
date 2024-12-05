7c775404362885bfad69ec4fcf10464179ec3d94
Christian Waters <cwaters@truenorthit.co.uk>
2024-12-05 12:14:21 +0000

    tidied up

--- 2024/day05/Program.cs
+++ 2024/day05/Program.cs
@@ -1,39 +1,18 @@
-﻿namespace day05
+﻿var file = File.ReadAllText("input.txt").Split($"{Environment.NewLine}{Environment.NewLine}");
+
+var rules = file[0].Split(Environment.NewLine).Select(rule => { var sp = rule.Split("|"); return (int.Parse(sp[0]), int.Parse(sp[1])); }).ToHashSet();
+var updates = file[1].Split(Environment.NewLine).Select(update => update.Split(",").Select(int.Parse).ToArray()).ToArray();
+
+(int part1, int part2) tp = updates.Zip(updates.Select(arr => arr.OrderBy(i => i, new PageComparer(rules)).ToArray()))
+            .Aggregate((0, 0),
+                (acc, tp) => tp.First.SequenceEqual(tp.Second)
+                ? (acc.Item1 + tp.First[tp.First.Length / 2], acc.Item2)
+                : (acc.Item1, acc.Item2 + tp.Second[tp.Second.Length / 2]));
+
+Console.WriteLine($"Answers: {tp}");
+
+public class PageComparer(HashSet<(int, int)> rules) : IComparer<int>
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var file = File.ReadAllText("input.txt").Split($"{Environment.NewLine}{Environment.NewLine}");
-            var rules = file[0].Split(Environment.NewLine).Select(rule => { var sp = rule.Split("|"); return (int.Parse(sp[0]), int.Parse(sp[1])); }).ToArray();
-            var updates = file[1].Split(Environment.NewLine).Select(update => update.Split(",").Select(int.Parse).ToArray()).ToArray();
-
-            var nodes = updates.SelectMany(arr => arr).Distinct();
-            Console.WriteLine($"Distinct nodes : {nodes}");
-
-            int part1 = 0; int part2 = 0;
-            foreach (var update in updates)
-            {
-                bool failed = true;
-            runagain:;
-                for (int i = 1; i < update.Length; i++)
-                {
-                    //is there a rule that causes update[i] to have to be before update[i-1]
-                    if (rules.Any(tp => tp.Item1 == update[i] && tp.Item2 == update[i - 1]))
-                    {
-                        failed = false;
-                        (update[i - 1], update[i]) = (update[i], update[i-1]);
-                        goto runagain;
-                    }
-                }
-                if (failed)
-                    part1 += update[update.Length / 2];
-                else
-                    part2 += update[update.Length / 2];
-            }
-            Console.WriteLine($"Part 1: {part1}");
-            Console.WriteLine($"Part 2: {part2}");
-        }
-
-    }
+    HashSet<(int, int)> Rules => rules;
+    public int Compare(int x, int y) => rules.Contains((x, y)) ? -1 : 1;
 }
