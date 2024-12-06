fc82e45effef99799e900947976510096a797443
Christian <cwaters@truenorthit.co.uk>
2024-12-06 10:10:43 +0000

    ugh pt2

--- 2024/day06/Program.cs
+++ 2024/day06/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Diagnostics;
+using System.Numerics;
 
 namespace day06
 {
@@ -7,24 +8,27 @@ namespace day06
         static Complex[] dirs = { new(-1, 0), new(0, 1), new(1, 0), new(0, -1) };
         static void Main(string[] args)
         {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
             var grid = File.ReadAllLines("input.txt")
                         .SelectMany((line, r) => line.Select((ch, c) => (r, c, ch)))
                         .ToDictionary(tp => new Complex(tp.r, tp.c), tp => tp.ch);
 
             var part1 = solve(grid).Select(tp => tp.Item1).Distinct().Count();
-            Console.WriteLine($"Part 1: {part1}");
+            Console.WriteLine($"Part 1: {part1} in {watch.ElapsedMilliseconds}ms");
+            watch.Restart();
 
             int part2 = 0;
-            foreach (var obstacle in grid.Where(kvp => kvp.Value == '.').Select(kvp => kvp.Key))
+            Parallel.ForEach(grid.Where(kvp => kvp.Value == '.').Select(kvp => kvp.Key), obstacle =>
             {
-                grid[obstacle] = '#';
-                if (solve(grid) == null)
+                var newGrid = grid.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                newGrid[obstacle] = '#';
+                if (solve(newGrid) == null)
                 {
-                    part2++;
+                    Interlocked.Increment(ref part2);
                 }
-                grid[obstacle] = '.';
-            }
-            Console.WriteLine($"Part 2: {part2}");
+            });
+            Console.WriteLine($"Part 2: {part2} in {watch.ElapsedMilliseconds}ms");
         }
 
         public static HashSet<(Complex,int)> solve(Dictionary<Complex, char> grid)
@@ -33,36 +37,33 @@ namespace day06
             var C = grid.Keys.Max(tp => tp.Imaginary);
 
             var start = grid.Where(kvp => kvp.Value == '^').Single().Key;
-            int dir = 0;
 
             var visited = new HashSet<(Complex, int)>();
-            var current = start;
-            visited.Add((current, dir));
+            (Complex position, int direction) current = (start, 0);
+            visited.Add(current);
             while (true)
             {
-                Complex next;
                 while (true)
                 {
-                    next = current + dirs[dir];
+                    Complex next = current.position + dirs[current.direction];
                     if (!grid.ContainsKey(next))
                         goto escape;
 
                     if (grid[next] == '#')
                     {
-                        dir = (dir + 1) % 4;
+                        current = (current.position, (current.direction + 1) % 4);
                     }
                     else
                     {
+                        current = (next, current.direction);
                         break;
                     }
                 }
 
-                if (visited.Contains((next, dir)))
+                if (!visited.Add(current))
                 {
                     return null;
                 }
-                visited.Add((next,dir));
-                current = next;
             }
         escape:;
             return visited;
