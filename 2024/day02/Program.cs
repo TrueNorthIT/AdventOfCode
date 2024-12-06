33a02990bdc0a1128b4a4cd4f8b64cdbd22aa0ed
Christian <cwaters@truenorthit.co.uk>
2024-12-06 17:32:54 +0000

    .

--- 2024/day02/Program.cs
+++ 2024/day02/Program.cs
@@ -1,15 +1,15 @@
-﻿var reports = File.ReadAllLines("input.txt").Select(line => line.Split(" ").Select(int.Parse).ToArray()).ToArray();
+﻿var reports = File.ReadAllLines("input.txt").Select(line => line.Split(" ").Select(int.Parse).ToArray());
 
 Console.WriteLine($"Part1: {reports.Count(isSafe)}");
-Console.WriteLine($"Part2: {reports.Count(report => Enumerable.Range(0, report.Length).Select(i => report[0..i].Concat(report[(i + 1)..]).ToArray()).Any(isSafe))}");
+Console.WriteLine($"Part2: {reports.Count(report => 
+    Enumerable.Range(0, report.Length).Select(i => 
+        report[0..i].Concat(report[(i + 1)..]).ToArray())
+    .Any(isSafe))}");
 
-bool isSafe(int[] report)
-{
-    int sign = Math.Sign(report[1] - report[0]);
-    for (int i = 1; i < report.Length; i++)
-    {
-        int diff = Math.Abs(report[i] - report[i - 1]);
-        if (diff < 1 || diff > 3 || Math.Sign(report[i] - report[i - 1]) != sign) return false;
-    }
-    return true;
-}
+bool isSafe(int[] report) => report.Skip(1).Aggregate<int, (bool success, int prev)>(
+        (true, report[0]),
+        (acc, next) =>
+            (Math.Abs(next - acc.prev) >= 1 && Math.Abs(next - acc.prev) <= 3
+            && Math.Sign(next - acc.prev) == Math.Sign(report[1] - report[0])
+            && acc.success,
+        next)).success;
