6e0aeeb1100283753dcd0dcd4193e4fd81679827
Christian <cwaters@truenorthit.co.uk>
2024-12-06 15:35:36 +0000

    it's complex

--- 2024/day06/Program.cs
+++ 2024/day06/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System.Numerics;
 
-Complex[] dirs = { new(-1, 0), new(0, 1), new(1, 0), new(0, -1) };
-
 var grid = File.ReadAllLines("input.txt")
             .SelectMany((line, r) => line.Select((ch, c) => (r, c, ch)))
             .ToDictionary(tp => new Complex(tp.r, tp.c), tp => tp.ch);
 
 var start = grid.Where(kvp => kvp.Value == '^').Single().Key;
 
-var guardsRoute = solve(new Complex(-1,-1));
+var guardsRoute = solve(null);
 Console.WriteLine($"Part 1: {guardsRoute.Count()}");
 
 var part2 = guardsRoute.Where(position => position != start)
@@ -17,18 +15,18 @@ var part2 = guardsRoute.Where(position => position != start)
 
 Console.WriteLine($"Part 2: {part2}");
 
-List<Complex> solve(Complex obstacle)
+List<Complex> solve(Complex? obstacle)
 {
-    var visited = new HashSet<(Complex position, int direction)>();
-    (Complex position, int direction) current = (start, 0);
+    var visited = new HashSet<(Complex position, Complex direction)>();
+    (Complex position, Complex direction) current = (start, new (-1,0));
     while (visited.Add(current))
     {
-        var next = current.position + dirs[current.direction];
+        var next = current.position + current.direction;
         if (!grid.ContainsKey(next))
             return visited.Select(tp => tp.position).Distinct().ToList();
 
         current = next == obstacle  || grid[next] == '#'
-                    ? (current.position, (current.direction + 1) % 4)
+                    ? (current.position, current.direction * new Complex(0,-1))
                     : (next, current.direction);
     }
     return null;
