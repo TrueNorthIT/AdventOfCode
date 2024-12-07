dfa59cce6d88a802bca64798fa7d3a43db89fadc
Christian <cwaters@truenorthit.co.uk>
2024-12-07 11:56:06 +0000

    recursion

---
+++ 2024/day07/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace day07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var equations = File.ReadAllLines("input.txt").Select(str => 
+                { var sp = str.Split(':'); return (long.Parse(sp[0]), sp[1].Trim().Split(' ').Select(long.Parse).ToList()); }).ToArray();
+
+            var part1 = 0L;
+            var part2 = 0L;
+            foreach ((long target, List<long> operands) in equations)
+            {
+                var res1 = totalWays(target, operands.First(), operands.Skip(1).ToList(), false);
+                if (res1 > 0) part1 += target;
+                var res2 = totalWays(target, operands.First(), operands.Skip(1).ToList(), true);
+                if (res2 > 0) part2 += target;
+            }
+        }
+        static int totalWays(long target, long currentTotal, List<long> numbers, bool part2)
+        {
+            if (!numbers.Any())
+            {
+                return target == currentTotal ? 1 : 0;
+            }
+            return totalWays(target, currentTotal * numbers.First(), numbers.Skip(1).ToList(), part2)
+                + totalWays(target, currentTotal + numbers.First(), numbers.Skip(1).ToList(), part2)
+                + (part2 ? totalWays(target, long.Parse($"{currentTotal}{numbers.First()}"), numbers.Skip(1).ToList(), part2) : 0);
+        }
+    }
+}
