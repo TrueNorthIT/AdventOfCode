3edc83cd6bddc078ea4a708db4b113353aad2fd0
Christian <cwaters@truenorthit.co.uk>
2024-12-08 12:28:42 +0000

    tidied up

--- 2024/day08/Program.cs
+++ 2024/day08/Program.cs
@@ -8,41 +8,27 @@ int R = (int)nodes.SelectMany(kvp => kvp.Value).Max(tp => tp.Real);
 int C = (int) nodes.SelectMany(kvp => kvp.Value).Max(tp => tp.Imaginary);
 Func<Complex, bool> inGrid = tp => tp.Real >= 0 && tp.Real <= R && tp.Imaginary >= 0 && tp.Imaginary <= C;
 
-nodes.Remove('.');
-var antiNodes = new HashSet<Complex>();
-var antiNodes2 = new HashSet<Complex>();
-foreach (var node in nodes)
+(var part1, var part2) = (new HashSet<Complex>(), new HashSet<Complex>());
+foreach (var (node,pair) in from n in nodes
+                            where n.Key != '.'
+                            from n1 in n.Value
+                            from n2 in n.Value
+                            where n1 != n2
+                            select (n, (n1, n2)))
 {
-    foreach (var pair in from n1 in node.Value
-                         from n2 in node.Value
-                         where n1 != n2
-                         select (n1, n2))
-    {
-        var delta = pair.n2 - pair.n1;
-        var solutions = new[] { pair.n1 + 2 * delta, pair.n2 - 2 * delta };
-        foreach (var valid in solutions.Where(inGrid))
-        {
-            antiNodes.Add(valid);
-        }
+    //we don't consider points between the antenna, luckily we don't have to!
+    var delta = pair.n2 - pair.n1;
+    var solutions = new[] { pair.n1 + 2 * delta, pair.n2 - 2 * delta };
+    foreach (var valid in solutions.Where(inGrid))
+        part1.Add(valid);
 
-        //now find GCD delta
-        var gcd = BigInteger.GreatestCommonDivisor((BigInteger)delta.Real, (BigInteger)delta.Imaginary);
-        var gcdDelta = new Complex((int) ((int) delta.Real / gcd), (int) ((int) delta.Imaginary / gcd));
-        Action<bool> process = direction =>
-        {
-            foreach (var i in Enumerable.Range(0, int.MaxValue))
-            {
-                var solution = pair.n1 + (direction ? 1 : -1) * i * gcdDelta;
-                if (inGrid(solution))
-                    antiNodes2.Add(solution);
-                else
-                    break;
-            }
-        };
-        process(false);
-        process(true);
-    }
-
+    //don't have to reduce by the gcd, data is kind to us again!
+    foreach (var dir in new[] { -1, 1 })
+        foreach (var solution in Enumerable.Range(0, int.MaxValue).Select(i => pair.n1 + dir * i * delta))
+            if (inGrid(solution))
+                part2.Add(solution);
+            else
+                break;
 }
-Console.WriteLine($"Part 1: {antiNodes.Count}");
-Console.WriteLine($"Part 2: {antiNodes2.Count}");
+Console.WriteLine($"Part 1: {part1.Count}");
+Console.WriteLine($"Part 2: {part2.Count}");
