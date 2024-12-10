ee68c2eec044099ad03dad4653b4d833666ab19e
Christian Waters <cwaters@truenorthit.co.uk>
2024-12-10 15:04:19 +0000

    final final

--- 2024/day10/Program.cs
+++ 2024/day10/Program.cs
@@ -1,24 +1,21 @@
 ﻿using System.Numerics;
 
-var dirs = new Complex[] { new(1, 0), new(0, -1), new(-1, 0), new(0, 1) };
-
 var grid = File.ReadAllLines("input.txt")
             .SelectMany((line, r) => line.Select((ch, c) => (r, c, ch)))
             .ToDictionary(tp => new Complex(tp.r, tp.c), tp => tp.ch);
 
 var gridMap = grid.GroupBy(kvp => kvp.Value).ToDictionary(grp => grp.Key, grp => grp.Select(kvp => kvp.Key).ToList());
 
-var trailHeadsCanReach = gridMap['0'].ToDictionary(key => key, key => new HashSet<Complex>([key]));
-
-var preceding = (char h, Complex square) => 
+var dirs = new Complex[] { new(1, 0), new(0, -1), new(-1, 0), new(0, 1) };
+var preceding = (char h, Complex square) =>
     dirs.Select(dir => square + dir)
     .Where(n => grid.ContainsKey(n) && (h - grid[n]) == 1);
 
+var trailHeadsCanReach = gridMap['0'].ToDictionary(key => key, key => new HashSet<Complex>([key]));
 solvedp(trailHeadsCanReach, (h, square) => preceding(h, square).SelectMany(neighbour => trailHeadsCanReach[neighbour]).ToHashSet());
 Console.WriteLine($"Part 1: {gridMap['9'].Sum(square => trailHeadsCanReach[square].Count())}");
 
 var pathCounts = gridMap['0'].ToDictionary(key => key, _ => 1);
-
 solvedp(pathCounts, (h, square) => preceding(h, square).Sum(neighbour => pathCounts[neighbour]));
 Console.WriteLine($"Part 2: {gridMap['9'].Sum(square => pathCounts[square])}");
 
