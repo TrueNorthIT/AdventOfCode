d84086683f4386f7d2f5e14119de53ddac2639e6
Christian Waters <cwaters@truenorthit.co.uk>
2024-12-10 12:04:37 +0000

    faster

--- 2024/day10/Program.cs
+++ 2024/day10/Program.cs
@@ -9,7 +9,7 @@ var dirs = new Complex[] { new (1, 0), new (0, -1), new (-1, 0), new (0, 1) };
 (int part1, int part2) = (0, 0);
 foreach (var kvp in grid.Where(kvp => kvp.Value == '0'))
 {
-    var visitedCount = new Dictionary<Complex,int>();
+    var visitedCount = new[] { kvp.Key }.ToDictionary(key => key, _ => 1);
     var queue = new Queue<Complex>();
     queue.Enqueue(kvp.Key);
     while (queue.Any())
@@ -18,10 +18,11 @@ foreach (var kvp in grid.Where(kvp => kvp.Value == '0'))
         foreach (var next in dirs.Select(dir => current + dir)
             .Where(x => grid.ContainsKey(x) && (grid[x] - grid[current] == 1)))
         {
-            if (!visitedCount.TryAdd(next, 1))
-                visitedCount[next]++;
+            if (visitedCount.TryAdd(next, visitedCount[current]))
+                queue.Enqueue(next);
+            else
+                visitedCount[next] += visitedCount[current];
 
-            queue.Enqueue(next);
         }
     }
     part1 += visitedCount.Where(kvp => grid[kvp.Key] == '9').Count();
