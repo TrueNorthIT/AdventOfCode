34217c2eb8d6b27c7a6114127db156c33c889a48
Christian Waters <cwaters@truenorthit.co.uk>
2024-12-10 11:53:08 +0000

    tidied up

--- 2024/day10/Program.cs
+++ 2024/day10/Program.cs
@@ -2,16 +2,14 @@
 
 var grid = File.ReadAllLines("input.txt")
             .SelectMany((line, r) => line.Select((ch, c) => (r, c, ch)))
-            .Where(tp => tp.ch != '.')
-            .ToDictionary(tp => new Complex(tp.r, tp.c), tp => int.Parse($"{tp.ch}"));
+            .ToDictionary(tp => new Complex(tp.r, tp.c), tp => tp.ch);
 
-var dirs = Enumerable.Repeat(new Complex(1,0), 4).Aggregate(c => c * new int(0,1));
+var dirs = new Complex[] { new (1, 0), new (0, -1), new (-1, 0), new (0, 1) };
 
-int part1 = 0;
-int part2 = 0;
-foreach (var kvp in grid.Where(kvp => kvp.Value == 0))
+(int part1, int part2) = (0, 0);
+foreach (var kvp in grid.Where(kvp => kvp.Value == '0'))
 {
-    var visited = new Dictionary<Complex,int>();
+    var visitedCount = new Dictionary<Complex,int>();
     var queue = new Queue<Complex>();
     queue.Enqueue(kvp.Key);
     while (queue.Any())
@@ -20,16 +18,15 @@ foreach (var kvp in grid.Where(kvp => kvp.Value == 0))
         foreach (var next in dirs.Select(dir => current + dir)
             .Where(x => grid.ContainsKey(x) && (grid[x] - grid[current] == 1)))
         {
-            if (visited.ContainsKey(next))
-                visited[next]++;
-            else
-                visited.Add(next, 1);
+            if (!visitedCount.TryAdd(next, 1))
+                visitedCount[next]++;
 
             queue.Enqueue(next);
         }
     }
-    part1 += visited.Where(kvp => grid[kvp.Key] == 9).Count();
-    part2 += visited.Where(kvp => grid[kvp.Key] == 9).Sum(kvp => kvp.Value);
+    part1 += visitedCount.Where(kvp => grid[kvp.Key] == '9').Count();
+    part2 += visitedCount.Where(kvp => grid[kvp.Key] == '9').Sum(kvp => kvp.Value);
 }
 
 Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
