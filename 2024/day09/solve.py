39fbb52f87bf69a76371bfcb489c1f866c5823c2
Joe Pitts <jpitts@truenorthit.co.uk>
2024-12-10 08:42:06 +0000

    Smashed it today at least

--- 2024/day09/solve.py
+++ 2024/day09/solve.py
@@ -1,116 +1,167 @@
 from collections import defaultdict
 import sys; from pathlib import Path; sys.path.append(str(Path(__file__).resolve().parent.parent)); from JoesAoCSolver import JoesAoCSolver
 
+
+class Block:
+    def __init__(self, size: int):
+        self.size = size
+        self.remaining_space = size
+        self.files = []
+
+    def has_files(self) -> bool:
+        return len(self.files) > 0
+
+    def can_fit(self, file_size: int) -> bool:
+        return file_size <= self.remaining_space
+
+    def add_file(self, file: list[int]):
+        self.files.extend(file)
+        self.remaining_space -= len(file)
+
+    def file_size(self) -> int:
+        return len(self.files)
+
+    def pop_num(self, num: int):
+        popped = self.files[:num]
+        self.files = self.files[num:]
+        self.remaining_space += num
+        return popped
+
+    def __repr__(self):
+        return "".join(map(str, self.files)) + "." * self.remaining_space
+
 class Day09Solver(JoesAoCSolver):
 
-    def parse_input(self) -> tuple[dict[int, int], dict[int, list[int]]]:
-        files = self.input_data[0::2]
-        file_dict = defaultdict(int)
-        for index, file_length in enumerate(files):
-            file_dict[index] = int(file_length)
+    def parse_input(self) -> list[Block]:
 
-        empty_space = self.input_data[1::2]
-        space_dict = {}
-        for index, space in enumerate(empty_space):
-            space_dict[index] = [int(space), []]
-        
-        
-        return file_dict, space_dict
+        hard_drive = []
+
+        for i, value in enumerate(self.input_data.rstrip()):
+            if i % 2 == 0:
+                # This is a file
+                file = Block(int(value))
+                file.add_file([i//2] * int(value))
+                hard_drive.append(file)
+            else:
+                # This is a space
+                space = Block(int(value))
+                hard_drive.append(space)
+
+
+        return hard_drive
 
     def part1(self):
-        file_dict, space_dict = self.parse_input()
-        current_free_block = 0
-        for file, file_size in sorted(file_dict.items(), reverse=True):
-            current_file_size = file_size
-            while current_file_size > 0 and current_free_block < len(space_dict):
-                if current_free_block > file:
-                    # We don't want to move data after where it currently is
-                    break
+        hard_drive = self.parse_input()
+        # Start from the rightmost block and move files left
+        space_index = 0
+        for block in hard_drive[::-1]:  # Process blocks from right to left
+            print(f"Processing block: {block}")
+            while block.has_files():
+                print(f"Processing file in block: {block.files}")
+                # Skip to the next free space if the current space is full
+                while space_index < len(hard_drive) and hard_drive[space_index].remaining_space == 0:
+                    print(f"Block {hard_drive[space_index]} is full, moving to the next block.")
+                    space_index += 1
+                
+                # If space_index exceeds the array length, no more free space is available
+                if space_index >= len(hard_drive):
+                    print("Error: No more free space available.")
+                    return
+                
+                # Move as many files as possible into the current free space
+                to_add = min(hard_drive[space_index].remaining_space, block.file_size())
+                moved_files = block.pop_num(to_add)  # Get the files being moved
+                hard_drive[space_index].add_file(moved_files)  # Move the files
+                print(f"Moved {moved_files} to block {space_index}.")
+                print(f"Updated block {space_index}: {hard_drive[space_index]}")
+                print(f"Remaining files in current block: {block}")
 
-                remaining_space = space_dict[current_free_block][0]  # Get the available space in the current block
-
-                # Determine how much space to use in this block
-                to_fill = min(current_file_size, remaining_space)
-
-                # Update the block and file size
-                space_dict[current_free_block][1].extend([file] * to_fill)
-                space_dict[current_free_block][0] -= to_fill
-                current_file_size -= to_fill
-
-                # Now let's clear that many from the file_dict
-                file_dict[file] -= to_fill
-
-                # Move to the next block if this one is full
-                if space_dict[current_free_block][0] == 0:
-                    current_free_block += 1
-
-
-        final_drive = []
-
-        for key, value in space_dict.items():
-            final_drive.extend([key] * file_dict[key])
-            final_drive.extend(value[1])
-
-        print(final_drive)#
-
-        # Final score is the sum of index * value for each file in the final drive
-        return sum(i * value for i, value in enumerate(final_drive))
-
+        print("Final state of hard drive:")
+        print("".join(map(str, hard_drive)))
 
     def part1_examples(self):
         return [("2333133121414131402", 1928)]
     
-    def build_drive(self, file_dict, space_dict):
-        final_drive = []
-        for key, value in file_dict.items():
-            final_drive.extend([chr(key)] * value)
-            if key in space_dict:
-                final_drive.extend([chr(ord("."))] * space_dict[key][0])
-        return "".join(final_drive)
+    # def build_drive(self, file_dict, space_dict):
+    #     final_drive = []
+    #     for key, value in file_dict.items():
+    #         final_drive.extend([chr(key)] * value)
+    #         if key in space_dict:
+    #             final_drive.extend([chr(ord("."))] * space_dict[key][0])
+    #             if space_dict[key][0] == 0:
+    #                 print("YEEHAW")
+    #                 final_drive.extend(chr(ord("!"))) # This is a separator
+    #     return "".join(final_drive)
     
-    def print_drive(self, drive):
-        print("".join(map(lambda x: str(ord(x)) if not x == '.' else '.', drive)))
+    # def parse_drive(self, drive):
+    #     # Need to also support the separator !
+    #     def parse(x):
+    #         if x == "!":
+    #             return "!"
+    #         if x == ".":
+    #             return "."
+    #         return str(ord(x))
+    #     return "".join(map(parse, drive))
 
-    def part2(self):            
-        def swap_substrings(s: str, val1: str, val2: str) -> str:
-            idx1, idx2 = s.find(val1), s.find(val2)
-            if idx1 == -1 or idx2 == -1: return s
-            if idx1 < idx2: return s
-            return s[:idx2] + val1 + s[idx2 + len(val2):idx1] + val2 + s[idx1 + len(val1):]
+    # def part2(self):            
+    #     def swap_substrings(s: str, file: str, space: str) -> tuple[bool,str]:
+    #         idx2 = s.find(space)
+    #         # We want the last index of the file
+    #         idx1 = s.rfind(file)
+            
+    #         if idx1 == -1 or idx2 == -1: return False, s
+    #         if idx1 < idx2: return False, s
+    #         return True, s[:idx2] + file + s[idx2 + len(space):idx1] + space + s[idx1 + len(file):]
 
-        file_dict, space_dict = self.parse_input()
-        initial_drive = self.build_drive(file_dict, space_dict)
-        drive_string = self.build_drive(file_dict, space_dict)
-        self.print_drive(drive_string)
-
-        for file_index, file_size in sorted(file_dict.items(), reverse=True):
-            file = str(chr(file_index)) * file_size
-            empty_space = "." * file_size * len(str(file_index))
-            drive_string = swap_substrings(drive_string, file,  empty_space)
-
-        self.print_drive(drive_string)
-
-        # If they are a different length, something went wrong!
-        assert len(drive_string) == len(initial_drive)
+    #     file_dict, space_dict = self.parse_input()
+    #     initial_drive = self.build_drive(file_dict, space_dict)
+    #     drive_string = self.build_drive(file_dict, space_dict)
         
-        # Final score is the sum of index * value for each file in the final drive
-        total = 0
-        for i, value in enumerate(drive_string):
-            if value != ".":
-                print(f"{i} * {ord(value)} = {i * int(ord(value))}")
-                total += i * int(ord(value))
-        return total
+        
+    #     prev_drive_string = self.parse_drive(drive_string)
+    #     print(prev_drive_string)    
+        
+    #     for file_index, file_size in sorted(file_dict.items(), reverse=True):
+    #         # print(f"Trying to move file {file_index} of size {file_size}")
+    #         file = str(chr(file_index)) * file_size
+    #         empty_space = "." * file_size * len(str(file_index))
+    #         did_swap, drive_string = swap_substrings(drive_string, file,  empty_space)
+    #         # Print the drive string and prev_drive_string with the differences highlighted in green
+    #         # if did_swap:
+    #         #     diffs = [f"\033[42m{c1}\033[0m" if c1 != c2 else c1 for c1, c2 in zip(self.parse_drive(drive_string), prev_drive_string)]
+    #         #     # Can we make an 'animation' of this,
+    #         #     # print("".join(diffs))
 
 
-    def part2_examples(self):
-        return [("243313312141413140202", 2858)]
+    #         prev_drive_string = self.parse_drive(drive_string)
+
+    #     self.parse_drive(drive_string)
+
+    #     # If they are a different length, something went wrong!
+    #     assert len(drive_string) == len(initial_drive)
+        
+    #     # Final score is the sum of index * value for each file in the final drive
+    #     total = 0
+    #     for i, value in enumerate(drive_string):
+    #         if value != "." and value != "!":
+    #             # print(f"{i} * {ord(value)} = {i * int(ord(value))}")
+    #             total += i * int(ord(value))
+    #     return total
 
 
+    # def part2_examples(self):
+    #     return [
+    #         ("101", 2858),
+    #         ("1313165", 169)
+    #     ]
 
+
+# Part 2: 6363268339304
+# Part 2: 14200825782129
 
 if __name__ == "__main__":
     solver = Day09Solver()
-    # solver.run("assertions")
-    solver.run("real")
+    solver.run("assertions")
+    # solver.run("real")
     # solver.benchmark(1000)
     
---
+++ 2024/day10/solve.py
@@ -0,0 +1,115 @@
+import collections
+import sys; from pathlib import Path; sys.path.append(str(Path(__file__).resolve().parent.parent)); from JoesAoCSolver import JoesAoCSolver
+
+
+UP = complex(0, -1)
+DOWN = complex(0, 1)
+LEFT = complex(-1, 0)
+RIGHT = complex(1, 0)
+
+class Day01Solver(JoesAoCSolver):
+
+    def parse_input(self) -> tuple[dict[complex, int], dict[int, list[complex]]]:
+        grid = {
+            complex(x, y): int(char)
+            for y, row in enumerate(self.input_data.splitlines())
+            for x, char in enumerate(row.strip())
+        }
+
+        
+        reverse_data = collections.defaultdict(list)
+        for pos, char in grid.items():
+            reverse_data[char].append(pos)
+
+        return grid, reverse_data
+
+    def count_trails(self, currentPos: complex, grid: dict[complex, int], visited: set[complex]) -> int:
+        current_value = grid[currentPos]
+
+        if currentPos in visited:
+            return 0
+
+        visited.add(currentPos)
+
+        if current_value == 9:
+            return 1
+
+        # Directions
+        directions = [UP, DOWN, LEFT, RIGHT]
+
+        # Recurse into all valid neighbors
+        trail_count = 0
+        for direction in directions:
+            neighbor = currentPos + direction
+            if grid.get(neighbor) == current_value + 1:
+                trail_count += self.count_trails(neighbor, grid, visited)
+
+        return trail_count
+    
+    def calculate_trails_ratings(self, currentPos: complex, grid: dict[complex, int]) -> int:
+        current_value = grid[currentPos]
+
+        if current_value == 9:
+            return 1
+
+        # Directions
+        directions = [UP, DOWN, LEFT, RIGHT]
+
+        # Recurse into all valid neighbors
+        trail_count = 0
+        for direction in directions:
+            neighbor = currentPos + direction
+            if grid.get(neighbor) == current_value + 1:
+                trail_count += self.calculate_trails_ratings(neighbor, grid)
+
+        return trail_count
+
+
+
+    def part1(self):
+        grid, lookup = self.parse_input()
+
+        return sum(self.count_trails(trailhead, grid, set()) for trailhead in lookup[0])
+
+    def part1_examples(self):
+        return [
+("""0123
+1234
+8765
+9876
+""", 1),
+("""89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732""", 36)]
+    
+    def part2(self):
+        grid, lookup = self.parse_input()
+
+        return sum(self.calculate_trails_ratings(trailhead, grid) for trailhead in lookup[0])
+
+    def part2_examples(self):
+        return [
+("""89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+""", 81)]
+    
+
+
+
+if __name__ == "__main__":
+    solver = Day01Solver()
+    # solver.run("assertions")
+    solver.run("real")
+    # solver.benchmark(1000)
+    
