e4b984f89dc7cc87eff26da8d5e2a84bc48a96a3
Christian Waters <cwaters@truenorthit.co.uk>
2024-12-10 16:32:30 +0000

    .

--- 2024/day10/Program.cs
+++ 2024/day10/Program.cs
@@ -4,23 +4,26 @@ var grid = File.ReadAllLines("input.txt")
             .SelectMany((line, r) => line.Select((ch, c) => (r, c, ch)))
             .ToDictionary(tp => new Complex(tp.r, tp.c), tp => tp.ch);
 
-var gridMap = grid.GroupBy(kvp => kvp.Value).ToDictionary(grp => grp.Key, grp => grp.Select(kvp => kvp.Key).ToList());
+var heightMap = grid.GroupBy(kvp => kvp.Value).ToDictionary(grp => grp.Key, 
+    grp => grp.Select(kvp => kvp.Key).ToList());
 
-var dirs = new Complex[] { new(1, 0), new(0, -1), new(-1, 0), new(0, 1) };
-var preceding = (char h, Complex square) =>
-    dirs.Select(dir => square + dir)
-    .Where(n => grid.ContainsKey(n) && (h - grid[n]) == 1);
+var dirs = new Complex[] { new (1, 0), new(0, -1), new(-1, 0), new(0, 1) };
 
-var trailHeadsCanReach = gridMap['0'].ToDictionary(key => key, key => new HashSet<Complex>([key]));
-solvedp(trailHeadsCanReach, (h, square) => preceding(h, square).SelectMany(neighbour => trailHeadsCanReach[neighbour]).ToHashSet());
-Console.WriteLine($"Part 1: {gridMap['9'].Sum(square => trailHeadsCanReach[square].Count())}");
+var canReach = grid.Keys.ToDictionary(square => square, 
+    square => grid[square] == '0' ? new HashSet<Complex>([square]) : []);
 
-var pathCounts = gridMap['0'].ToDictionary(key => key, _ => 1);
-solvedp(pathCounts, (h, square) => preceding(h, square).Sum(neighbour => pathCounts[neighbour]));
-Console.WriteLine($"Part 2: {gridMap['9'].Sum(square => pathCounts[square])}");
+var paths = grid.Keys.ToDictionary(square => square, 
+    square => grid[square] == '0' ? 1 : 0);
 
-void solvedp<T>(Dictionary<Complex, T> paths, Func<char, Complex, T> dp)
-{
-    for (char h = '1'; h <= '9'; h++)
-        gridMap[h].ForEach(square => paths[square] = dp(h, square));
-}
+foreach (char h in Enumerable.Range('0', 10))
+    foreach (var square in heightMap[h])
+        foreach (var neighbour in dirs.Select(dir => square + dir)
+            .Where(n => grid.ContainsKey(n) && (h - grid[n]) == 1))
+    {
+        canReach[square].UnionWith(canReach[neighbour]);
+        paths[square] += paths[neighbour];
+    }
+
+Console.WriteLine($"Part 1: {heightMap['9'].Sum(
+    square => canReach[square].Count())}");
+Console.WriteLine($"Part 2: {heightMap['9'].Sum(square => paths[square])}");
