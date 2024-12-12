2b96da9fc902b3c433a70d5500aaf9c5d8af1dbf
Christian <cwaters@truenorthit.co.uk>
2024-12-12 15:47:40 +0000

    .

--- 2024/day12/Program.cs
+++ 2024/day12/Program.cs
@@ -16,13 +16,15 @@ foreach (var key in grid.Keys.Where(key => !regions.Any(region => region.points.
         if (!visited.Add(curr))
             continue;
 
-        dirs.Select(dir => curr + dir).Where(n => grid.ContainsKey(n) && grid[n] == grid[curr])
+        dirs.Select(dir => curr + dir)
+            .Where(n => grid.ContainsKey(n) && grid[n] == grid[curr])
             .ToList().ForEach(queue.Enqueue);
 
-        (from neighbour in dirs.Select(dir => curr + dir)
-         where (grid.ContainsKey(neighbour) && grid[neighbour] != grid[curr]) || !grid.ContainsKey(neighbour)
-         select new[] { curr, neighbour }.OrderBy(c => c.Real).ThenBy(c => c.Imaginary).ToArray())
-        .ToList().ForEach(arr => neighbours.Add((arr[0], arr[1])));
+        foreach (var neighbour in dirs.Select(dir => curr + dir).Where(n => !grid.ContainsKey(n) || grid[n] != grid[curr]))
+        {
+            var wall = new[] { curr, neighbour }.OrderBy(c => c.Real).ThenBy(c => c.Imaginary).ToArray();
+            neighbours.Add((wall[0], wall[1]));
+        }
     }
 
     var sides =     count(neighbours, grid[key], c => c.Imaginary, c => c.Real) 
@@ -31,15 +33,15 @@ foreach (var key in grid.Keys.Where(key => !regions.Any(region => region.points.
     regions.Add((neighbours.Count, sides, visited));
 }
 
-Complex getInnerSide((Complex c1, Complex c2) tp, char plant) => grid.ContainsKey(tp.c1) && grid[tp.c1] == plant ? tp.c1 : tp.c2;
+Complex innerSide((Complex c1, Complex c2) wall, char plant) => !grid.ContainsKey(wall.c1) || grid[wall.c1] != plant ? wall.c2 : wall.c1;
 int count(HashSet<(Complex p1, Complex p2)> neighbours, char plant, Func<Complex, double> first, Func<Complex, double> second)
-    => neighbours.Where(c => first(c.Item1) == first(c.Item2))
-    .GroupBy(c => second(c.Item1))
-    .Select(grp => grp.OrderBy(c => first(c.Item1)).ToArray())
+    => neighbours.Where(wall => first(wall.p1) == first(wall.p2))
+    .GroupBy(wall => second(wall.p1))
+    .Select(grp => grp.OrderBy(wall => first(wall.p1)).ToArray())
     .Sum(set => set.Aggregate(
-        new Acc(set[0], second(getInnerSide(set[0], plant)), 1),
-        (acc, h) => new Acc(h, second(getInnerSide(h, plant)),
-            (Math.Abs(first(h.Item1) - first(acc.wall.p1)) > 1) || (acc.inside != second(getInnerSide(h, plant)))
+        new Acc(set[0], second(innerSide(set[0], plant)), 1),
+        (acc, curr) => new Acc(curr, second(innerSide(curr, plant)),
+            (Math.Abs(first(curr.p1) - first(acc.prev.p1)) > 1) || (acc.inside != second(innerSide(curr, plant)))
                 ? acc.total + 1 : acc.total),
         acc => acc.total));
 
@@ -48,4 +50,4 @@ Console.WriteLine($"Part 1: {part1}");
 var part2 = regions.Sum(tp => tp.sides * tp.points.Count);
 Console.WriteLine($"Part 2: {part2}");
 
-record Acc((Complex p1, Complex p2) wall, Complex inside,int total);
+record Acc((Complex p1, Complex p2) prev, Complex inside,int total);
