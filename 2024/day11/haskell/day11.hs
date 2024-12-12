c607ade05ab0b211b294db24d64e1f76a534a254
Christian <cwaters@truenorthit.co.uk>
2024-12-12 01:32:34 +0000

    kind of stupid now
    .
    
    .

--- 2024/day11/haskell/day11.hs
+++ 2024/day11/haskell/day11.hs
@@ -1,17 +1,11 @@
 import Data.Function.Memoize
 main = do
-    f <- readFile "input.txt"
-    print [sum $ map (r s . read) $ words f | s <- [25, 75]]
-
-r :: Integer -> Integer -> Integer
+ f <- readFile "input.txt"
+ print [sum $ map (r s . read) $ words f | s <- [25 :: Int, 75]]
 r = memoize2 c where
-    c 0 _ = 1
-    c p 0 = d p 1
-    c p t
-        |   even $ length $ show t
-            = sum . map (d p) $ s t
-        | otherwise = d p $ t * 2024
-    d = r . flip (-) 1
-
-s i = map (\f -> read $ f l h) [take, drop] 
-    where h = show i; l = length h `div` 2
+ c 0 _ = 1
+ c p 0 = d p 1
+ c p (t :: Int) | even $ length $ show t = sum . map (d p) $ s t
+ c p t = d p $ t * 2024
+ d = r . flip (-) 1
+ s i = map (\f -> read $ f l h) [take, drop] where h = show i; l = length h `div` 2
