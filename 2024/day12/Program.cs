ead4e4d2eec519f6d8dbbe843655cc3979690b92
Christian <cwaters@truenorthit.co.uk>
2024-12-12 14:54:20 +0000

    .

--- 2024/day12/Program.cs
+++ 2024/day12/Program.cs
@@ -7,71 +7,45 @@ var grid = File.ReadAllLines("input.txt")
 var dirs = new Complex[] { new(1, 0), new(0, -1), new(-1, 0), new(0, 1) };
 
 var regions = new List<(int perimeter, int sides, HashSet<Complex> points)>();
-
-foreach (var key in grid.Keys)
+foreach (var key in grid.Keys.Where(key => !regions.Any(region => region.points.Contains(key))))
 {
-    if (regions.Any(region => region.points.Contains(key)))
-        continue;
-
-    var queue = new Queue<Complex>();
-    queue.Enqueue(key);
-    var visited = new HashSet<Complex>();
-    var neighbours = new HashSet<(Complex, Complex)>();    
-    //flood fill from here
-    while (queue.Any())
+    (var visited, var neighbours) = (new HashSet<Complex>(), new HashSet<(Complex,Complex)>());
+    var queue = new Queue<Complex>([key]);
+    while (queue.TryDequeue(out Complex curr))
     {
-        var curr = queue.Dequeue();
-        if (visited.Contains(curr))
+        if (!visited.Add(curr))
             continue;
 
-        foreach (var neighbour in dirs.Select(dir => curr + dir).Where(n => grid.ContainsKey(n) && grid[n] == grid[curr]))
-        {
-            queue.Enqueue(neighbour);
-        }
-        foreach (var neighbour in dirs.Select(dir => curr + dir).Where(n => (grid.ContainsKey(n) && grid[n] != grid[curr]) || !grid.ContainsKey(n)))
-        {
-            var wall = new[] { curr, neighbour }.OrderBy(c => c.Real).ThenBy(c => c.Imaginary).ToArray();            
-            neighbours.Add((wall[0], wall[1]));
-        }
+        dirs.Select(dir => curr + dir).Where(n => grid.ContainsKey(n) && grid[n] == grid[curr])
+            .ToList().ForEach(queue.Enqueue);
 
-        visited.Add(curr);
+        (from neighbour in dirs.Select(dir => curr + dir)
+         where (grid.ContainsKey(neighbour) && grid[neighbour] != grid[curr]) || !grid.ContainsKey(neighbour)
+         select new[] { curr, neighbour }.OrderBy(c => c.Real).ThenBy(c => c.Imaginary).ToArray())
+        .ToList().ForEach(arr => neighbours.Add((arr[0], arr[1])));
     }
 
-    var sides = count(neighbours, grid[key], c => c.Imaginary, c => c.Real) + count(neighbours, grid[key], c => c.Real, c => c.Imaginary);
+    var sides =     count(neighbours, grid[key], c => c.Imaginary, c => c.Real) 
+                +   count(neighbours, grid[key], c => c.Real, c => c.Imaginary);
+
     regions.Add((neighbours.Count, sides, visited));
 }
 
-//(r1,c1) -> (r2,c2)
-//p1
-//p2
-//for a horizontal wall, the Im parts will match, the Real will be x and x+1
-//-   -- - -
-//  -    --
-//So we group by r / c and count the number of clusters
-//also keeping track of which c / r is inside for a particual scan-line so we know if we are on a new wall
-//and also make sure everything is sorted consistently!
-int count(HashSet<(Complex, Complex)>  neighbours, char plant, Func<Complex, double> first, Func<Complex, double> second) 
+Complex getInnerSide((Complex c1, Complex c2) tp, char plant) => grid.ContainsKey(tp.c1) && grid[tp.c1] == plant ? tp.c1 : tp.c2;
+int count(HashSet<(Complex p1, Complex p2)> neighbours, char plant, Func<Complex, double> first, Func<Complex, double> second)
     => neighbours.Where(c => first(c.Item1) == first(c.Item2))
     .GroupBy(c => second(c.Item1))
     .Select(grp => grp.OrderBy(c => first(c.Item1)).ToArray())
-    .Sum(set =>
-    {
-        int count = 1;
-        Func<Complex, bool> isInside = cell => (grid.ContainsKey(cell) ? grid[cell] == plant : false);
-        var inside = isInside(set[0].Item1) ? second(set[0].Item1) : second(set[0].Item2);
-        var last = set[0];
-        foreach (var h in set.Skip(1))
-        {
-            var newInside = isInside(h.Item1) ? second(h.Item1) : second(h.Item2);
-            if (Math.Abs(first(h.Item1) - first(last.Item1)) > 1 || inside != newInside)
-                count++;
-            last = h;
-            inside = newInside;
-        }
-        return count;
-    });
+    .Sum(set => set.Aggregate(
+        new Acc(set[0], second(getInnerSide(set[0], plant)), 1),
+        (acc, h) => new Acc(h, second(getInnerSide(h, plant)),
+            (Math.Abs(first(h.Item1) - first(acc.wall.p1)) > 1) || (acc.inside != second(getInnerSide(h, plant)))
+                ? acc.total + 1 : acc.total),
+        acc => acc.total));
 
 var part1 = regions.Sum(tp => tp.perimeter * tp.points.Count);
 Console.WriteLine($"Part 1: {part1}");
 var part2 = regions.Sum(tp => tp.sides * tp.points.Count);
 Console.WriteLine($"Part 2: {part2}");
+
+record Acc((Complex p1, Complex p2) wall, Complex inside,int total);
