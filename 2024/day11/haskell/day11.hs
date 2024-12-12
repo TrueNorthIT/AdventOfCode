afab732bbd0224715a0eaf6c0035c50c49d45973
Christian <cwaters@truenorthit.co.uk>
2024-12-12 00:22:57 +0000

    compactified

--- 2024/day11/haskell/day11.hs
+++ 2024/day11/haskell/day11.hs
@@ -1,20 +1,17 @@
-    import Data.Function.Memoize
-    main = do
-        content <- readFile "input.txt"
-        print [sum $ map (memoizedCount s . read) $ words content | s <- [25, 75]]
+import Data.Function.Memoize
+main = do
+    f <- readFile "input.txt"
+    print [sum $ map (r s . read) $ words f | s <- [25, 75]]
 
-    memoizedCount :: Integer -> Integer -> Integer
-    memoizedCount = memoize2 count where
-        count 0 _ = 1
-        count step 0 = descend step 1
-        count step tile
-            |   even $ length $ show tile
-                = sum . map (descend step) $ split tile
-            | otherwise = descend step $ tile * 2024
-        descend = memoizedCount . flip (-) 1
-    
-    split i = map (\f -> read $ f l str) [take, drop] 
-        where str = show i; l = length str `div` 2
-    
-    --digits a  = toInteger $ ceiling $ logBase 10 $ fromIntegral a + 1
-    --split a =  [div,rem] where (div,rem) = divMod a $ (^) 10 $ fst $ divMod (digits a) 2
+r :: Integer -> Integer -> Integer
+r = memoize2 c where
+    c 0 _ = 1
+    c p 0 = d p 1
+    c p t
+        |   even $ length $ show t
+            = sum . map (d p) $ s t
+        | otherwise = d p $ t * 2024
+    d = r . flip (-) 1
+
+s i = map (\f -> read $ f l h) [take, drop] 
+    where h = show i; l = length h `div` 2
