1d77579ac847009abee53f7f4b2d4dd901365c61
Christian <cwaters@truenorthit.co.uk>
2024-12-13 20:54:26 +0000

    .

--- 2024/day12/Program.cs
+++ 2024/day12/Program.cs
@@ -1,47 +1,47 @@
-﻿        using System.Numerics;
+﻿using System.Numerics;
 
-        var dirs = new Complex[] { new(1, 0), new(0, -1), new(-1, 0), new(0, 1) };
-        var grid = File.ReadAllLines("input.txt")
-            .SelectMany((line, r) => line.Select((ch, c) => (r, c, ch)))
-            .ToDictionary(tp => new Complex(tp.r, tp.c), tp => tp.ch);
+var dirs = new Complex[] { new(1, 0), new(0, -1), new(-1, 0), new(0, 1) };
+var grid = File.ReadAllLines("input.txt")
+    .SelectMany((line, r) => line.Select((ch, c) => (r, c, ch)))
+    .ToDictionary(tp => new Complex(tp.r, tp.c), tp => tp.ch);
 
-        var regions = new List<(int perimeter, int sides, HashSet<Complex> points)>();
-        foreach (var key in grid.Keys
-            .Where(key => !regions.Any(region => region.points.Contains(key))))
+var regions = new List<(int perimeter, int sides, HashSet<Complex> points)>();
+foreach (var key in grid.Keys
+    .Where(key => !regions.Any(region => region.points.Contains(key))))
+{
+    (var visited, var walls) = (new HashSet<Complex>(), new List<(Complex p1,Complex p2)>());
+    var queue = new Queue<Complex>([key]);
+    while (queue.TryDequeue(out Complex curr))
+    {
+        if (!visited.Add(curr))
+            continue;
+
+        foreach (var n in from dir in dirs select curr + dir)
         {
-            (var visited, var walls) = (new HashSet<Complex>(), new List<(Complex p1,Complex p2)>());
-            var queue = new Queue<Complex>([key]);
-            while (queue.TryDequeue(out Complex curr))
-            {
-                if (!visited.Add(curr))
-                    continue;
-
-                foreach (var n in from dir in dirs select curr + dir)
-                {
-                    if (grid.ContainsKey(n) && grid[n] == grid[curr])
-                        queue.Enqueue(n);
-                    if (!grid.ContainsKey(n) || grid[n] != grid[curr])
-                        walls.Add(sort(curr, n));
-                }
-            }
-            int count = walls.Where(wall => wall.p1.Real == wall.p2.Real)
-                .GroupBy(wall => wall.p1.Imaginary)
-                .Select(grp => grp.OrderBy(wall => wall.p1.Real))
-                .Sum(set => set.Aggregate(
-                    (set.First(), inner(set.First(), grid[key]).Imaginary, 1),
-                    (acc, curr) => (curr, inner(curr, grid[key]).Imaginary,
-                        (Math.Abs(curr.p1.Real - acc.Item1.p1.Real) > 1)
-                        || acc.Item2 != inner(curr, grid[key]).Imaginary ? acc.Item3 + 1 : acc.Item3),
-                    acc => acc.Item3));
-
-            regions.Add((walls.Count, 2 * count, visited));
+            if (grid.ContainsKey(n) && grid[n] == grid[curr])
+                queue.Enqueue(n);
+            if (!grid.ContainsKey(n) || grid[n] != grid[curr])
+                walls.Add(sort(curr, n));
+        }
     }
+    int count = walls.Where(wall => wall.p1.Real == wall.p2.Real)
+        .GroupBy(wall => wall.p1.Imaginary)
+        .Select(grp => grp.OrderBy(wall => wall.p1.Real))
+        .Sum(set => set.Aggregate(
+            (set.First(), inner(set.First(), grid[key]).Imaginary, 1),
+            (acc, curr) => (curr, inner(curr, grid[key]).Imaginary,
+                (Math.Abs(curr.p1.Real - acc.Item1.p1.Real) > 1)
+                || acc.Item2 != inner(curr, grid[key]).Imaginary ? acc.Item3 + 1 : acc.Item3),
+            acc => acc.Item3));
 
-    Complex inner((Complex c1, Complex c2) wall, char plant) 
-        => !grid.ContainsKey(wall.c1) || grid[wall.c1] != plant ? wall.c2 : wall.c1;
+    regions.Add((walls.Count, 2 * count, visited));
+}
 
-    (Complex, Complex) sort(Complex c1, Complex c2) 
-        => c1.Real <= c2.Real ? c1.Imaginary < c2.Imaginary ? (c1, c2) : (c2, c1) : (c1, c2);
+Complex inner((Complex c1, Complex c2) wall, char plant) 
+    => !grid.ContainsKey(wall.c1) || grid[wall.c1] != plant ? wall.c2 : wall.c1;
 
-    Console.WriteLine($"Part 1: {regions.Sum(tp => tp.perimeter * tp.points.Count)}");
-    Console.WriteLine($"Part 2: {regions.Sum(tp => tp.sides * tp.points.Count)}");
+(Complex, Complex) sort(Complex c1, Complex c2) 
+    => c1.Real <= c2.Real ? c1.Imaginary < c2.Imaginary ? (c1, c2) : (c2, c1) : (c1, c2);
+
+Console.WriteLine($"Part 1: {regions.Sum(tp => tp.perimeter * tp.points.Count)}");
+Console.WriteLine($"Part 2: {regions.Sum(tp => tp.sides * tp.points.Count)}");
--- 2024/day13/Program.cs
+++ 2024/day13/Program.cs
@@ -1,39 +1,13 @@
-﻿using System.Numerics;
-using System.Text.RegularExpressions;
+﻿    using System.Numerics;
+    Console.WriteLine((solve(0), solve((long)1E13)));
 
-var machines = File.ReadAllText("input.txt").Split($"{Environment.NewLine}{Environment.NewLine}");
-Console.WriteLine($"Part 1: {solve(new BigInteger(00000000000000))}");
-Console.WriteLine($"Part 2: {solve(new BigInteger(10000000000000))}");
-
-BigInteger solve(BigInteger offset)
-{
-    BigInteger prizes = 0;
-    BigInteger tokens = 0;
-
-    foreach (var machine in machines)
-    {
-        var machineRegex = new Regex(@"Button [AB]: X\+(\d+), Y\+(\d+)");
-        var prizeRegex = new Regex(@"Prize: X=(\d+), Y=(\d+)");
-
-        var arr = machine.Split($"{Environment.NewLine}");
-        var m1 = machineRegex.Match(arr[0]);
-        var m2 = machineRegex.Match(arr[1]);
-        var mp = prizeRegex.Match(arr[2]);
-
-        var (x1, y1) = (BigInteger.Parse(m1.Groups[1].Value),
-                        BigInteger.Parse(m1.Groups[2].Value));
-
-        var (x2, y2) = (BigInteger.Parse(m2.Groups[1].Value), 
-                        BigInteger.Parse(m2.Groups[2].Value));
-
-        var (xp, yp) = (offset + BigInteger.Parse(mp.Groups[1].Value), offset + BigInteger.Parse(mp.Groups[2].Value));
-
-        var (nom, den) = (yp * x2 - xp * y2, y1 * x2 - x1 * y2);
-        var a = nom / den;
-        var bx2 = xp - a * x1;
-
-        if (nom % den == 0 && bx2 % x2 == 0)
-            tokens += 3 * a + (bx2 / x2);
-    }
-    return tokens;
-}
+    long solve(long offset) => File.ReadAllText("input.txt").Split("\r\n\r\n")
+    .Sum(machine => machine.Split("\r\n").SelectMany(
+        line => new string(line.Where(ch => Char.IsDigit(ch) || ch == ' ').ToArray())
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(str => BigRational.Parse(str))).ToArray() 
+                is [var xa, var ya, var xb, var yb, var xp, var yp]
+                ? ((yp + offset) * xb - (xp + offset) * yb) / (ya * xb - xa * yb) is var a 
+                && ((xp + offset) - a * xa) / xb is var b
+                && new[] { a, b }.All(BigRational.IsInteger) 
+            ? (long) (3 * a + b) : 0 : 0);
