e930ee71319085b7b907e8b8b1674476dc5b3660
Christian <cwaters@truenorthit.co.uk>
2024-12-13 12:26:13 +0000

    .

--- 2024/day11/haskell/day11.hs
+++ 2024/day11/haskell/day11.hs
@@ -1,20 +1,17 @@
-    import Data.Function.Memoize
-    main = do
-        content <- readFile "input.txt"
-        print [sum $ map (memoizedCount s . read) $ words content | s <- [25, 75]]
+main = do
+    content <- readFile "input.txt"
+    print [sum $ map (count s . read) $ words content | s <- [25 :: Int, 75]]
 
-    memoizedCount :: Integer -> Integer -> Integer
-    memoizedCount = memoize2 count where
-        count 0 _ = 1
-        count step 0 = descend step 1
-        count step tile
-            |   even $ length $ show tile
-                = sum . map (descend step) $ split tile
-            | otherwise = descend step $ tile * 2024
-        descend = memoizedCount . flip (-) 1
-    
-    split i = map (\f -> read $ f l str) [take, drop] 
-        where str = show i; l = length str `div` 2
+count 0 _ = 1
+count step 0 = descend step 1
+count step (tile :: Int)
+    |   even $ length $ show tile
+        = sum . map (descend step) $ split tile
+    | otherwise = descend step $ tile * 2024
+descend = count . flip (-) 1
+
+split i = map (\f -> read $ f l str) [take, drop] 
+    where str = show i; l = length str `div` 2
     
     --digits a  = toInteger $ ceiling $ logBase 10 $ fromIntegral a + 1
     --split a =  [div,rem] where (div,rem) = divMod a $ (^) 10 $ fst $ divMod (digits a) 2
--- 2024/day13/Program.cs
+++ 2024/day13/Program.cs
@@ -1,32 +1,34 @@
 ﻿using System.Numerics;
 using System.Text.RegularExpressions;
 
-var machines = File.ReadAllText("input.txt").Split($"{Environment.NewLine}{Environment.NewLine}")
-    .Select(m => m.Split($"{Environment.NewLine}")).ToArray();
-
+var machines = File.ReadAllText("input.txt").Split($"{Environment.NewLine}{Environment.NewLine}");
 Console.WriteLine($"Part 1: {solve(new BigInteger(00000000000000))}");
 Console.WriteLine($"Part 2: {solve(new BigInteger(10000000000000))}");
 
 BigInteger solve(BigInteger offset)
 {
+    BigInteger prizes = 0;
     BigInteger tokens = 0;
-    foreach (var arr in machines)
+
+    foreach (var machine in machines)
     {
         var machineRegex = new Regex(@"Button [AB]: X\+(\d+), Y\+(\d+)");
         var prizeRegex = new Regex(@"Prize: X=(\d+), Y=(\d+)");
 
-        var machineRegexes = arr.Take(2).Select(line => machineRegex.Match(line)).ToArray();
+        var arr = machine.Split($"{Environment.NewLine}");
+        var m1 = machineRegex.Match(arr[0]);
+        var m2 = machineRegex.Match(arr[1]);
         var mp = prizeRegex.Match(arr[2]);
-        
-        var (x1, y1) = (BigInteger.Parse(machineRegexes[0].Groups[1].Value), 
-                        BigInteger.Parse(machineRegexes[0].Groups[2].Value));
-        var (x2, y2) = (BigInteger.Parse(machineRegexes[1].Groups[1].Value), 
-                        BigInteger.Parse(machineRegexes[1].Groups[2].Value));
-        
-        var (xp, yp) = (offset + BigInteger.Parse(mp.Groups[1].Value), 
-                        offset + BigInteger.Parse(mp.Groups[2].Value));
 
-        var (nom, den)  = (yp * x2 - xp * y2, y1 * x2 - x1 * y2);
+        var (x1, y1) = (BigInteger.Parse(m1.Groups[1].Value),
+                        BigInteger.Parse(m1.Groups[2].Value));
+
+        var (x2, y2) = (BigInteger.Parse(m2.Groups[1].Value), 
+                        BigInteger.Parse(m2.Groups[2].Value));
+
+        var (xp, yp) = (offset + BigInteger.Parse(mp.Groups[1].Value), offset + BigInteger.Parse(mp.Groups[2].Value));
+
+        var (nom, den) = (yp * x2 - xp * y2, y1 * x2 - x1 * y2);
         var a = nom / den;
         var bx2 = xp - a * x1;
 
