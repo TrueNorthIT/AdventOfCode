1119afeba0daa52c1c1fd8fa859eca806c19e78e
Christian <cwaters@truenorthit.co.uk>
2024-12-13 22:45:00 +0000

    .

--- 2024/day13/Program.cs
+++ 2024/day13/Program.cs
@@ -1,13 +1,14 @@
-﻿    using System.Numerics;
-    Console.WriteLine((solve(0), solve((long)1E13)));
+﻿using System.Numerics;
+Console.WriteLine((solve(0), solve((long)1E13)));
 
-    long solve(long offset) => File.ReadAllText("input.txt").Split("\r\n\r\n")
-    .Sum(machine => machine.Split("\r\n").SelectMany(
-        line => new string(line.Where(ch => Char.IsDigit(ch) || ch == ' ').ToArray())
-            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-            .Select(str => BigRational.Parse(str))).ToArray() 
-                is [var xa, var ya, var xb, var yb, var xp, var yp]
-                ? ((yp + offset) * xb - (xp + offset) * yb) / (ya * xb - xa * yb) is var a 
-                && ((xp + offset) - a * xa) / xb is var b
-                && new[] { a, b }.All(BigRational.IsInteger) 
-            ? (long) (3 * a + b) : 0 : 0);
+long solve(long offset) => File.ReadAllText("input.txt").Split("\r\n\r\n")
+.Sum(machine => machine.Split("\r\n").SelectMany(
+    line => string.Concat(line.Where(ch => char.IsDigit(ch) || ch == ' '))
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(str => BigRational.Parse(str))).ToList()
+    is [var xa, var ya, var xb, var yb, var xp, var yp]
+        ? (xp + offset, yp + offset) is (var xpo, var ypo)
+        ? ((ypo * xb - xpo * yb) / (ya * xb - xa * yb)) is var a
+        ? ((xpo - a * xa) / xb) is var b
+        ? new[] { a, b }.All(BigRational.IsInteger)
+        ? (long)(3 * a + b) : 0 : 0 : 0 : 0 : 0);
