58f11ce186f187eda4117c5e22fef0fe17c00f1d
Christian <cwaters@truenorthit.co.uk>
2024-12-14 20:31:54 +0000

    visualization of solving each axis independently

--- 2024/day14/Program.cs
+++ 2024/day14/Program.cs
@@ -6,22 +6,25 @@
     }).ToList();
 
 (var W, var H) = (101, 103);
-List<(long x, long y)> step(long step)
-{
-    var next = robots.Select(robot => 
-        ((robot.px + step * (robot.vx + W)) % W,
-        (robot.py + step * (robot.vy + H)) % H))
-    .ToList();
+var step = (long step) => robots.Select(robot => 
+    (   x: (robot.px + step * (robot.vx + W)) % W,
+        y: (robot.py + step * (robot.vy + H)) % H));
 
-    if ((step - 86) % 101 == 0)
+
+Console.WriteLine($"Part 1: {countQuadrants(step(100))}");
+
+(var bx, var by) = (analyse(true), analyse(false));
+for (long t = bx; true; t += W)
+    if (t % H == by)
     {
-        Console.WriteLine($"STEP={step}");
-        print(next);
+        printGrid(step(t));
+        Console.WriteLine($"Tree detected at t={t}");
+        Console.WriteLine($"{t} = {bx} mod 101");
+        Console.WriteLine($"{t} = {by} mod 103");
+        break;
     }
-    return next;
-}
 
-long countQuadrants(List<(long x, long y)> robots)
+long countQuadrants(IEnumerable<(long x, long y)> robots)
 {
     var quadrants = new long[] { 0, 0, 0, 0, 0 };
     foreach (var robot in robots)
@@ -37,18 +40,56 @@ long countQuadrants(List<(long x, long y)> robots)
         if (robot.x > (W / 2) && robot.y > H / 2)
             quadrants[3]++;
     }
-
     return quadrants.Take(4).Aggregate(1L, (acc, n) => acc * n);
 }
-Console.WriteLine($"Part 1: {countQuadrants(step(100))}");
 
-for (var s = 0; s <= 10403; s++)
-    step(s);
+long analyse(bool xDir)
+{
+    var direction = xDir ? "x" : "y";
+    (var minVariance, var minVarianceStep) = (double.MaxValue, 0);
+    var limit = xDir ? W : H;
 
-Console.WriteLine($"Part 2: Inspect the output!");
+    //loop through all possible values for this axis
+    //and plot the distribution by count, it looks like a tree in 1d :)
+    for (int s = 0; s < limit; s++)
+    {
+        var variance = printSpectrum(s, xDir, limit);
+        if (variance < minVariance)
+        {
+            (minVariance, minVarianceStep) = (variance, s);
+        }
+        Console.WriteLine($"Step = {s}, variance = {variance:F1}");
+        Console.WriteLine();
+        Thread.Sleep(50);
+    }
 
+    Console.SetCursorPosition(0, 0);
+    printSpectrum(minVarianceStep, xDir, limit);
+    Console.WriteLine();
+    Console.WriteLine($"Check complete for axis {direction}, signal detected with variance {minVariance} at s={minVarianceStep}:");
+    Console.WriteLine();
+    Console.ReadKey();
+    return minVarianceStep;
+}
+double printSpectrum(long s, bool xDir, int limit)
+{
+    Console.SetCursorPosition(0, 0);
+    var positions = step(s).Select(point => xDir ? point.x : point.y);
+    var countsByPositionsMap = positions.GroupBy(p => p).ToDictionary(kvp => kvp.Key, kvp => kvp.Count());
+    var countsByPosition = Enumerable.Range(0, limit).Select(i => countsByPositionsMap.GetValueOrDefault(i, 0)).ToArray();
 
-void print(List<(long x, long y)> robots)
+    var mean = positions.Average();
+    var variance = positions.Sum(p => Math.Pow(p - mean, 2));
+    for (int h = 30; h >= 1; h--)
+    {
+        var line = String.Concat(countsByPosition.Select(v => v >= h ? "*" : "."));
+        Console.WriteLine(line);
+    }
+
+    return variance;
+}
+
+void printGrid(IEnumerable<(long x, long y)> robots)
 {
     var output = Enumerable.Range(0, H).Select(y => Enumerable.Repeat('.', W).ToArray()).ToArray();
     foreach (var robot in robots)
