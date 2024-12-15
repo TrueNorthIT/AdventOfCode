783a23e8f82eec8f6fdc2c09716590f189f88e32
Christian <cwaters@truenorthit.co.uk>
2024-12-15 09:50:42 +0000

    p1

--- 2024/day14/Program.cs
+++ 2024/day14/Program.cs
@@ -1,17 +1,17 @@
-﻿var robots = File.ReadAllLines("input.txt").Select(line => string.Concat(
+﻿var start = File.ReadAllLines("input.txt").Select(line => string.Concat(
         line.Where(ch => char.IsDigit(ch) || ch == ' ' || ch == '-' || ch == ','))
-    .Split([" ", ","], StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList())
+    .Split([" ", ","], StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList())
     .Select(arr => arr switch { [var px, var py, var vx, var vy] 
         => (px,py, vx, vy)
     }).ToList();
 
-const int W = 101;
-const int H = 103;
-var step = (long step) => robots.Select(robot => 
-    (   x: (robot.px + step * (robot.vx + W)) % W,
-        y: (robot.py + step * (robot.vy + H)) % H));
+const int W = 101; const int H = 103;
 
-long countQuadrants(IEnumerable<(long x, long y)> robots)
+(int x, int y)[] robots(int step) => start.Select(robot => 
+    (   x: (robot.px + step * (robot.vx + W)) % W,
+        y: (robot.py + step * (robot.vy + H)) % H)).ToArray();
+
+long countQuadrants((int x, int y)[] robots)
     => robots.CountBy(tp => tp switch {
         ( < W / 2, < H / 2) => 0,
         ( > W / 2, < H / 2) => 1,
@@ -21,80 +21,52 @@ long countQuadrants(IEnumerable<(long x, long y)> robots)
     }).Where(kvp => kvp.Key != 99)
     .Aggregate(1L, (acc, n) => acc * n.Value);
 
+Console.WriteLine($"Part 1: {countQuadrants(robots(100))}");
 (var bx, var by) = (analyse(true), analyse(false));
-for (long t = bx; true; t += W)
-    if (t % H == by)
-    {
-        Console.WriteLine(showGrid(step(t)));
-        Console.WriteLine($"Part 1: {countQuadrants(step(100))}, Part 2: t={t}");
-        Console.WriteLine($"{t} = {bx} mod {H}");
-        Console.WriteLine($"{t} = {by} mod {W}");
-        break;
-    }
 
-long analyse(bool xDir)
-{
-    const bool PRINT = true;
-    var direction = xDir ? "x" : "y";
-    (var minVariance, var minVarianceStep) = (double.MaxValue, 0);
-    var limit = xDir ? W : H;
+var part2 = bx + W * Enumerable.Range(0, int.MaxValue).First(i => (bx + W * i) % H == by);
+Console.WriteLine($"Part 2: {part2}");
 
-    for (int s = 0; s < limit; s++)
-    {
-        var variance = step(s).Select(point => xDir ? point.x : point.y) switch { var positions
-            => positions.Average() switch { var mean 
-            => positions.Sum(p => Math.Pow(p - mean, 2)) }};
-        
-        (minVariance, minVarianceStep) = variance < minVariance ? (variance, s) 
-            : (minVariance, minVarianceStep);
+int analyse(bool xDir) =>
+    Enumerable.Range(0, xDir ? W : H).Index().MinBy(s => robots(s.Item)
+    .Select(point => xDir ? point.x : point.y) switch { var positions
+    => positions.Average() switch { var mean 
+    => positions.Sum(position => Math.Pow(position - mean, 2)) }}).Index;
 
-        if (PRINT)
-        {
-            //plot the distribution by count, it looks like a tree in 1d when we hit the magic number :)
-            printSpectrum(s, xDir, limit);
-            Console.WriteLine($"Step = {s}, variance = {variance:F1}");
-            Console.WriteLine();
-            Thread.Sleep(50);
-        }
-    }
-
-    Console.SetCursorPosition(0, 0);
-    printSpectrum(minVarianceStep, xDir, limit);
-    Console.WriteLine();
-    Console.WriteLine($"Check complete for axis {direction}, signal detected with variance {minVariance} at s={minVarianceStep}:");
-    return minVarianceStep;
-}
+//Console.WriteLine(showGrid(step(t)));
+//Console.WriteLine($"{t} = {bx} mod {H}");
+//Console.WriteLine($"{t} = {by} mod {W}");
 
 ///
 /// Helpers to visualize the grid and the spectrums
 ///
-void printSpectrum(long s, bool xDir, int limit)
-{
-    Console.SetCursorPosition(0, 0);    
-    var countsByPosition = Enumerable.Range(0, limit)
-        .Select(i => step(s).Select(point => xDir ? point.x : point.y)
-        .GroupBy(p => p).ToDictionary(kvp => kvp.Key, 
-            kvp => kvp.Count()).GetValueOrDefault(i, 0))
-    .ToArray();
+//void printSpectrum(long s, bool xDir, int limit)
+//{
+//    Console.SetCursorPosition(0, 0);    
+//    var countsByPosition = Enumerable.Range(0, limit)
+//        .Select(i => step(s).Select(point => xDir ? point.x : point.y)
+//        .GroupBy(p => p).ToDictionary(kvp => kvp.Key, 
+//            kvp => kvp.Count()).GetValueOrDefault(i, 0))
+//    .ToArray();
 
-    for (int h = 30; h >= 1; h--)
-        Console.WriteLine(String.Concat(countsByPosition.Select(v => v >= h ? "*" : " ")));
-}
+//    for (int h = 30; h >= 1; h--)
+//        Console.WriteLine(String.Concat(countsByPosition.Select(v => v >= h ? "*" : " ")));
+//}
 
-string showGrid(IEnumerable<(long x, long y)> robots)
-{
-    var output = Enumerable.Range(0, H).Select(y => Enumerable.Repeat(' ', W).ToArray()).ToArray();    
-    foreach (var robot in robots)
-        output[robot.y][robot.x] = '*';
+//string showGrid(IEnumerable<(long x, long y)> robots)
+//{
+//    var output = Enumerable.Range(0, H).Select(y => Enumerable.Repeat(' ', W).ToArray()).ToArray();    
+//    foreach (var robot in robots)
+//        output[robot.y][robot.x] = '*';
 
-    var result = new System.Text.StringBuilder();
-    foreach (var arr in output)
-    {
-        foreach (var ch in arr)
-            result.Append(ch);
-        result.AppendLine();
-    }
-    result.AppendLine();
+//    var result = new System.Text.StringBuilder();
+//    foreach (var arr in output)
+//    {
+//        foreach (var ch in arr)
+//            result.Append(ch);
+//        result.AppendLine();
+//    }
+//    result.AppendLine();
 
-    return result.ToString();
-}
+//    return result.ToString();
+//}
---
+++ 2024/day15/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Numerics;
+
+//(^ for up, v for down, < for left, > for right)
+var dirs = new Complex[] { new(-1, 0), new(1, 0), new(0, -1), new(0, 1) };
+
+var files = File.ReadAllText("input.txt").Split("\r\n\r\n");
+var grid = files[0].Split("\r\n")
+            .SelectMany((line, r) => line.Select((ch, c) => (r, c, ch)))
+            .ToDictionary(tp => new Complex(tp.r, tp.c), tp => tp.ch);
+
+var start = grid.Single(kvp => kvp.Value == '@').Key; 
+grid[start] = '.';
+
+var moves = files[1].ReplaceLineEndings("");
+Console.WriteLine($"Initial grid:");
+showGrid(start);
+
+var curr = start;
+foreach (var m in moves)
+{
+    curr = move(curr, m);
+}
+
+Console.WriteLine();
+Console.WriteLine($"Final grid:");
+showGrid(curr);
+var p1 = grid.Where(kvp => kvp.Value == 'O').Sum(kvp => 100 * kvp.Key.Real + kvp.Key.Imaginary);
+Console.WriteLine($"Part 1: {p1}");
+
+
+// Returns the new robot position
+// and pushes boxes in the global grid
+Complex move(Complex start, char move)
+{
+    var dir = move switch
+    {
+        '^' => dirs[0],
+        'v' => dirs[1],
+        '<' => dirs[2],
+        '>' => dirs[3],
+    };
+    var length = 1;
+    Complex i = start;
+    while (true)
+    {
+        switch (grid[i + length*dir])
+        {
+            case '.': i += dir; goto exit; //we have moved
+            case '#': goto exit; //we have hit an obstruction
+            case 'O': length++;  //we are pushing on a length of wall
+                continue;
+            default: throw new();
+        }
+    }
+exit:;    
+    //we now know from start to i is completely clear (.'s)
+    for (var clear = start; clear != i; clear += dir)
+        grid[clear] = '.';
+
+    //and these are all O's
+    for (var os = i + dir; os != i + length * dir; os += dir)
+        grid[os] = 'O';
+
+    //return the new start position
+    return i;
+}
+
+void showGrid(Complex robot)
+{
+    var R = (int)grid.Max(kvp => kvp.Key.Real);
+    var C = (int)grid.Max(kvp => kvp.Key.Imaginary);
+    var output = new char[R+1][];
+    for (int r = 0; r <= R; r++)
+        output[r] = new char[C+1];
+
+    foreach (var square in grid)
+    {
+        output[(int)square.Key.Real][(int)square.Key.Imaginary] = square.Value;
+    }
+    output[(int) robot.Real][(int) robot.Imaginary] = '@';
+
+    var result = new System.Text.StringBuilder();
+    foreach (var arr in output)
+    {
+        foreach (var ch in arr)
+            result.Append(ch);
+        result.AppendLine();
+    }
+    result.AppendLine();
+    Console.WriteLine(result.ToString());
+}
