d99c14018571dcfd0eb92ea694264b04047aacfd
Christian <cwaters@truenorthit.co.uk>
2024-12-15 22:16:33 +0000

    now more fluent

--- 2024/day15/Program.cs
+++ 2024/day15/Program.cs
@@ -1,41 +1,39 @@
+using System.Collections.Immutable; //because it's fluent...
 using Point = (int r, int c);
 using Move = ((int r, int c) from, (int r, int c) to, char ch);
-using Grid = System.Collections.Generic.Dictionary<(int r, int c), char>;
+using Grid = System.Collections.Immutable.ImmutableDictionary<(int r, int c), char>;
+using KVP = System.Collections.Generic.KeyValuePair<(int r, int c), char>;
 
-var dirs = new Dictionary<char, (int r, int c)>() 
-    { { '^', (-1, 0) }, { 'v', (1, 0) }, { '<', (0, -1) }, { '>', (0, 1) } };
-
+var dirs = "^v<>".Zip([(-1, 0), (1, 0), (0, -1), (0, 1)]).ToDictionary<char,Point>();
 (var grid, var moves) = File.ReadAllText("input.txt").Split("\r\n\r\n")  switch { var files => 
-    (files[0].Split("\r\n").SelectMany((line, r) => line.Select((ch, c) => ((r, c), ch))).ToDictionary(),
+    (files[0].Split("\r\n").SelectMany((line, r) => line.Select((ch, c) => new KVP((r, c), ch)))
+        .ToImmutableDictionary(),
     files[1].ReplaceLineEndings(""))};
 
-Console.WriteLine((part1(), part2()));
-
-double part1() => solve(grid, grid.Single(kvp => kvp.Value == '@').Key)
-    .Where(kvp => kvp.Value == 'O').Sum(kvp => 100 * kvp.Key.r + kvp.Key.c);
-    
-double part2() => solve(
-    grid: grid.SelectMany(kvp => 
-    new[] { ((kvp.Key.r, kvp.Key.c * 2), kvp.Value == 'O' ? '[' : kvp.Value),
-            ((kvp.Key.r, kvp.Key.c * 2 + 1), kvp.Value == 'O' ? ']' : kvp.Value)})
-        .ToDictionary(kvp => kvp.Item1, kvp => kvp.Item2),
-    start: grid.Single(kvp => kvp.Value == '@').Key switch { var st => (st.r, st.c * 2) })
-.Where(kvp => kvp.Value == '[')
-.Sum(kvp => 100 * kvp.Key.r + kvp.Key.c);
+Console.WriteLine(
+    (solve( grid: grid, 
+            start: grid.Single(kvp => kvp.Value == '@').Key)
+    .Sum(kvp => kvp.Value == 'O' ? 100 * kvp.Key.r + kvp.Key.c : 0), 
+    solve(  grid: grid.SelectMany(kvp =>
+                new KVP[] { new ((kvp.Key.r, kvp.Key.c * 2), kvp.Value == 'O' ? '[' : kvp.Value),
+                            new ((kvp.Key.r, kvp.Key.c * 2 + 1), kvp.Value == 'O' ? ']' : kvp.Value)})
+            .ToImmutableDictionary(),
+            start: grid.Single(kvp => kvp.Value == '@').Key switch { var st => (st.r, st.c * 2) })
+    .Sum(kvp => kvp.Value == '[' ? 100 * kvp.Key.r + kvp.Key.c : 0)));
 
 Grid solve(Grid grid, Point start) 
-    => moves.Aggregate((grid: new Grid(grid), curr: start), (acc, m) 
+    => moves.Aggregate((grid: grid.ToImmutableDictionary(), curr: start), (acc, m) 
     => step(acc.grid, acc.curr, m) switch { 
         (true, var next, var updates) => 
-            (Update(acc.grid, updates.Select(tp => (tp.from, '.'))
-            .Concat(updates.Select(tp => (tp.to, tp.ch)))), next),
+            (acc.grid.SetItems(updates.Select(tp => new KVP(tp.from, '.'))
+            .Concat(updates.Select(tp => new KVP(tp.to, tp.ch)))), next),
         (false, var next, _) => (acc.grid, next)
     }, acc => acc.grid);
 
 (bool success, Point position, List<Move> moves) step(Grid grid, Point start, char move) 
     => (start.r + dirs[move].r, start.c + dirs[move].c) switch { (int r, int c) next
     => grid[next] switch { var ch => ch switch {
-        '.' or '@' => (true, next, [(start, next, grid[start])]),
+    '.' or '@' => (true, next, [(start, next, grid[start])]),
         '#' => (false, start, []),
         var box when ch == 'O' || move == '>' || move == '<'
             => step(grid, next, move) switch { var st
@@ -48,6 +46,3 @@ Grid solve(Grid grid, Point start)
             => st1.success && st2.success
                 ? (true, next, [(start, (r, c), grid[start]), .. st1.moves, .. st2.moves])
                 : (false, start, []) } } } } };
-
-Grid Update(Grid grid, IEnumerable<(Point, char)> items) {
-    foreach (var item in items) grid[item.Item1] = item.Item2; return grid; }
