30f1afd04834b16b84dd81aeda614223f65ec23e
Christian <cwaters@truenorthit.co.uk>
2024-12-16 17:09:17 +0000

    compactified

--- 2024/day16/Program.cs
+++ 2024/day16/Program.cs
@@ -1,67 +1,39 @@
-﻿using System.Numerics;
-using CacheKey = (System.Numerics.Complex fromPos, System.Numerics.Complex fromDir, System.Numerics.Complex toPos, System.Numerics.Complex toDir);
-using QueueKey = (System.Numerics.Complex position, System.Numerics.Complex direction);
+﻿    using System.Numerics;
+    using QueueKey = (System.Numerics.Complex fromPos, System.Numerics.Complex fromDir, System.Numerics.Complex toPos, System.Numerics.Complex toDir);
+    using CacheKey = (System.Numerics.Complex position, System.Numerics.Complex direction);
+    using CacheVal = (int score, System.Collections.Generic.HashSet<System.Numerics.Complex> visited);
 
-var grid = File.ReadAllText("input.txt").Split("\r\n")
+    var grid = File.ReadAllText("input.txt").Split("\r\n")
         .SelectMany((line, r) => line.Select((ch, c) => (new Complex(r, c), ch)))
         .ToDictionary(tp => tp.Item1, tp => tp.ch);
 
-(var S, var E) = (grid.Single(kvp => kvp.Value == 'S').Key, grid.Single(kvp => kvp.Value == 'E').Key);
-(grid[S], grid[E]) = ('.', '.');
+    var ends = grid.Where(kvp => Char.IsLetter(kvp.Value)).ToDictionary(kvp => kvp.Value, kvp=> kvp.Key);
 
-(var ccw, var cw) = (new Complex(0, 1), new Complex(0, -1));
-var dirs = new Complex[] { new(0, 1), new(1, 0), new(0, -1), new(-1, 0) }; //E, S, W, N
+    (var ccw, var cw) = (new Complex(0, 1), new Complex(0, -1));
+    var dirs = new Complex[] { new(0, 1), new(1, 0), new(0, -1), new(-1, 0) }; //E, S, W, N
 
-var cache = new Dictionary<QueueKey, (int, HashSet<Complex> routes)>();
-var queue = new PriorityQueue<CacheKey, int>();
-
-//add a cache entry for the start square, telling us how we got there
-cache.Add((S + dirs[2], dirs[0]), (0, new HashSet<Complex>([S])));
-//and queue up our first move, we came from west of S, heading East
-queue.Enqueue((S + dirs[2], dirs[0], S, dirs[0]), 0);
-
-while (queue.Count > 0)
-{
-    //queue is prioritised by score
-    //route is every square we have taken to reach the current position
-    //including the current position
-    queue.TryDequeue(out CacheKey tp, out int currScore);
-
-    (int bestScore, HashSet<Complex> bestRoute) score;
-    if (cache.TryGetValue((tp.toPos, tp.toDir), out score))
+    var cache = new [] { (new CacheKey(ends['S'] + dirs[2], dirs[0]), new CacheVal(0, new [] { ends['S'] }.ToHashSet())) }.ToDictionary();
+    var queue = new PriorityQueue<QueueKey, int>([((ends['S'] + dirs[2], dirs[0], ends['S'], dirs[0]), 0)]);
+    while (queue.TryDequeue(out QueueKey tp, out int currScore))
     {
-        if (score.bestScore == currScore)
+        if (cache.TryGetValue((tp.toPos, tp.toDir), out CacheVal best) 
+            && best.score <= currScore)
         {
-            //we have a new way of reaching the same (square,dir) with the same score
-            //merge the current route, with the new route
-            //we can't have processed this yet so we can let be processed when it is dequeued
-            score.bestRoute.UnionWith(score.bestRoute.Union(cache[(tp.fromPos, tp.fromDir)].routes));
-            score.bestRoute.Add(tp.fromPos);
+            if (best.score == currScore)
+                best.visited.UnionWith([tp.fromPos,..best.visited.Union(cache[(tp.fromPos, tp.fromDir)].visited)]);
             continue;
         }
-        else if (score.bestScore < currScore)
-        {
-            //we have done better
-            continue;
-        }
-    }
-    //we have a new best score for this square
-    //we can overwrite the cache with the route of the square we came from plus the current position
-    cache[(tp.toPos, tp.toDir)] = (currScore, cache[(tp.fromPos, tp.fromDir)].routes.Union([tp.toPos]).ToHashSet());
+        cache[(tp.toPos, tp.toDir)] = (currScore, cache[(tp.fromPos, tp.fromDir)].visited.Union([tp.toPos]).ToHashSet());
     
-    if (grid[tp.toPos + tp.toDir] != '#')
-        queue.Enqueue((tp.toPos, tp.toDir, tp.toPos + tp.toDir, tp.toDir), currScore + 1);
+        if (grid[tp.toPos + tp.toDir] != '#')
+            queue.Enqueue((tp.toPos, tp.toDir, tp.toPos + tp.toDir, tp.toDir), currScore + 1);
 
-    queue.EnqueueRange( new [] { cw, ccw }.Select(rot => ((tp.toPos, tp.toDir, tp.toPos, tp.toDir * rot), currScore + 1000)));
-}
+        queue.EnqueueRange( new [] { cw, ccw }.Select(rot => ((tp.toPos, tp.toDir, tp.toPos, tp.toDir * rot), currScore + 1000)));
+    }
 
-var p1 = cache.Where(kvp => kvp.Key.position == E).Min(kvp => kvp.Value.Item1);
-var bestRoutes = cache.Where(kvp => kvp.Key.position == E && kvp.Value.Item1 == p1).ToList();
-var p2 = bestRoutes.SelectMany(kvp => kvp.Value.routes).ToHashSet();
-
-printGrid(grid, S, p1, p2);
-Console.WriteLine($"Part 1: {p1}");
-Console.WriteLine($"Part 2: {p2.Count}");
+    var p1 = cache.Where(kvp => kvp.Key.position == ends['E']).Min(kvp => kvp.Value.Item1);
+    var p2 = cache.Where(kvp => kvp.Key.position == ends['E'] && kvp.Value.Item1 == p1).SelectMany(kvp => kvp.Value.visited).ToHashSet();
+    Console.WriteLine((p1,p2.Count));
 
 void printGrid(Dictionary<Complex, char> grid, Complex position, int bestScore, HashSet<Complex> route)
 {
