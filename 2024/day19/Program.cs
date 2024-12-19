401be99e6a69a8642bb6cda6b4c0125a21eaa58f
Christian Waters <cwaters@truenorthit.co.uk>
2024-12-19 11:30:15 +0000

    renaming

--- 2024/day19/Program.cs
+++ 2024/day19/Program.cs
@@ -1,22 +1,19 @@
-﻿var files = File.ReadAllText("input.txt").Split("\r\n\r\n");
-var patterns = files[0].Split(", ").ToArray();
-var designs = files[1].Split("\r\n");
+﻿    var files = File.ReadAllText("input.txt").Split("\r\n\r\n");
+    var patterns = files[0].Split(", ").ToArray();
+    var designs = files[1].Split("\r\n");
 
-var cache = new Dictionary<string, long>();
+    var cache = new Dictionary<string, long>();
 
-Console.WriteLine($"Part 1: {designs.Where(d => possible(d) > 0).Count()}");
-Console.WriteLine($"Part 2: {designs.Sum(possible)}");
+    Console.WriteLine($"Part 1: {designs.Where(d => possibleMemoized(d) > 0).Count()}");
+    Console.WriteLine($"Part 2: {designs.Sum(possibleMemoized)}");
 
-long possibleCached (string design)
-{
-    if (!cache.ContainsKey(design))
+    long possibleMemoized(string design)
     {
-        cache.Add(design, possible(design));
+        if (!cache.ContainsKey(design))
+            cache.Add(design, possible(design));
+        return cache[design];
     }
-    return cache[design];
-}
-long possible(string design) => design switch
-{
-    var empty when String.IsNullOrEmpty(design) => 1,
-    var other => patterns.Where(p => design.StartsWith(p)).Sum(p => possibleCached(design[p.Length..])),
-};
+    long possible(string design) => design switch {
+        var empty when String.IsNullOrEmpty(empty) => 1,
+        var other => patterns.Where(other.StartsWith).Sum(p => possibleMemoized(other[p.Length..])),
+    };
