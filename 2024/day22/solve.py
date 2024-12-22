5fa5cd7084d17ba54f97215104081c6564591fbb
Joe Pitts <jpitts@truenorthit.co.uk>
2024-12-22 15:33:26 +0000

    Only generate the numbers once

--- 2024/day22/solve.py
+++ 2024/day22/solve.py
@@ -15,26 +15,33 @@ class Day22Solver(JoesAoCSolver):
         return (step_three, diff)
     
     def generate_nth_pusdeo_random_number(self, seed: int, n: int) -> int:
+        numbers = []
         for _ in range(n):
-            seed, _ = self.generate_pusdeo_random_number(seed)
-        return seed
+            seed , diff = self.generate_pusdeo_random_number(seed)
+            numbers.append((seed, diff))
+        return seed, numbers
     
-    def generate_up_to_nth_pusdeo_random_number(self, seed: int, n: int):
+    def generate_bannana_sequences(self, numbers: list[tuple[int, int]]) -> dict[tuple[int, int, int], int]:
         seq_dict = {}
         seq = (None, None, None, None)
-        for i in range(n):
-            seed, diff = self.generate_pusdeo_random_number(seed)
+        for i, (seed, diff) in enumerate(numbers):
             if (seq:= seq[1:] + (diff,)) not in seq_dict and i >= 3:
                 seq_dict[seq] = seed % 10
         return seq_dict
     
+    seed_numbers = []
     def part1(self):
-        return sum(self.generate_nth_pusdeo_random_number(seed, 2000) for seed in self.parse_input())        
+        total = 0
+        for seed in self.parse_input():
+            seed, numbers = self.generate_nth_pusdeo_random_number(seed, 2000)
+            self.seed_numbers.append(numbers) 
+            total += seed 
+        return total
         
     def part2(self):        
         sum_dict = Counter()
-        for seed in self.parse_input():
-            sum_dict.update(self.generate_up_to_nth_pusdeo_random_number(seed, 2000))
+        for numbers in self.seed_numbers:
+            sum_dict.update(self.generate_bannana_sequences(numbers))
         return sum_dict.most_common(1)[0][1]
 
 if __name__ == "__main__":
