caa9b476069288c790116a013bf0c5366af62720
Christian <cwaters@truenorthit.co.uk>
2024-12-24 16:38:09 +0000

    p2

--- 2024/day24/Program.cs
+++ 2024/day24/Program.cs
@@ -1,5 +1,5 @@
 ﻿var files = File.ReadAllText("input.txt").Split("\r\n\r\n");
-(string x, string y, string z, string operation)[] gates = files[1].Split("\r\n").Select(line => line.Split(" -> ")).Select(arr =>
+(string x, string y, string z, string operation)[] originalGates = files[1].Split("\r\n").Select(line => line.Split(" -> ")).Select(arr =>
 {
     var sp2 = arr[0].Split(" ");
     return ((sp2[0], sp2[2], arr[1], sp2[1]));
@@ -8,7 +8,7 @@
 
 {   //Part 1
     var part1Outputs = files[0].Split("\r\n").Select(line => line.Split(": ")).Select(arr => (arr[0], arr[1] == "1")).ToDictionary();
-    var p1Gates = gates.GroupBy(tp => tp.x).Concat(gates.GroupBy(tp => tp.y)).GroupBy(grp => grp.Key).ToDictionary(grp => grp.Key, grp => grp.SelectMany(grp => grp).ToArray());
+    var p1Gates = originalGates.GroupBy(tp => tp.x).Concat(originalGates.GroupBy(tp => tp.y)).GroupBy(grp => grp.Key).ToDictionary(grp => grp.Key, grp => grp.SelectMany(grp => grp).ToArray());
     var bits = String.Join("", solve(part1Outputs.ToDictionary(), p1Gates).Select(b => b ? "1" : "0").ToArray());
     var dec = Convert.ToUInt64(bits, 2);
     Console.WriteLine($"Part 1: {(bits, dec)}");
@@ -16,65 +16,47 @@
 
 //Part 2
 //swap 1
-foreach (var swap in new (int,int)[] { })
+var p2Swaps = new (int, int)[] { (75, 144), (9, 86), (37, 49), (0, 107) };
+foreach (var swap in p2Swaps)
 {
     Console.WriteLine($"Swapping : {swap.Item1},{swap.Item2}");
-    var g1z = gates[swap.Item1].z;
-    var g2z = gates[swap.Item2].z;
-    gates[swap.Item1].z = g2z;
-    gates[swap.Item2].z = g1z;
+    var g1z = originalGates[swap.Item1].z;
+    var g2z = originalGates[swap.Item2].z;
+    originalGates[swap.Item1].z = g2z;
+    originalGates[swap.Item2].z = g1z;
 }
-int bitFailedAt = 0;
-Console.WriteLine($"Last failed at {bitFailedAt}");
+
+var p2 = String.Join(",", p2Swaps.SelectMany(tp => new[] { originalGates[tp.Item1].z, originalGates[tp.Item2].z }).OrderBy(str => str).ToArray());
+//fvw,grf,mdb,nwq,wpq,z18,z22,z36
 
 int inputSize = 45;
 var rand = new Random();
-Dictionary<(int g1, int g2), double> averages = new Dictionary<(int g1, int g2), double>();
-for (int g1 = 0; g1 < gates.Length; g1++)
-    for (int g2 = g1 + 1; g2 < gates.Length; g2++)
+int bitFailedAt = 5;
+for (int g1 = 0; g1 < originalGates.Length; g1++)
+    for (int g2 = 0; g2 < originalGates.Length; g2++)
     {
-        gates = gates.ToArray();
-        //swap a gate, we have to swap the outputs! not the gates :(
-        var g1z = gates[g1].z;
-        var g2z = gates[g2].z;
-        gates[g1].z = g2z;
-        gates[g2].z = g1z;
+        if (g2 < g1)
+            continue;
+
+        //either no swap, or swap a gate
+        //clone our array with any swaps we had already made
+        var gates = originalGates.ToArray();
+        var g1Val = gates[g1];
+        var g2Val = gates[g2];
+        gates[g1] = (g1Val.x, g1Val.y, g2Val.z, g1Val.operation);
+        gates[g2] = (g2Val.x, g2Val.y, g1Val.z, g2Val.operation); ;
 
         var _gates = gates.GroupBy(tp => tp.x).Concat(gates.GroupBy(tp => tp.y)).GroupBy(grp => grp.Key).ToDictionary(grp => grp.Key, grp => grp.SelectMany(grp => grp).ToArray());
 
-        for (int bit = bitFailedAt; bit < inputSize; bit++)
+        for (int bit = 0; bit < inputSize; bit++)
         {
             long x = (1L << bit) - 1L;
             long y = 1;
             var xStr = Convert.ToString(x, 2).PadLeft(inputSize, '0');
+            var yStr = Convert.ToString(y, 2).PadLeft(inputSize, '0');
+            var outputs = getOutputs(x, y);
 
-            var outputs = new Dictionary<string, bool>();
-            foreach (var ch in new[] { 'x', 'y' })
-            {
-                //random
-                //var max = (1L << inputSize);
-                //var input = rand.NextInt64(max);
-                //if (ch == 'x') x = input; else y = input;
-
-                long input;
-                if (ch == 'x') input = x; else input = y;
-
-                var binaryStr = Convert.ToString(input, 2).PadLeft(inputSize, '0');
-                for (int digit = binaryStr.Length - 1; digit >= 0; digit--)
-                {
-                    var indexStr = (binaryStr.Length - 1 - digit).ToString("D2");
-                    if (binaryStr[digit] == '1')
-                    {
-                        outputs.Add($"{ch}{indexStr}", true);
-                    }
-                    else
-                    {
-                        outputs.Add($"{ch}{indexStr}", false);
-                    }
-                }
-            }
-
-            //output bits is always one more than input bits
+            //output bits should always be one more than input bits
             var compBits = solve(outputs, _gates);
             var compSum = getDecimal(compBits);
             var actualSum = x + y;
@@ -85,27 +67,44 @@ for (int g1 = 0; g1 < gates.Length; g1++)
                 if (bit > bitFailedAt)
                 {
                     var failedAtStr = Convert.ToString(x, 2).PadLeft(inputSize + 1, '0');
-                    Console.WriteLine($"Failed at {x} / {failedAtStr}, swapping gates {g1},{g2}");
+                    Console.WriteLine($"swapping gates {g1},{g2} Failed at {x} / {failedAtStr}");
                     bitFailedAt = bit;
                 }
-                break;
+                goto failed;
             }
-
-            var numberMatchingBits = compBits.Zip(actualBits).Count(tp => tp.First == tp.Second);
-            //averages.Add((g1, g2), numberMatchingBits);
-            //totalMatchingBits += numberMatchingBits;
         }
-
-        ////swap back!
-        //gates[g1].z = g1z;
-        //gates[g2].z = g2z;
+        Console.WriteLine($"*** Swapping gates {g1},{g2} No failure!");
+        break;
+    failed:;
     }
-
-var results = averages.OrderByDescending(a => a.Value).ToList();
-var results2 = averages.OrderBy(a => a.Value).ToList();
 
 Console.WriteLine();
 
+Dictionary<string, bool> getOutputs(long x, long y)
+{
+    var outputs = new Dictionary<string, bool>();
+    foreach (var ch in new[] { 'x', 'y' })
+    {
+        long input;
+        if (ch == 'x') input = x; else input = y;
+
+        var binaryStr = Convert.ToString(input, 2).PadLeft(inputSize, '0');
+        for (int digit = binaryStr.Length - 1; digit >= 0; digit--)
+        {
+            var indexStr = (binaryStr.Length - 1 - digit).ToString("D2");
+            if (binaryStr[digit] == '1')
+            {
+                outputs.Add($"{ch}{indexStr}", true);
+            }
+            else
+            {
+                outputs.Add($"{ch}{indexStr}", false);
+            }
+        }
+    }
+
+    return outputs;
+}
 bool[] getOutputBits(long a) => Convert.ToString(a, 2).PadLeft(inputSize + 1, '0').Select(ch => ch == '1').ToArray();
 long getDecimal(bool[] bits) => (long) Convert.ToUInt64(String.Join("", bits.Select(b => b ? "1" : "0").ToArray()), 2);
 
