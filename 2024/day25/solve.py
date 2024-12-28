04124f17cbdfe29899aa24a96b3359470c344e29
Joe Pitts <jpitts@truenorthit.co.uk>
2024-12-28 17:38:21 +0000

    Golfed

--- 2024/day25/solve.py
+++ 2024/day25/solve.py
@@ -6,59 +6,14 @@ class Day25Solver(JoesAoCSolver):
     def parse_input(self):
         locks_and_keys = self.input_data.split("\n\n")
         
-        locks = []
-        keys = []
-        
-        
-        for lock_or_key in locks_and_keys:
-            first_line = lock_or_key.split("\n")[0]
-            if first_line == "#####":
-                # It's a lock
-                locks.append(lock_or_key)
-            else:
-                # It's a key
-                keys.append(lock_or_key)
-        
-        return locks, keys
-        
-    def score_lock(self, lock):
-        pins = [0, 0, 0, 0, 0]
-        for line in lock.split("\n")[1:]:
-            for pin in range(5):
-                if line[pin] == "#":
-                    pins[pin] += 1
-        return pins
-        
-    def score_key(self, key):
-        heights = [0, 0, 0, 0, 0]
-        for line in key.split("\n")[1:-1]:
-            for height in range(5):
-                if line[height] == "#":
-                    heights[height] += 1
-        return heights
+        locks = [[sum(1 for line in lock.splitlines()[1:] if line[pin] == "#") for pin in range(5)] for lock in locks_and_keys if lock.index("#####") == 0]
+        keys = [[sum(1 for line in key.splitlines()[1:-1] if line[pin] == "#") for pin in range(5)] for key in locks_and_keys if key.index("#####") != 0]
     
-    def does_key_fit(self, key, lock):
-        key_heights = self.score_key(key)
-        lock_pins = self.score_lock(lock)
-        
-        # add the heights to the pins
-        sum_heights = [key_heights[i] + lock_pins[i] for i in range(5)]
-        # print(f"Lock {lock_pins} and Key {key_heights} combined: {sum_heights}")
-        return not any(h > 5 for h in sum_heights)
-
+        return locks, keys
+    
     def part1(self):
-        
         locks, keys = self.parse_input()
-
-        key_lock_combinations = itertools.product(keys, locks)
-        
-        fit = 0
-        for key, lock in key_lock_combinations:
-            if self.does_key_fit(key, lock):
-                fit += 1
-                # print(f"Key: {key} fits Lock: {lock}")
-        
-        return fit        
+        return sum(all(h <= 5 for h in [key[i] + lock[i] for i in range(5)]) for key, lock in itertools.product(keys, locks))
 
 if __name__ == "__main__":
     solver = Day25Solver()
