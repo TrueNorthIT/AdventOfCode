71cb2044a27b741057c890fbe2a3ab7642995a62
Christian <cwaters@truenorthit.co.uk>
2024-12-30 15:57:37 +0000

    fully automated

--- 2024/day24/Program.cs
+++ 2024/day24/Program.cs
@@ -14,68 +14,71 @@
     Console.WriteLine($"Part 1: {(bits, dec)}");
 }
 
+int inputSize = 45;
+
 //Part 2
-var p2Swaps = new (int, int)[] { (75, 144), (9, 86), (37, 49), (0, 107) };
-foreach (var swap in p2Swaps)
+var p2Swaps = new List<(int g1, int g2)>();
+while (p2Swaps.Count < 4)
 {
-    Console.WriteLine($"Swapping : {swap.Item1},{swap.Item2}");
-    var g1z = originalGates[swap.Item1].z;
-    var g2z = originalGates[swap.Item2].z;
-    originalGates[swap.Item1].z = g2z;
-    originalGates[swap.Item2].z = g1z;
+    int bitFailedAt = 5;
+    (int g1, int g2) best = (-1, -1);
+    for (int g1 = 0; g1 < originalGates.Length; g1++)
+        for (int g2 = 0; g2 < originalGates.Length; g2++)
+        {
+            if (g1 < g2) continue;
+            var gates = originalGates;
+            swap(gates, g1, g2);
+
+            //need to fix this so we don't need a copy would be must faster
+            var _gates = gates.GroupBy(tp => tp.x).Concat(gates.GroupBy(tp => tp.y)).GroupBy(grp => grp.Key).ToDictionary(grp => grp.Key, grp => grp.SelectMany(grp => grp).ToArray());
+
+            for (int bit = 0; bit < inputSize; bit++)
+            {
+                long x = (1L << bit) - 1L;
+                long y = 1;
+                var inputs = getInputs(x, y);
+
+                //output bits should always be one more than input bits
+                var compBits = solve(inputs, _gates);
+                var compSum = getDecimal(compBits);
+                var actualSum = x + y;
+                var actualBits = getOutputBits(actualSum);
+
+                if (compSum != actualSum)
+                {
+                    if (bit > bitFailedAt)
+                    {
+                        var failedAtStr = Convert.ToString(x, 2).PadLeft(inputSize + 1, '0');
+                        Console.WriteLine($"New best candidate ({g1},{g2}). First {bit-1} bits are fixed - failed at {failedAtStr}");
+                        best = (g1, g2);
+                        bitFailedAt = bit;
+                    }
+                    goto failed;
+                }
+            }
+            best = (g1, g2);
+            goto done;
+        failed:;
+
+            swap(gates, g2, g1);
+        }
+
+    done:;
+    Console.WriteLine($"Choosing best swap {best}");
+    p2Swaps.Add((best.g1, best.g2));
+    swap(originalGates, best.g1, best.g2);
 }
 
 var p2 = String.Join(",", p2Swaps.SelectMany(tp => new[] { originalGates[tp.Item1].z, originalGates[tp.Item2].z }).OrderBy(str => str).ToArray());
 Console.WriteLine($"Part 2: {p2}");
 
-int inputSize = 45;
-var rand = new Random();
-int bitFailedAt = 5;
-for (int g1 = 0; g1 < originalGates.Length; g1++)
-    for (int g2 = 0; g2 < originalGates.Length; g2++)
-    {
-        if (g2 < g1)
-            continue;
+void swap((string x, string y, string z, string operation)[] gates, int g1, int g2)
+{
+    (var g1z, var g2z) = (gates[g1].z, gates[g2].z);
+    (gates[g1].z, gates[g2].z) = (g2z, g1z);
+}
 
-        //either no swap, or swap a gate
-        //clone our array with any swaps we had already made
-        var gates = originalGates.ToArray();
-        (var g1z, var g2z) = (gates[g1].z, gates[g2].z);
-        (gates[g1].z, gates[g2].z) = (g2z, g1z);
-
-        var _gates = gates.GroupBy(tp => tp.x).Concat(gates.GroupBy(tp => tp.y)).GroupBy(grp => grp.Key).ToDictionary(grp => grp.Key, grp => grp.SelectMany(grp => grp).ToArray());
-
-        for (int bit = 0; bit < inputSize; bit++)
-        {
-            long x = (1L << bit) - 1L;
-            long y = 1;
-            var xStr = Convert.ToString(x, 2).PadLeft(inputSize, '0');
-            var yStr = Convert.ToString(y, 2).PadLeft(inputSize, '0');
-            var outputs = getOutputs(x, y);
-
-            //output bits should always be one more than input bits
-            var compBits = solve(outputs, _gates);
-            var compSum = getDecimal(compBits);
-            var actualSum = x + y;
-            var actualBits = getOutputBits(actualSum);
-
-            if (compSum != actualSum)
-            {
-                if (bit > bitFailedAt)
-                {
-                    var failedAtStr = Convert.ToString(x, 2).PadLeft(inputSize + 1, '0');
-                    Console.WriteLine($"swapping gates {g1},{g2} Failed at {x} / {failedAtStr}");
-                    bitFailedAt = bit;
-                }
-                goto failed;
-            }
-        }
-        Console.WriteLine($"*** Swapping gates {g1},{g2} No failure!");
-        break;
-    failed:;
-    }
-
-Dictionary<string, bool> getOutputs(long x, long y)
+Dictionary<string, bool> getInputs(long x, long y)
 {
     var outputs = new Dictionary<string, bool>();
     foreach (var ch in new[] { 'x', 'y' })
@@ -102,10 +105,11 @@ Dictionary<string, bool> getOutputs(long x, long y)
 }
 bool[] getOutputBits(long a) => Convert.ToString(a, 2).PadLeft(inputSize + 1, '0').Select(ch => ch == '1').ToArray();
 long getDecimal(bool[] bits) => (long) Convert.ToUInt64(String.Join("", bits.Select(b => b ? "1" : "0").ToArray()), 2);
+string print(long a) => Convert.ToString(a, 2).PadLeft(inputSize, '0');
 
-bool[] solve(Dictionary<string, bool> outputs, Dictionary<string, (string x, string y, string z, string operation)[]> gates)
+bool[] solve(Dictionary<string, bool> inputs, Dictionary<string, (string x, string y, string z, string operation)[]> gates)
 {
-    var queue = new Queue<string>(outputs.Keys);
+    var queue = new Queue<string>(inputs.Keys);
     while (queue.TryDequeue(out var next))
     {
         if (!gates.ContainsKey(next))
@@ -113,18 +117,18 @@ bool[] solve(Dictionary<string, bool> outputs, Dictionary<string, (string x, str
             continue;
         foreach (var g in gates[next])
         {
-            if (outputs.ContainsKey(g.x) && outputs.ContainsKey(g.y) && !outputs.ContainsKey(g.z))
+            if (inputs.ContainsKey(g.x) && inputs.ContainsKey(g.y) && !inputs.ContainsKey(g.z))
             {
                 switch (g.operation)
                 {
                     case "AND":
-                        outputs[g.z] = outputs[g.x] && outputs[g.y];
+                        inputs[g.z] = inputs[g.x] && inputs[g.y];
                         break;
                     case "OR":
-                        outputs[g.z] = outputs[g.x] || outputs[g.y];
+                        inputs[g.z] = inputs[g.x] || inputs[g.y];
                         break;
                     case "XOR":
-                        outputs[g.z] = outputs[g.x] ^ outputs[g.y];
+                        inputs[g.z] = inputs[g.x] ^ inputs[g.y];
                         break;
                 }
                 queue.Enqueue(g.z);
@@ -132,5 +136,5 @@ bool[] solve(Dictionary<string, bool> outputs, Dictionary<string, (string x, str
         }
     }
 
-    return outputs.Where(tp => tp.Key.StartsWith("z")).OrderByDescending(tp => tp.Key).Select(kvp => kvp.Value).ToArray();
+    return inputs.Where(tp => tp.Key.StartsWith("z")).OrderByDescending(tp => tp.Key).Select(kvp => kvp.Value).ToArray();
 }
