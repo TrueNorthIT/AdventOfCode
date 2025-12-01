62a836e93ecad6a4d2c8906e92c34b0e0206582e
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-01 13:14:17 +0000

    .

--- 2025/day01/Day01/Program.cs
+++ 2025/day01/Day01/Program.cs
@@ -1,9 +1,11 @@
-﻿Console.WriteLine(File.ReadAllLines("i").Aggregate((c: 50, p1: 0, p2: 0), (a, s) =>
+﻿var(c, a, b) = (50, 0, 0);
+foreach (var s in File.ReadAllLines("i"))
 {
     for (int i = 0; i < int.Parse(s[1..]); i++)
     {
-        a.c += s[0] == 'R' ? 1 : -1;
-        if (a.c % 100 == 0) a.p2++;
+        c += s[0] == 'R' ? 1 : -1;
+        if (c % 100 == 0) b++;
     }
-    return (a.c, a.p1 += a.c % 100 == 0 ? 1 : 0, a.p2);
-}));
+    if (c % 100 == 0) a++;
+}
+Console.WriteLine((a,b));
