81172dc9736c5381eaf24891f79bf4b654e556c1
Samuel Hattersley <SHattersley@trueNorthIT.co.uk>
2025-12-01 10:35:56 +0000

    part 2

--- 2025/day01/day1.py
+++ 2025/day01/day1.py
@@ -1,3 +1,4 @@
+
 def inputting():
     text = open("input.txt","r")
     lines = text.readlines()
@@ -8,13 +9,42 @@ def inputting():
             continue
         newlines.append(newline)
     return newlines
-print(inputting())
 
 def calcnewdegree(currentdegree,direction,number):
     if direction == "L":
         return (currentdegree -number) % 100
     else:
         return (currentdegree+number) %100
+def calcnewdegree2(currentdegree,direction,number):
+    past0 = numberpassing(currentdegree, direction, number)
+    if direction == "L":
+        return ((currentdegree -number) % 100,past0)
+    else:
+        return ((currentdegree+number) %100,past0)
+
+
+
+def numberpassing(currentdegree,direction,number):
+    passes = 0
+
+    if direction =='L':
+        newdegree = currentdegree-number
+        while newdegree <= 0:
+            passes += 1
+            newdegree += 100
+        if currentdegree==0:
+            passes-=1
+
+    if direction == 'R':
+        newdegree = currentdegree+number
+        while newdegree>=100:
+            passes+=1
+            newdegree-=100
+    return passes
+
+print(numberpassing(50,'L',49))
+
+
 def part1(lines):
     currentdegree=50
     currentnum =0
@@ -26,4 +56,20 @@ def part1(lines):
             print(line)
             currentnum+=1
     return currentnum
-print(part1(inputting()))
+def part2(lines):
+    print(len(lines))
+    currentdegree = 50
+    currentnum = 0
+    passing0=0
+    for line in lines:
+        direction = line[0]
+        number = int(line[1:])
+        currentdegreegetter = calcnewdegree2(currentdegree, direction, number)
+        currentdegree =currentdegreegetter[0]
+        passing0 =passing0+ currentdegreegetter[1]
+        print(line)
+        print(passing0)
+        if currentdegree == 0:
+            currentnum += 1
+    return currentnum,passing0
+print(part2(inputting()))
