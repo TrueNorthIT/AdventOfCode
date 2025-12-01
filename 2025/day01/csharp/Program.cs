b372eb2a154d83177a83711fb5fe1452789471e6
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-01 21:29:32 +0000

    power automate solution

---
+++ 2025/day01/csharp/Program.cs
@@ -0,0 +1,6 @@
+﻿int c=50,a=0,b=0;
+foreach(var s in File.ReadLines("i")){
+ for(int i=0;i<int.Parse(s[1..]);(i,c)=(i+1,(c+(s[0]=='R'?1:-1))%100))
+  if(c==0)b++;
+ if(c==0)a++;
+}throw new((a,b)+"");
