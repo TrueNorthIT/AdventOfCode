82db2e6b152e46ad0dab49bcb6c436c2772b8179
Muhamad Hewa Rahim <mhewarahim@truenorthit.co.uk>
2025-12-01 14:40:41 +0000

    part 2

--- 2025/day01/solve.py
+++ 2025/day01/solve.py
@@ -1,19 +1,41 @@
+def newdegree(currentdegree,direction,number):
+    past0 = numberpassing(currentdegree, direction, number)
+    if direction == "L":
+        return ((currentdegree -number) % 100,past0)
+    else:
+        return ((currentdegree+number) %100,past0)
+    
+def numberpassing(currentdegree,direction,number):
+    passes = 0
+    if direction =='L':
+        newdegree = currentdegree-number
+        while newdegree <= 0:
+            passes += 1
+            newdegree += 100
+        if currentdegree==0:
+            passes-=1
+
+    if direction == 'R':
+        newdegree = currentdegree+number
+        while newdegree>=100:
+            passes+=1
+            newdegree-=100
+    return passes
+
 value = 50
-count = 0
+currentnum = 0
+count=0
 
 with open("values.txt", "r") as file:
     lines = file.read().splitlines()
 
 for line in lines:
-    if line[0] == "L":
-        value = value - int(line[1:])
-        while value < 0:
-            value = value + 100
-    else:
-        value = value + int(line[1:])
-        while value > 99:
-            value = value - 100
+    direction = line[0]
+    number = int(line[1:])
+    currentdegreegetter = newdegree(value, direction, number)
+    value = currentdegreegetter[0]
+    count = count + currentdegreegetter[1]
+    print(line)
+    print(count)
     if value == 0:
-        count += 1
-        
-print(count)
+        currentnum += 1
