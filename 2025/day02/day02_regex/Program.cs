dc8767be53dc2897e3cbe8bca1b5fc54ffb53b5d
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-02 22:49:54 +0000

    .

--- 2025/day02/day02_regex/Program.cs
+++ 2025/day02/day02_regex/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System.Text.RegularExpressions;
 
 HashSet<long> p1 = [], p2 = [];
-bool m(long i, bool b) => Regex.IsMatch(i + "", "^(\\d{1,})\\1{1" + (b ? "}$" : ",100}$"));
+bool m(long i, bool b) => Regex.IsMatch(i + "", @"^(\d{1,})\1{1" + (b ? "}$" : ",100}$"));
 
-foreach (var arr in File.ReadAllText("example.txt").Split(',').Select(r =>
-    r.Split('-').Select(long.Parse).ToArray()))
-    for (var i = arr[0]; i <= arr[1]; i++)
-        if (m(i,true))
+foreach (var s in File.ReadAllText("example.txt").Split(',').Select(r =>
+    r.Split('-').Select(long.Parse).ToList()))
+    for (var i = s[0]; i <= s[1]; i++)
+        if (m(i,1>0))
             p1.Add(i);
-        else if (m(i, false))
+        else if (m(i, 1<0))
             p2.Add(i);
 
-int.Parse($"{p1.Sum()},{p1.Concat(p2).Sum()}");
+int.Parse((p1.Sum(),p1.Sum()+p2.Sum())+"");
