6182e5d86763f20038779dae243f9d2c212d81c7
Christian <cwaters@truenorthit.co.uk>
2025-12-02 13:24:07 +0000

    golfff

--- 2025/day02/day02_regex/Program.cs
+++ 2025/day02/day02_regex/Program.cs
@@ -4,12 +4,9 @@ HashSet<long> p1 = [], p2 = [];
 var r = "^(\\d{1,})\\1{1";
 bool m(long i, bool b) => Regex.IsMatch(i + "", r + (b ? "}$" : ",100}$"));
 
-foreach ((long st, long en) in File.ReadAllText("example.txt").Split(',').Select(r =>
-    r.Split('-') switch {
-        [var a, var b] => (long.Parse(a), long.Parse(b))
-    }
-))
-    for (var i = st; i <= en; i++)
+foreach (var arr in File.ReadAllText("example.txt").Split(',').Select(r =>
+    r.Split('-').Select(long.Parse).ToArray()))
+    for (var i = arr[0]; i <= arr[1]; i++)
         if (m(i,true))
             p1.Add(i);
         else if (m(i, false))
