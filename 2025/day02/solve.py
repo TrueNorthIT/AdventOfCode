ae70f036157537a507065a617175ec4d9498c8ce
Muhamad Hewa Rahim <mhewarahim@truenorthit.co.uk>
2025-12-02 11:05:53 +0000

    day 2 part 2

--- 2025/day02/solve.py
+++ 2025/day02/solve.py
@@ -1,19 +1,17 @@
+l1, l2 = [], []
 total = 0
 
 with open("values.txt", "r") as file:
     lines = file.read().split(',')
 
 for line in lines:
-    range = line.split('-')
-    x = int(range[0])
-    while x <= int(range[1]):
-        length = len(str(x))
-        even = length%2
-        if even == 0:
-            length = int(length/2)
-            string = str(x)
-            if string[:length] == string[length:]:
-                total += int(string)
-        x += 1
+    part1, part2 = line.split('-')
+    for i in range(int(part1), int(part2) + 1, 1):
+        for size in range(1, len(str(i)) // 2 + 1):
+            if len(str(i)) % size == 0: 
+                pattern = str(i)[:size]
+                if pattern * (len(str(i)) // size) == str(i):
+                    total += i
+                    break
     
 print(total)
