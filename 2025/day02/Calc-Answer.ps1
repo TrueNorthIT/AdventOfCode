ed33849673c13cc8cf6df3a6c6c6b2f24696390c
Alex Radice <aradice@truenorthit.co.uk>
2025-12-02 08:43:12 +0000

    Day 2 part 2

--- 2025/day02/Calc-Answer.ps1
+++ 2025/day02/Calc-Answer.ps1
@@ -3,12 +3,25 @@
     $range = $_.Split("-")
     for ($i = [long]$range[0]; $i -le [long]$range[1]; $i++) {
         $istr = $i.ToString()
-        if ($istr.Length % 2 -eq 0)
-        {
-            if ($istr.Substring(0, $istr.Length / 2) -eq $istr.Substring($istr.Length / 2))
+        $matching = $false
+        for ($fragments = 2; (-not $matching) -and ($fragments -le $istr.Length); $fragments ++) {
+            if ($istr.Length % $fragments -eq 0)
             {
-                $i
+                $firstPart = $istr.Substring(0, $istr.Length / $fragments)
+                $index = $firstPart.Length
+                $matching = $true
+                do
+                {
+                    if ($firstPart -ne $istr.Substring($index, $firstPart.Length))
+                    {
+                        $matching = $false
+                    }
+                    $index += $firstPart.Length
+                } while ($matching -and ($index -lt $istr.Length))
             }
+        }
+        if ($matching) {
+            $i
         }
     }
 } | Measure-Object -Sum | Select-Object -Property Sum
