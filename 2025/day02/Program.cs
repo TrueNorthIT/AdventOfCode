cb64b26baccdeab7629a0c9234ec11d7670dd503
Christian <cwaters@truenorthit.co.uk>
2025-12-02 10:28:36 +0000

    tidied up

--- 2025/day02/Program.cs
+++ 2025/day02/Program.cs
@@ -1,33 +1,12 @@
-﻿(long start, long end)[] ranges = File.ReadAllText("input.txt").Split(',').Select(range =>
+﻿(long start, long end)[] ranges = [.. File.ReadAllText("input.txt").Split(',').Select(range =>
 {
     var sp = range.Split('-');
     return (long.Parse(sp[0]), long.Parse(sp[1]));
-}).ToArray();
+})];
 
-var invalid = new List<long>();
+var p1 = new HashSet<long>();
+var p2 = new HashSet<long>();
 foreach (var range in ranges)
-{
-    for (long i = range.start; i <= range.end; i++)
-    {
-        var st = i.ToString();
-        if (st.Length % 2 != 0)
-            continue;
-
-        var i1 = st.Substring(0, st.Length / 2);
-        var i2 = st.Substring(st.Length / 2, st.Length / 2);
-        if (i1 == i2)
-        {
-            invalid.Add(i);
-        }
-    }
-}
-
-var p1 = invalid.Sum();
-Console.WriteLine(p1);
-
-var invalidp2 = new List<long>();
-foreach (var range in ranges)
-{
     for (long i = range.start; i <= range.end; i++)
     {
         var st = i.ToString();
@@ -40,18 +19,14 @@ foreach (var range in ranges)
             var size = st.Length / divisor;
             var first = st.Substring(0, size);
             for (int t = 1; t < divisor; t++)
-            {
-                var next = st.Substring(t * size, size);
-                if (next != first)
-                {
+                if (st.Substring(t * size, size) != first)
                     goto skip;
-                }
-            }
-            invalidp2.Add(i);
+
+            if (divisor == 2)
+                p1.Add(i);
+            p2.Add(i);
             skip:;
         }
     }
-}
-var p2 = invalidp2.Distinct().Sum();
-Console.WriteLine(p2);
 
+Console.WriteLine($"{p1.Sum()},{p2.Sum()}");
