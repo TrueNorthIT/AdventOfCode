4a0768bbc72707afee922e1c52ae64c448a628f0
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-03 18:35:59 +0000

    day 3 golfing

--- 2025/day03/Program.cs
+++ 2025/day03/Program.cs
@@ -1,20 +1,13 @@
-﻿var banks = File.ReadAllLines("input.txt");
-
-(var p1, var p2) = (0L, 0L);
-foreach (var bank in banks)
-{
-    p1 += best(bank, "", 2);
-    p2 += best(bank, "", 12);
-}
-
-long best(string rem, string num, int L) => num.Length == L ?
-    long.Parse(num)
-    : rem[..^(L - num.Length - 1)]
-        .Index()
-        .OrderByDescending(tp => tp.Item)
-        .ThenBy(tp => tp.Index)
-        .First() switch {
-            var next => best(rem[(next.Index + 1)..], num + next.Item, L)
-        };
-
-Console.WriteLine((p1,p2));
+﻿int.Parse((solve(2), solve(12))+"");
+long solve(int max) => File.ReadAllLines("input.txt").Sum(b => best(b, "", max));
+long best(string rem, string num, int L) =>
+    (num.Length == L) switch {
+    true => long.Parse(num),
+    false => rem[..^(L - num.Length - 1)]
+                .Index()
+                .OrderByDescending(tp => tp.Item)
+                .ThenBy(tp => tp.Index)
+                .First() switch { var next 
+                => best(rem[(next.Index + 1)..], num + next.Item, L) 
+            }
+    };
