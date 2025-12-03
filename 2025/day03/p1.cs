1356cdf005f6d77c683be3856a78548915fd38bc
Richard Kelsey <richard.kelsey12@gmail.com>
2025-12-03 10:51:59 +0000

    day3

---
+++ 2025/day03/p1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using System.Text;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = new List<string>();
+
+            FileStream fs = new FileStream("C:\\Users\\RichardKelsey\\source\\repos\\Aoc\\Aoc\\input.txt", FileMode.Open, FileAccess.Read);
+            //FileStream fs = new FileStream("C:\\Users\\RichardKelsey\\source\\repos\\Aoc\\Aoc\\demo.txt", FileMode.Open, FileAccess.Read);
+
+            StreamReader streamReader = new StreamReader(fs);
+
+            while (!streamReader.EndOfStream)
+            {
+                input.Add(streamReader.ReadLine());
+            }
+
+            Console.WriteLine($"Read {input.Count} lines");
+
+            int totalOutput = 0;
+
+            foreach (var line in input)
+            {
+                Vector2 num1 = new Vector2 (0,0);
+                Vector2 num2 = new Vector2(0, 0);
+                float current = 0;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    current = float.Parse(line[i].ToString());
+                    if (current > num1.X && i != line.Length - 1)
+                    {
+                        num1.X = current;
+                        num1.Y = i;
+                        num2 = new Vector2(0, 0);
+                    }
+                    else if (current > num2.X && i > num1.Y)
+                    {
+                        num2.X = current;
+                        num2.Y = i;
+                    }
+                }
+                Console.WriteLine($"Line: {line} Max1: {num1.X} at pos {num1.Y} Max2: {num2.X} at {num2.Y} combo: {num1.X}{num2.X}");
+                totalOutput += int.Parse($"{num1.X}{num2.X}");
+            }
+
+            Console.WriteLine($"Total Output: {totalOutput}");
+        }
+    }
+}
---
+++ 2025/day03/p2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics; // Needed for Vector2 and BigInteger
+using System.Text;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = new List<string>();
+
+            string filePath = "C:\\Users\\RichardKelsey\\source\\repos\\Aoc\\Aoc\\input.txt";
+            //string filePath = "C:\\Users\\RichardKelsey\\source\\repos\\Aoc\\Aoc\\demo.txt";
+
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        input.Add(line);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Read {input.Count} lines");
+
+            long totalOutput = 0;
+
+            foreach (var line in input)
+            {
+                StringBuilder sb = new StringBuilder();
+                int lastIndexFound = -1;
+
+                for (int i = 0; i < 12; i++)
+                {
+                    int remainingNeeded = 11 - i;
+
+                    int maxSearchIndex = line.Length - 1 - remainingNeeded;
+
+                    Vector2 bestCandidate = new Vector2(-1, -1);
+
+                    for (int k = lastIndexFound + 1; k <= maxSearchIndex; k++)
+                    {
+                        float currentVal = float.Parse(line[k].ToString());
+
+                        if (currentVal > bestCandidate.X)
+                        {
+                            bestCandidate = new Vector2(currentVal, k);
+                            if (currentVal == 9) break;
+                        }
+                    }
+
+                    sb.Append(bestCandidate.X);
+
+                    lastIndexFound = (int)bestCandidate.Y;
+                }
+
+                long lineValue = long.Parse(sb.ToString());
+                totalOutput += lineValue;
+            }
+
+            Console.WriteLine($"Total Output: {totalOutput}");
+        }
+    }
+}
