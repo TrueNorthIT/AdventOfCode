213796c7235a1013b52a241ce1bf8929d7333a90
Alex Radice <aradice@truenorthit.co.uk>
2025-12-03 17:02:34 +0000

    Day 3 part 2

--- 2025/day03/Logic.sql
+++ 2025/day03/Logic.sql
@@ -42,4 +42,50 @@ BEGIN
 END
 GO
 
-select sum(dbo.get_Power([puzzle])) from dbo.inputsample
+create function dbo.get_Power2(@input VARCHAR(100)) RETURNS BIGINT
+AS
+BEGIN
+	declare @index1 int	
+	declare @index2 int	
+	declare @index3 int	
+	declare @index4 int	
+	declare @index5 int	
+	declare @index6 int	
+	declare @index7 int	
+	declare @index8 int	
+	declare @index9 int	
+	declare @index10 int	
+	declare @index11 int	
+	declare @index12 int	
+	set @index1 = dbo.get_GetFirstIndexOfHighest(@input, 1, LEN(@input)-11)
+	set @index2 = dbo.get_GetFirstIndexOfHighest(@input, @index1 + 1, LEN(@input)-10)
+	set @index3 = dbo.get_GetFirstIndexOfHighest(@input, @index2 + 1, LEN(@input)-9)
+	set @index4 = dbo.get_GetFirstIndexOfHighest(@input, @index3 + 1, LEN(@input)-8)
+	set @index5 = dbo.get_GetFirstIndexOfHighest(@input, @index4 + 1, LEN(@input)-7)
+	set @index6 = dbo.get_GetFirstIndexOfHighest(@input, @index5 + 1, LEN(@input)-6)
+	set @index7 = dbo.get_GetFirstIndexOfHighest(@input, @index6 + 1, LEN(@input)-5)
+	set @index8 = dbo.get_GetFirstIndexOfHighest(@input, @index7 + 1, LEN(@input)-4)
+	set @index9 = dbo.get_GetFirstIndexOfHighest(@input, @index8 + 1, LEN(@input)-3)
+	set @index10 = dbo.get_GetFirstIndexOfHighest(@input, @index9 + 1, LEN(@input)-2)
+	set @index11 = dbo.get_GetFirstIndexOfHighest(@input, @index10 + 1, LEN(@input)-1)
+	set @index12 = dbo.get_GetFirstIndexOfHighest(@input, @index11 + 1, LEN(@input))
+	return CONVERT(bigint, 
+		substring(@input, @index1,1) 
+		+ substring(@input, @index2,1)
+		+ substring(@input, @index3,1)
+		+ substring(@input, @index4,1)
+		+ substring(@input, @index5,1)
+		+ substring(@input, @index6,1)
+		+ substring(@input, @index7,1)
+		+ substring(@input, @index8,1)
+		+ substring(@input, @index9,1)
+		+ substring(@input, @index10,1)
+		+ substring(@input, @index11,1)
+		+ substring(@input, @index12,1)
+	)
+END
+GO
+
+select dbo.get_Power2('818181911112111')
+
+select sum(dbo.get_Power2([puzzle])) from dbo.input
