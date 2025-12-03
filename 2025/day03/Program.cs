fb241a31adc3cf826c1c9ade37159fc52ec76b35
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-03 09:47:27 +0000

    p2 tidy

--- 2025/day03/Program.cs
+++ 2025/day03/Program.cs
@@ -3,8 +3,8 @@
 (var p1, var p2) = (0L, 0L);
 foreach (var bank in banks)
 {
-    p1 += best(bank, new List<char>(), 2);
-    p2 += best(bank, new List<char>(), 12);
+    p1 += best(bank, [], 2);
+    p2 += best(bank, [], 12);
 }
 
 long best(string remainder, List<char> digits, int maxLength)
@@ -12,14 +12,13 @@ long best(string remainder, List<char> digits, int maxLength)
     if (digits.Count == maxLength)
         return long.Parse(String.Join("", digits));
 
-    var remainingDigits = maxLength - digits.Count;
     for (char c = '9'; c >= '0'; c--)
     {
-        var indexOf = remainder.IndexOf(c,0,remainder.Length - remainingDigits + 1);
+        var indexOf = remainder.IndexOf(c,0,remainder.Length - maxLength - digits.Count + 1);
         if (indexOf == -1)
             continue;
 
-        return best(remainder[(indexOf + 1)..], digits.Concat([c]).ToList(), maxLength);
+        return best(remainder[(indexOf + 1)..], [.. digits, c], maxLength);
     }
     throw new();
 }
