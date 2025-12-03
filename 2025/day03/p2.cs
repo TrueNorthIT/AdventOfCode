991f5ee3b319ad538f26327bb4557672c13a91ba
Richard Kelsey <richard.kelsey12@gmail.com>
2025-12-03 10:55:05 +0000

    take away copilot comment >:(

--- 2025/day03/p2.cs
+++ 2025/day03/p2.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Numerics; // Needed for Vector2 and BigInteger
+using System.Numerics;
 using System.Text;
 
 namespace MyApp
