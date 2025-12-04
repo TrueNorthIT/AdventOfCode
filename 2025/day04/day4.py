2d8b7bb4109f11f917a79db4ad5d13edf18e6ac3
Samuel Hattersley <SHattersley@trueNorthIT.co.uk>
2025-12-04 09:52:50 +0000

    day4 part 1

---
+++ 2025/day04/day4.py
@@ -0,0 +1,116 @@
+def inputting():
+    text = open("input.txt","r")
+    lines = text.readlines()
+    newlines=[]
+    for line in lines:
+        newline = line.replace("\n","")
+        if newline =="":
+            continue
+        newlines.append(newline)
+    return newlines
+
+def positionchecker(lines,rownum,colnum):
+    return lines[rownum][colnum]=="@"
+
+def part1(lines):
+    columnlength=len(lines)
+    rowlength=len(lines[0])
+    total=0
+    for linenum in range(0,columnlength):
+        for colnum in range(0,rowlength):
+            currentpos = lines[linenum][colnum]
+            #print(currentpos)
+            if currentpos == ".":
+                continue
+            elif currentpos == "@":
+                aroundtotal=0
+                if colnum==0:
+                    if linenum==0:
+                        pass
+                    elif linenum ==columnlength-1:
+                        pass
+                    else:
+                        if(positionchecker(lines,linenum-1,colnum)):
+                            aroundtotal+=1
+                        if(positionchecker(lines,linenum+1,colnum)):
+                            aroundtotal+=1
+                        if (positionchecker(lines, linenum , colnum+1)):
+                            aroundtotal += 1
+                        if (positionchecker(lines, linenum + 1, colnum+1)):
+                            aroundtotal += 1
+                        if (positionchecker(lines, linenum - 1, colnum+1)):
+                            aroundtotal += 1
+                if colnum ==rowlength-1:
+                    if linenum==0:
+                        pass
+                    elif linenum ==columnlength-1:
+                        pass
+                    else:
+                        if(positionchecker(lines,linenum-1,colnum)):
+                            aroundtotal+=1
+                        if(positionchecker(lines,linenum+1,colnum)):
+                            aroundtotal+=1
+                        if (positionchecker(lines, linenum , colnum-1)):
+                            aroundtotal += 1
+                        if (positionchecker(lines, linenum + 1, colnum-1)):
+                            aroundtotal += 1
+                        if (positionchecker(lines, linenum - 1, colnum-1)):
+                            aroundtotal += 1
+                if linenum ==0:
+                    if colnum==0:
+                        pass
+                    elif colnum ==columnlength-1:
+                        pass
+                    else:
+                        if (positionchecker(lines, linenum , colnum+1)):
+                            aroundtotal += 1
+                        if (positionchecker(lines, linenum + 1, colnum)):
+                            aroundtotal += 1
+                        if (positionchecker(lines, linenum, colnum - 1)):
+                            aroundtotal += 1
+                        if (positionchecker(lines, linenum + 1, colnum - 1)):
+                            aroundtotal += 1
+                        if (positionchecker(lines, linenum + 1, colnum + 1)):
+                            aroundtotal += 1
+                if linenum ==columnlength-1:
+                    if colnum==0:
+                        pass
+                    elif colnum ==columnlength-1:
+                        pass
+                    else:
+                        if (positionchecker(lines, linenum , colnum+1)):
+                            aroundtotal += 1
+                        if (positionchecker(lines, linenum - 1, colnum)):
+                            aroundtotal += 1
+                        if (positionchecker(lines, linenum, colnum - 1)):
+                            aroundtotal += 1
+                        if (positionchecker(lines, linenum - 1, colnum - 1)):
+                            aroundtotal += 1
+                        if (positionchecker(lines, linenum - 1, colnum + 1)):
+                            aroundtotal += 1
+                if linenum!=0 and linenum!=columnlength-1 and colnum!=0 and colnum!=rowlength-1:
+                    if (positionchecker(lines, linenum + 1, colnum)):
+                        aroundtotal += 1
+                    if (positionchecker(lines, linenum + 1, colnum + 1)):
+                        aroundtotal += 1
+                    if (positionchecker(lines, linenum + 1, colnum - 1)):
+                        aroundtotal += 1
+                    if (positionchecker(lines, linenum, colnum + 1)):
+                        aroundtotal += 1
+                    if (positionchecker(lines, linenum, colnum - 1)):
+                        aroundtotal += 1
+                    if (positionchecker(lines, linenum - 1, colnum)):
+                        aroundtotal += 1
+                    if (positionchecker(lines, linenum - 1, colnum - 1)):
+                        aroundtotal += 1
+                    if (positionchecker(lines, linenum - 1, colnum + 1)):
+                        aroundtotal += 1
+                if aroundtotal<=3:
+                    total+=1
+                else:
+                    pass
+        print("\n")
+    return total
+
+print(part1(inputting()))
+
