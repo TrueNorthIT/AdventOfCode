3c42c6066cf4a014320180a808a331f0935bcb2d
Alex Radice <aradice@truenorthit.co.uk>
2025-12-04 08:51:41 +0000

    Day 4 part 2

--- 2025/day04/Program.cs
+++ 2025/day04/Program.cs
@@ -1,21 +1,49 @@
-﻿namespace day04
+﻿using System.Text;
+
+namespace day04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines("input.txt");
+            string[] modifiedLines = Pad(lines);
+            int totalRemoved = 0;
+            int removedThisTime = 0;
+            do
+            {
+                var linesBuilder = new List<string>();
+                removedThisTime = modifiedLines.Skip(1).Take(lines.Length).Select((string line, int index) =>
+                {
+                    var modifiedIndex = index + 1;
+                    var lineBuilder = new StringBuilder();
+                    int numToRemove = line.Skip(1).Take(lines[0].Length).Select((char chr, int chrIndex) =>
+                    {
+                        var modifiedChrIndex = chrIndex + 1;
+                        if (chr == '.')
+                        {
+                            lineBuilder.Append(".");
+                            return 0;
+                        }
+                        bool canRemove = $"{modifiedLines[modifiedIndex - 1].Substring(modifiedChrIndex - 1, 3)}{modifiedLines[modifiedIndex].Substring(modifiedChrIndex - 1, 1)}{modifiedLines[modifiedIndex].Substring(modifiedChrIndex + 1, 1)}{modifiedLines[modifiedIndex + 1].Substring(modifiedChrIndex - 1, 3)}".Count(c => c == '.') > 4;
+                        if (canRemove) lineBuilder.Append(".");
+                        else lineBuilder.Append("@");
+                        return canRemove ? 1 : 0;
+                    }).Sum();
+                    linesBuilder.Add(lineBuilder.ToString());
+                    return numToRemove;
+                }).Sum();
+                totalRemoved += removedThisTime;
+                modifiedLines = Pad(linesBuilder.ToArray());
+            } while (removedThisTime > 0);
+            Console.WriteLine(totalRemoved);
+        }
+
+        private static string[] Pad(string[] lines)
+        {
             string padder = new('.', lines[0].Length + 2);
             var modifiedLines = new[] { padder }.Concat(lines.Select(s => $".{s}.")).Concat([padder]).ToArray();
-            Console.WriteLine(modifiedLines.Skip(1).Take(lines.Length).Select((string line, int index) => {
-                var modifiedIndex = index + 1;
-                return line.Skip(1).Take(lines[0].Length).Select((char chr, int chrIndex) =>
-                {
-                    var modifiedChrIndex = chrIndex + 1;
-                    if (chr == '.') return 0;
-                    return $"{modifiedLines[modifiedIndex - 1].Substring(modifiedChrIndex - 1, 3)}{modifiedLines[modifiedIndex].Substring(modifiedChrIndex - 1, 1)}{modifiedLines[modifiedIndex].Substring(modifiedChrIndex + 1, 1)}{modifiedLines[modifiedIndex + 1].Substring(modifiedChrIndex - 1, 3)}".Count(c => c == '.') > 4 ? 1 : 0;
-                }).Sum();
-            }).Sum());
+            return modifiedLines;
         }
     }
 }
