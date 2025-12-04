9cf68379aca4744b0b8b5d0b8fdee041f6abff5f
Alex Radice <aradice@truenorthit.co.uk>
2025-12-04 08:35:02 +0000

    Day 4 part 1

---
+++ 2025/day04/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace day04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            string padder = new('.', lines[0].Length + 2);
+            var modifiedLines = new[] { padder }.Concat(lines.Select(s => $".{s}.")).Concat([padder]).ToArray();
+            Console.WriteLine(modifiedLines.Skip(1).Take(lines.Length).Select((string line, int index) => {
+                var modifiedIndex = index + 1;
+                return line.Skip(1).Take(lines[0].Length).Select((char chr, int chrIndex) =>
+                {
+                    var modifiedChrIndex = chrIndex + 1;
+                    if (chr == '.') return 0;
+                    return $"{modifiedLines[modifiedIndex - 1].Substring(modifiedChrIndex - 1, 3)}{modifiedLines[modifiedIndex].Substring(modifiedChrIndex - 1, 1)}{modifiedLines[modifiedIndex].Substring(modifiedChrIndex + 1, 1)}{modifiedLines[modifiedIndex + 1].Substring(modifiedChrIndex - 1, 3)}".Count(c => c == '.') > 4 ? 1 : 0;
+                }).Sum();
+            }).Sum());
+        }
+    }
+}
