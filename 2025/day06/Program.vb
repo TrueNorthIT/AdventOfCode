6ffbda2d9c8172040e876b012650f76e4758a9bc
Alex Radice <aradice@truenorthit.co.uk>
2025-12-06 07:05:13 +0000

    Day 6 part 2

--- 2025/day06/Program.vb
+++ 2025/day06/Program.vb
@@ -7,16 +7,30 @@ Module Program
 
     Sub Main(args As String())
         Dim lines() As String = File.ReadAllLines("input.txt")
-        Dim nums() = lines.Take(lines.Length - 1).Select(Function(s) Splitter(s)).ToArray()
+        Dim numsBuilderBuilder As List(Of Long()) = New List(Of Long())
+        Dim numsBuilder As List(Of Long) = New List(Of Long)
+        Dim numStr As String
+        For builderIndex = 0 To lines(0).Length - 1
+            numStr = String.Join("", lines.Take(lines.Length - 1).Select(Function(line) line.Substring(builderIndex, 1)))
+            If numStr.Trim() = "" Then
+                numsBuilderBuilder.Add(numsBuilder.ToArray())
+                numsBuilder = New List(Of Long)
+            Else
+                numsBuilder.Add(Long.Parse(numStr))
+            End If
+        Next
+        If numsBuilder.Any() Then
+            numsBuilderBuilder.Add(numsBuilder.ToArray())
+        End If
         Dim ops() As String = lines.TakeLast(1).First().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray()
         Dim total As Long = 0
         For index = 0 To ops.Length - 1
             If ops(index) = "+" Then
-                Dim colSum As Long = nums.Sum(Function(arr) arr(index))
+                Dim colSum As Long = numsBuilderBuilder(index).Sum()
                 Console.WriteLine("Sum {0}", colSum)
                 total = total + colSum
             ElseIf ops(index) = "*" Then
-                Dim colProduct As Long = nums.Skip(1).Aggregate(nums(0)(index), Function(soFar, element) soFar * element(index))
+                Dim colProduct As Long = numsBuilderBuilder(index).Skip(1).Aggregate(numsBuilderBuilder(index)(0), Function(soFar, element) soFar * element)
                 Console.WriteLine("Prd {0}", colProduct)
                 total = total + colProduct
             End If
