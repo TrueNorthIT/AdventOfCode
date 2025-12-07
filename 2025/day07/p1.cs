873ccff7694c2afa9ab83c51c5bb9d4a3521c0ed
Richard Kelsey <richard.kelsey12@gmail.com>
2025-12-07 20:56:00 +0000

    day 7 - aoc man will you let me brute force just one time

---
+++ 2025/day07/p1.cs
@@ -0,0 +1,44 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+//string[] lines = File.ReadAllLines("demo.txt");
+int rows = lines.Length;
+int cols = lines[0].Length;
+
+char[,] grid = new char[rows, cols];
+
+for (int r = 0; r < rows; r++)
+{
+    for (int c = 0; c < cols; c++)
+    {
+        grid[r, c] = lines[r][c];
+    }
+}
+
+long splitCounter = 0;
+
+HashSet<int> beamPositions = [];
+HashSet<int> newBeamPositions = [];
+HashSet<int> removeBeamPositions = [];
+
+for (int i = 0; i < grid.GetLength(0); i++)
+{
+    for (int j = 0; j < grid.GetLength(1); j++)
+    {
+        if (grid[i, j] == 'S')
+        {
+            beamPositions.Add(j);
+        }
+        else if (grid[i, j] == '^')
+        {
+            if (beamPositions.Contains(j))  splitCounter++;
+            removeBeamPositions.Add(j);
+            newBeamPositions.Add(j + 1);
+            newBeamPositions.Add(j - 1);
+        }
+    }
+    beamPositions.UnionWith(newBeamPositions);
+    beamPositions.ExceptWith(removeBeamPositions);
+    removeBeamPositions.Clear();
+    newBeamPositions.Clear();
+}
+
+Console.WriteLine($"Split count: {splitCounter}");
