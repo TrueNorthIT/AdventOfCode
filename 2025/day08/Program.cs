cae53079d3bea1f89e950a3b183b50d011e8fd6f
Alex Radice <aradice@truenorthit.co.uk>
2025-12-08 13:58:52 +0000

    Day 8 part 2

--- 2025/day08/Program.cs
+++ 2025/day08/Program.cs
@@ -5,25 +5,51 @@
         static void Main(string[] args)
         {
             var vectors = File.ReadAllLines("input.txt").Select(s => { var parts = s.Split(","); return new Vector { X = int.Parse(parts[0]), Y = int.Parse(parts[1]), Z = int.Parse(parts[2]) }; }).ToArray();
-            var spacings = vectors.Take(vectors.Length - 1).SelectMany((vector, index) => {
-                return vectors.Skip(index + 1).Select((vector2, index2) => new Spacing { Idx1 = index, Idx2 = index2 + index + 1, Distance = vector.DistanceFrom(vector2) });
+            var spacings = vectors.Take(vectors.Length - 1).SelectMany((vector, index) =>
+            {
+                return vectors.Skip(index + 1).Select((vector2, index2) => new Spacing { Idx1 = index, Idx2 = index2 + index + 1, Distance = vector.DistanceSquared(vector2) });
             }).OrderBy(sp => sp.Distance).ToArray();
             var connections = new Dictionary<int, List<int>>();
+            //Part1(vectors, spacings, connections);
+            Part2(vectors, spacings, connections);
+        }
+
+        private static void Part2(Vector[] vectors, Spacing[] spacings, Dictionary<int, List<int>> connections)
+        {
+            int spacingIndex = 0;
+            do
+            {
+                AddConnection(connections, spacings[spacingIndex]);
+                spacingIndex++;
+            } while (!AllConnected(connections, vectors.Length));
+            Console.WriteLine(vectors[spacings[spacingIndex - 1].Idx1].X * vectors[spacings[spacingIndex - 1].Idx2].X);
+        }
+
+        private static bool AllConnected(Dictionary<int, List<int>> connections, int vectorsCount)
+        {
+            var accountFor = new HashSet<int>();
+            var circuitSizes = new List<int>();
+            for (var i = 0; i < vectorsCount; i++)
+            {
+                if (!accountFor.Contains(i))
+                {
+                    var circuitElements = new HashSet<int>(i);
+                    accountFor.Add(i);
+                    AddElements(connections, circuitElements, i);
+                    foreach (var element in circuitElements) accountFor.Add(element);
+                    circuitSizes.Add(circuitElements.Count);
+                    Console.WriteLine(string.Join(", ", circuitElements));
+                    return circuitElements.Count == vectorsCount;
+                }
+            }
+            return false;
+        }
+
+        private static void Part1(Vector[] vectors, Spacing[] spacings, Dictionary<int, List<int>> connections)
+        {
             foreach (var spacing in spacings.Take(1000))
             {
-                Console.WriteLine("Distance {0} connecting {1} and {2}", spacing.Distance, spacing.Idx1, spacing.Idx2);
-                if (!connections.TryGetValue(spacing.Idx1, out var connectedTo))
-                {
-                    connectedTo = new List<int>();
-                }
-                connectedTo.Add(spacing.Idx2);
-                connections[spacing.Idx1] = connectedTo;
-                if (!connections.TryGetValue(spacing.Idx2, out var connected2To))
-                {
-                    connected2To = new List<int>();
-                }
-                connected2To.Add(spacing.Idx1);
-                connections[spacing.Idx2] = connected2To;
+                AddConnection(connections, spacing);
             }
             var count = 0;
             var accountFor = new HashSet<int>();
@@ -43,6 +69,23 @@
             }
             Console.WriteLine("{0} circuits", count);
             Console.WriteLine(circuitSizes.OrderByDescending(i => i).Take(3).Aggregate((i, j) => i * j));
+        }
+
+        private static void AddConnection(Dictionary<int, List<int>> connections, Spacing spacing)
+        {
+            Console.WriteLine("Distance {0} connecting {1} and {2}", spacing.Distance, spacing.Idx1, spacing.Idx2);
+            if (!connections.TryGetValue(spacing.Idx1, out var connectedTo))
+            {
+                connectedTo = new List<int>();
+            }
+            connectedTo.Add(spacing.Idx2);
+            connections[spacing.Idx1] = connectedTo;
+            if (!connections.TryGetValue(spacing.Idx2, out var connected2To))
+            {
+                connected2To = new List<int>();
+            }
+            connected2To.Add(spacing.Idx1);
+            connections[spacing.Idx2] = connected2To;
         }
 
         static void AddElements(Dictionary<int, List<int>> connections, HashSet<int> thisSet, int i)
@@ -72,9 +115,9 @@
             public double Y { get; set; }
             public double Z { get; set; }
 
-            public double DistanceFrom(Vector other)
+            public double DistanceSquared(Vector other)
             {
-                return Math.Sqrt(Math.Pow(this.X - other.X, 2) + Math.Pow(this.Y - other.Y, 2) + Math.Pow(this.Z - other.Z, 2));
+                return Math.Pow(this.X - other.X, 2) + Math.Pow(this.Y - other.Y, 2) + Math.Pow(this.Z - other.Z, 2);
             }
         }
 
