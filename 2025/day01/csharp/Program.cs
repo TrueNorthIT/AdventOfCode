bf175774a1f2e9601faaeb4433dda44474485dc7
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-09 00:48:19 +0000

    golfing

--- 2025/day01/csharp/Program.cs
+++ 2025/day01/csharp/Program.cs
@@ -1,6 +1,7 @@
-﻿int c=50,a=0,b=0;
+﻿int i,c=50,a=0,b=0;
 foreach(var s in File.ReadLines("i")){
- for(int i=0;i<int.Parse(s[1..]);(i,c)=(i+1,(c+(s[0]=='R'?1:-1))%100))
+ for (i=0;i++<int.Parse(s[1..]);c=(c+(s[0]<'R'?-1:1))%100)
   if(c==0)b++;
  if(c==0)a++;
-}throw new((a,b)+"");
+}
+throw new((a,b)+"");
