e370b235e113ca40f12aa8015f31550cea87dc8e
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-09 17:55:41 +0000

    tidied day 9

--- 2025/day09/Program.cs
+++ 2025/day09/Program.cs
@@ -1,65 +1,48 @@
-﻿using System;
+﻿var points = File.ReadAllLines("input.txt")
+                .Select(line => line.Split(',').Select(double.Parse) switch { 
+                    var sp => (x: sp.First(), y: sp.Skip(1).First()) })
+            .ToArray();
 
-List<(double x, double y)> points = File.ReadAllLines("input.txt").Select(line => line.Split(','))
-            .Select(arr => (double.Parse(arr[0]), double.Parse(arr[1])))
-            .ToList();
-
-var p1ans = (from p1 in points
- from p2 in points
- select (p1,p2,(1 + Math.Abs(p2.Item1 - p1.Item1)) * (1 + Math.Abs(p2.Item2 - p1.Item2)))).ToList();
-
-Console.WriteLine(p1ans.MaxBy(tp => tp.Item3).Item3);
-
-
-var rectangles = from c1 in points
-           from c2 in points
-           select (c1, c2);
-
-//we have 4 sides of a rectangle
-//  (p1.x,p1.y) -> (p1.x,p2.y)
-//  (p1.x,p2.y) -> (p2.x,p2.y)
-//  (p2.x,p2.y) -> (p2.x,p1.y)
-//  (p2.x,p1.y) -> (p1.x,p1.y)
+var rectangles = from c1 in points from c2 in points where c1.x < c2.x || c1.y < c2.y select (c1, c2);
+Console.WriteLine($"Part 1: {rectangles.Max(tp => (1 + Math.Abs(tp.c2.x - tp.c1.x)) * (1 + Math.Abs(tp.c2.y - tp.c1.y)))}");
 
 //rectangle is entirely inside if no side of polygon crosses any side of the rectangle
+//however we must ensure we are checking against the outside of the polygon
+//so first step is to walk the outside of the polygon storing its boundary
 
+//find the TL corner to start at
 var start = points.Index().OrderBy(p => p.Item.y).ThenBy(p => p.Item.x).First();
-//we know start point is a TL corner
-var prevP = (x: start.Item.x - 0.5, y: start.Item.y - 0.5);
+//and assume we are going CLOCKWISE, 0check this for real input!
 var prevTurnRight = true;
-//assume we are going CLOCKWISE, and we turned right prev, check this for real input!
 var polygonBoundary = new List<(double x, double y)> {
-    prevP
+     (x: start.Item.x - 0.5, y: start.Item.y - 0.5)
 };
-for (int c = 1; c < points.Count; c++)
+
+for (int c = 1; c < points.Length; c++)
 {
-    var pi = points[(c + start.Index - 1) % points.Count];
-    var ci = points[(c + start.Index) % points.Count];
-    var ni = points[(c + start.Index + 1) % points.Count];
+    var pi = points[(c + start.Index - 1) % points.Length];
+    var ci = points[(c + start.Index) % points.Length];
+    var ni = points[(c + start.Index + 1) % points.Length];
     var currVector = (x: ci.x - pi.x, y: ci.y - pi.y);
     var nextVector = (x: ni.x - ci.x, y: ni.y - ci.y);
 
     bool nextTurnRight = crossproduct(currVector, (0,0), nextVector) < 0;
 
+    //the length of our curent side depends if we are going R-R (+1), L-R / R-L (+0), L-L (-1)
     int delta = nextTurnRight && prevTurnRight 
-                    ? 1 : nextTurnRight ^ prevTurnRight 
-                          ? 0 : -1;
+                ? 1 :   nextTurnRight ^ prevTurnRight 
+                        ? 0 : -1;
 
-    //if next turn is a right turn, we want to move one more than our vector
-    //otherwise just move the length of our vector
     if (currVector.x == 0)
-    {
         currVector = (0, currVector.y + delta * currVector.y / Math.Abs(currVector.y));
-    }
     else
-    {
         currVector = (currVector.x + delta * currVector.x / Math.Abs(currVector.x), 0);
-    }
 
     prevTurnRight = nextTurnRight;
-    prevP = (prevP.x + currVector.x, prevP.y + currVector.y);
-    polygonBoundary.Add(prevP);
+    polygonBoundary.Add((polygonBoundary.Last().x + currVector.x, polygonBoundary.Last().y + currVector.y));
 }
+
+Console.WriteLine("Outline found");
 
 var found = new List<((double x, double y) c1, (double x, double y) c2, double )>();
 foreach (var rectangle in rectangles)
@@ -75,44 +58,29 @@ foreach (var rectangle in rectangles)
     var s4 = (maxx, miny);
 
     var sides = new [] { (s1,s2), (s2,s3), (s3,s4), (s4,s1) };
-
     foreach (var side in sides)
     {
         for (int p = 0; p < polygonBoundary.Count; p++)
         {
             var p1 = polygonBoundary[p];
-            var p2 = polygonBoundary[(p + 1) % points.Count];
+            var p2 = polygonBoundary[(p + 1) % points.Length];
 
             if (crosses(side.Item1, side.Item2, p1, p2))
             {
                 valid = false;
-                break;
+                goto skip;
             }
         }
     }
-    if (valid)
-    {
-        found.Add((rectangle.c1, rectangle.c2, (1 + Math.Abs(rectangle.c2.x - rectangle.c1.x)) * (1 + Math.Abs(rectangle.c2.y - rectangle.c1.y))));
-    }
+    found.Add((rectangle.c1, rectangle.c2, (1 + Math.Abs(rectangle.c2.x - rectangle.c1.x)) * (1 + Math.Abs(rectangle.c2.y - rectangle.c1.y))));
+skip:;
 }
 
 var p2ans = found.MaxBy(tp => tp.Item3);
 Console.WriteLine(p2ans);
 
-
-
 bool crosses((double x, double y) p1, (double x, double y) p2, (double x, double y) l1, (double x, double y) l2)
 {
-    //we have a line from l1 to l2
-    //is p1 on one side of the line, and p2 on the other
-    //all lines are either vertical or horizontal
-    if (l1.x == l2.x && p1.x == p2.x)
-        //both vertical, cannot cross
-        return false;
-    if (l1.y == l2.y && p1.y == p2.y)
-        //both horizontal, cannot cross
-        return false;
-
     if (l1.x == l2.x)
     {
         //polygon side is vertical, so two points are on a horizontal
@@ -122,7 +90,7 @@ bool crosses((double x, double y) p1, (double x, double y) p2, (double x, double
             //the horizontal points cross
             //is the vertical range within the line (incl.)
             var sortedy = new[] { l1.y, l2.y }.OrderBy(x => x).ToArray();
-            if (p1.y >= sortedy[0] && p1.y <= sortedy[1])
+            if (p1.y > sortedy[0] && p1.y < sortedy[1])
             {
                 return true;
             }
@@ -142,7 +110,6 @@ bool crosses((double x, double y) p1, (double x, double y) p2, (double x, double
         }
         return false;
     }
-
     throw new();
 }
 double crossproduct((double x, double y) p, (double x, double y) l1, (double x, double y) l2)
