9d841dbc72290309635832e23fa442ee514cc4e7
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-09 00:07:24 +0000

    tidy up

--- 2025/day07/day7_fsharp/Program.fs
+++ 2025/day07/day7_fsharp/Program.fs
@@ -1,20 +1,26 @@
 ﻿let lines = System.IO.File.ReadAllLines("input.txt")
-let (totals : int64 array2d) = Array2D.zeroCreate lines.Length lines[0].Length
+let R, C = lines.Length, lines[0].Length
+let mutable p1 = 0L
+let (p2Totals : int64 array array) = [| for _ in 1 .. R -> [| for _ in 1 .. C -> 0L|]|]
 
 for c = 0 to lines[0].Length - 1 do
-    Array2D.set totals 0 c (if lines[0][c] = 'S' then 1 else 0)
+    p2Totals[0][c] <- if lines[0][c] = 'S' then 1 else 0
+    p2Totals[0][c] <- if lines[0][c] = 'S' then 1 else 0
 
 for r = 1 to lines.Length - 1 do
     for c = 0 to lines[0].Length - 1 do
-        let v = match lines[r][c] with
-                | '.' -> if c - 1 > 0 && lines[r][c-1] = '^' then totals[r-1,c-1] else 0
-                        + if c + 1 < lines[0].Length && lines[r][c+1] = '^' then totals[r-1,c+1] else 0
-                        + totals[r-1,c]
-                | _ -> 0;
-        Array2D.set totals r c v
+        p2Totals[r][c] =
+            match lines[r][c] with
+                | '.' -> if c - 1 > 0 && lines[r][c-1] = '^' then p2Totals[r-1][c-1] else 0
+                        + if c + 1 < lines[0].Length && lines[r][c+1] = '^' then p2Totals[r-1][c+1] else 0
+                        + p2Totals[r-1][c]
+                | _ -> 0
+        |> ignore;
+        p1 <- p1 + if p2Totals[r-1][c] > 0 && lines[r][c] = '^' then 1L else 0
 
 let mutable p2 = 0L
 for c = 0 to lines[0].Length - 1 do
-    p2 <- p2 + totals[lines.Length - 1,c]
+    p2 <- p2 + p2Totals[lines.Length - 1][c]
 
+printfn $"Part 1: {p1}";
 printfn $"Part 2: {p2}";
