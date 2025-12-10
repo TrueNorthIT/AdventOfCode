ef25c06dddfe768f0c342d403609b2edb8688b31
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-10 10:18:09 +0000

    day 10, p1

--- 2025/day09/Program.cs
+++ 2025/day09/Program.cs
@@ -1,44 +1,59 @@
 ﻿using System.Diagnostics;
+using Point = (double x, double y);
 
-Stopwatch watch = Stopwatch.StartNew();
-
+var watch = Stopwatch.StartNew();
 var points = File.ReadAllLines("input.txt")
                 .Select(line => line.Split(',').Select(double.Parse) switch { 
                     var sp => (x: sp.First(), y: sp.Skip(1).First()) })
             .ToArray();
 
-double size((double x, double y) p1, (double x, double y) p2) => (1 + Math.Abs(p2.x - p1.x)) * (1 + Math.Abs(p2.y - p1.y));
+var rectangles = from c1 in points from c2 in points 
+                 where c1.x < c2.x || c1.y < c2.y 
+                 select (sides: sides(c1,c2), size: size(c1,c2));
 
-var rectangles = from c1 in points from c2 in points where c1.x < c2.x || c1.y < c2.y select (c1, c2, size: size(c1,c2));
-Console.WriteLine($"Part 1: {rectangles.Max(tp => (1 + Math.Abs(tp.c2.x - tp.c1.x)) * (1 + Math.Abs(tp.c2.y - tp.c1.y)))} in {watch.ElapsedMilliseconds}ms");
+Console.WriteLine($"Part 1: {rectangles.Max(tp => tp.size)} in {watch.ElapsedMilliseconds}ms");
+
 watch.Restart();
 
-//rectangle is entirely inside if no side of polygon crosses any side of the rectangle
-//however we must ensure we are checking against the outside of the polygon
-//so first step is to walk the outside of the polygon storing its boundary
+//we treat the tiles as being centred on the integer coordinates as specified
+//the outside edge is therefore +-0.5 from there
+//if we treat our test rectangles as being on the integer cordinates
+//then our edge cases go away and we will always have a line crossing when
+//going in or out of the grid
+//so first step is to walk the outside of the polygon checking which way we are turning
+//and storing its boundary offset by +-0.5 as appropriate
+//turns are always +-90
+//  ___
+//  ###  two right turns, edge is +1 = 3
+//  # #
+//
+//  __#
+//  ###  right then left turn edge is +0 = 2
+//  #
+//
+//  #_#  two left turns, edge is -1 = 1
+//  ###
 
-//find the Top Left corner to start at
-var start = points.Index().OrderBy(p => p.Item.y).ThenBy(p => p.Item.x).First();
-//and assume we are going CLOCKWISE, 0check this for real input!
+
+//find the top left point and start CLOCKWISE, check if this is true for real input!
+(var startIndex, var startPoint) = points.Index().OrderBy(p => p.Item.y).ThenBy(p => p.Item.x).First();
+//if we are the top most point we must have just made a right turn if we are going clockwise
 var prevTurnRight = true;
-var polygonBoundary = new List<(double x, double y)> {
-     (x: start.Item.x - 0.5, y: start.Item.y - 0.5)
-};
 
+List<Point> boundary = [(x: startPoint.x - 0.5, y: startPoint.y - 0.5)];
 for (int c = 1; c < points.Length; c++)
 {
-    var pi = points[(c + start.Index - 1) % points.Length];
-    var ci = points[(c + start.Index) % points.Length];
-    var ni = points[(c + start.Index + 1) % points.Length];
+    var pi = points[(c + startIndex - 1) % points.Length];
+    var ci = points[(c + startIndex) % points.Length];
+    var ni = points[(c + startIndex + 1) % points.Length];
     var currVector = (x: ci.x - pi.x, y: ci.y - pi.y);
     var nextVector = (x: ni.x - ci.x, y: ni.y - ci.y);
 
-    bool nextTurnRight = crossproduct(currVector, (0,0), nextVector) < 0;
+    bool nextTurnRight = crossproduct(currVector, nextVector) < 0;
 
     //the length of our curent side depends if we are going R-R (+1), L-R / R-L (+0), L-L (-1)
     int delta = nextTurnRight && prevTurnRight 
-                ? 1 :   nextTurnRight ^ prevTurnRight 
-                        ? 0 : -1;
+                ? 1 : nextTurnRight ^ prevTurnRight ? 0 : -1;
 
     if (currVector.x == 0)
         currVector = (0, currVector.y + delta * currVector.y / Math.Abs(currVector.y));
@@ -46,72 +61,42 @@ for (int c = 1; c < points.Length; c++)
         currVector = (currVector.x + delta * currVector.x / Math.Abs(currVector.x), 0);
 
     prevTurnRight = nextTurnRight;
-    polygonBoundary.Add((polygonBoundary.Last().x + currVector.x, polygonBoundary.Last().y + currVector.y));
+    boundary.Add((boundary.Last().x + currVector.x, boundary.Last().y + currVector.y));
 }
 
-foreach (var rectangle in rectangles.OrderByDescending(tp => tp.size))
+foreach (var rect in rectangles.OrderByDescending(tp => tp.size))
 {    
-    var minx = Math.Min(rectangle.c1.x, rectangle.c2.x);
-    var maxx = Math.Max(rectangle.c1.x, rectangle.c2.x);
-    var miny = Math.Min(rectangle.c1.y, rectangle.c2.y);
-    var maxy = Math.Max(rectangle.c1.y, rectangle.c2.y);
-    var s1 = (minx, miny);
-    var s2 = (minx, maxy);
-    var s3 = (maxx, maxy);
-    var s4 = (maxx, miny);
-
-    var sides = new [] { (s1,s2), (s2,s3), (s3,s4), (s4,s1) };
-    foreach (var side in sides)
+    foreach (var side in rect.sides)
     {
-        for (int p = 0; p < polygonBoundary.Count; p++)
+        for (int p = 0; p < boundary.Count; p++)
         {
-            var p1 = polygonBoundary[p];
-            var p2 = polygonBoundary[(p + 1) % points.Length];
-
-            if (crosses(side.Item1, side.Item2, p1, p2))
+            if (crosses(side.p1, side.p2, boundary[p], boundary[(p + 1) % points.Length]))
                 goto skip;
         }
     }
-    Console.WriteLine($"Part 2: {rectangle.size} in {watch.ElapsedMilliseconds}ms");
+    Console.WriteLine($"Part 2: {rect.size} in {watch.ElapsedMilliseconds}ms");
     return;
 skip:;
 }
 
-bool crosses((double x, double y) p1, (double x, double y) p2, (double x, double y) l1, (double x, double y) l2)
-{
-    if (l1.x == l2.x)
-    {
-        if ((p1.x <= p2.x && p1.x < l1.x && p2.x > l2.x)
-            || (p2.x < l1.x && p1.x > l2.x))
-        {
-            //the horizontal points cross
-            //is the vertical range within the line (incl.)
-            if ((l1.y <= l2.y && p1.y > l1.y && p1.y < l2.y)
-                || (p1.y > l2.y && p1.y < l1.y))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-    if (l1.y == l2.y)
-    {
-        if ((p1.y < p2.y && p1.y < l1.y && p2.y > l2.y)
-            || (p2.y < l1.y && p1.y > l2.y) )
-        {
-            if ((l1.x < l2.x && p1.x >= l1.x && p1.x <= l2.x)
-                || p1.x >= l2.x && p1.x <= l1.x)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-    throw new();
-}
-double crossproduct((double x, double y) p, (double x, double y) l1, (double x, double y) l2)
-{
-    var fx = l2.x - l1.x;
-    var fy = l2.y - l1.y;
-    return fx * (p.y - l1.y) - fy * (p.x - l1.x);
-}
+(Point p1, Point p2)[] sides(Point p1, Point p2) => [
+    ((p1.x, p1.y), (p1.x, p2.y)),
+    ((p1.x, p2.y), (p2.x, p2.y)),
+    ((p2.x, p2.y), (p2.x, p1.y)),
+    ((p2.x, p1.y), (p1.x, p1.y)),
+];
+
+double size(Point p1, Point p2) => (1 + Math.Abs(p2.x - p1.x)) * (1 + Math.Abs(p2.y - p1.y));
+
+bool crosses(Point p1, Point p2, Point l1, Point l2) =>
+    l1.x == l2.x 
+        ?   ((p1.x <= p2.x && p1.x < l1.x && p2.x > l2.x)
+                || (p2.x < l1.x && p1.x > l2.x))
+            && ((l1.y <= l2.y && p1.y > l1.y && p1.y < l2.y)
+                || (p1.y > l2.y && p1.y < l1.y))        
+        :   ((p1.y < p2.y && p1.y < l1.y && p2.y > l2.y)
+                || (p2.y < l1.y && p1.y > l2.y))
+            && ((l1.x < l2.x && p1.x >= l1.x && p1.x <= l2.x)
+                || p1.x >= l2.x && p1.x <= l1.x);
+
+double crossproduct(Point p, Point l2) => l2.x * p.y - l2.y * p.x;
---
+++ 2025/day10/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+var input = File.ReadAllLines("input.txt").Select(line => line.Split(' ') switch
+{
+    var arr =>
+        (arr.First(), arr.Skip(1).Take(arr.Length - 2).ToArray(), arr.Skip(arr.Length - 1).Single())
+});
+
+var lights = input.Select(tp => tp.Item1[1..^1]).ToArray();
+
+var buttons = input.Select(tp => tp.Item2.Select(
+        button => button[1..^1].Split(',')
+            .Select(int.Parse).ToArray()).ToArray()).ToArray();
+
+var joltages = input.Select(tp => tp.Item3[1..^1].Split(',').Select(int.Parse).ToArray()).ToArray();
+
+
+long solve(int index)
+{
+    Dictionary<string, long> cache = new();
+    Queue<(string state, long presses)> queue = new();
+    var first = (empty(lights[index].Length), 0);
+    queue.Enqueue(first);
+    
+    while (queue.Count > 0)
+    {
+        (var state, var presses) = queue.Dequeue();
+        if (cache.ContainsKey(state))
+        {
+            continue;
+        }
+        cache[state] = presses;
+        if (state == lights[index])
+        {
+            return presses;
+        }
+        long best = long.MaxValue;
+        foreach (var button in buttons[index])
+        {
+            var newLights = press(state, button);
+            queue.Enqueue((newLights, presses + 1));            
+        }
+    }
+    throw new();
+}
+
+var fewest = Enumerable.Range(0, lights.Length).Select(i => solve(i)).ToArray();
+var p1 = fewest.Sum();
+Console.WriteLine(p1);
+
+string empty(int length) => new string(Enumerable.Repeat('.', length).ToArray());
+string press(string before, int[] button)
+{
+    var res = new StringBuilder(before);
+    foreach(var toggle in button)
+    {
+        res[toggle] = res[toggle] == '#' ? '.' : '#';
+    }
+    return res.ToString();
+}
+
+
+Console.WriteLine();
+record State(string lights, long presses);
