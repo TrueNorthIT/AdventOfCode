73c9f1eb8f66985195c6282357c82b3aa8bbb8bd
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-11 11:51:19 +0000

    compactifified

--- 2025/day11/Program.cs
+++ 2025/day11/Program.cs
@@ -1,23 +1,8 @@
 ﻿var graph = File.ReadAllLines("input.txt").Select(line => line.Split(": ")).ToDictionary(arr => arr[0], arr => arr[1].Split(' ').ToArray());
-
 var cache = new Dictionary<(string node, bool dac, bool fft), long>();
 Console.WriteLine(ways(("svr", false, false)));
-
-long ways((string node, bool dac, bool fft) key)
-{
-    if (key.node == "out")
-        return key.dac && key.fft ? 1 : 0;
-
-    if (cache.ContainsKey(key))
-        return cache[key];
-
-    long total = 0;
-    foreach (var next in graph[key.node])
-    {
-        var newKey = (next, key.dac || next == "dac", key.fft || next == "fft");
-        total += ways(newKey);
-    }
-
-    cache[key] = total;
-    return total;
-}
+long ways((string node, bool dac, bool fft) key) => key.node switch {
+    "out" => key.dac && key.fft ? 1 : 0,
+    _ when cache.ContainsKey(key) => cache[key],
+    _ => cache[key] = graph[key.node].Sum(next => ways((next, key.dac || next == "dac", key.fft || next == "fft"))),
+};
