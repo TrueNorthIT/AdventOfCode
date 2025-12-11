77c3c99f671dc5c90f3d45321d677f51c8bc4a94
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-11 13:46:59 +0000

    both parts together

--- 2025/day11/Program.cs
+++ 2025/day11/Program.cs
@@ -1,8 +1,15 @@
-﻿var graph = File.ReadAllLines("input.txt").Select(line => line.Split(": ")).ToDictionary(arr => arr[0], arr => arr[1].Split(' ').ToArray());
-var cache = new Dictionary<(string node, bool dac, bool fft), long>();
-Console.WriteLine(ways(("svr", false, false)));
-long ways((string node, bool dac, bool fft) key) => key.node switch {
-    "out" => key.dac && key.fft ? 1 : 0,
-    _ when cache.ContainsKey(key) => cache[key],
-    _ => cache[key] = graph[key.node].Sum(next => ways((next, key.dac || next == "dac", key.fft || next == "fft"))),
-};
+﻿using System.Collections.Concurrent;
+using State = (string node, bool dac, bool fft);
+
+var graph = File.ReadAllLines("input.txt").Select(line => line.Split(": "))
+    .ToDictionary(arr => arr[0], arr => arr[1].Split(' ').ToArray());
+
+Console.WriteLine(
+    (ways(new (), ("you", true, true)), 
+     ways(new (), ("svr", false, false))));
+
+long ways(ConcurrentDictionary<State, long> cache, State key) => key.node == "out"
+    ? (key.dac && key.fft ? 1 : 0)
+    : cache.GetOrAdd(key,
+        _ => graph[key.node].Sum(next =>
+            ways(cache, (next, key.dac || next == "dac", key.fft || next == "fft"))));
