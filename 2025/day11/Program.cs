8be54892f3840a1b370fdbf0f9a9b5b3d346dabe
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-11 11:37:23 +0000

    day 11 p1

---
+++ 2025/day11/Program.cs
@@ -0,0 +1,18 @@
+﻿var graph = File.ReadAllLines("input.txt").Select(line => line.Split(": ")).ToDictionary(arr => arr[0], arr => arr[1].Split(' ').ToArray());
+
+var cache = new Dictionary<string, long>();
+Console.WriteLine(ways("you"));
+
+long ways(string node)
+{
+    if (node == "out")
+        return 1;
+
+    if (cache.ContainsKey(node))
+        return cache[node];
+
+    long total = graph[node].Sum(next => ways(next));
+
+    cache[node] = total;
+    return total;
+}
