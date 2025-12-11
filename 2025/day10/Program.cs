e60002ef38120a0a6e19bba9604b0194018933fb
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-11 00:28:06 +0000

    day 10, p2

--- 2025/day10/Program.cs
+++ 2025/day10/Program.cs
@@ -1,211 +1,145 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
 
-var input = File.ReadAllLines("input.txt").Select(line => line.Split(' ') switch
-{
-    var arr =>
-        (arr.First(), arr.Skip(1).Take(arr.Length - 2).ToArray(), arr.Skip(arr.Length - 1).Single())
+var input = File.ReadAllLines("input.txt").Select(line => line.Split(' ') switch {
+    var arr => (    arr.First(), 
+                    arr[1..^1].ToArray(),
+                arr.Skip(arr.Length - 1).Single())
 });
 
 var lights = input.Select(tp => tp.Item1[1..^1]).ToArray();
-
 var buttons = input.Select(tp => tp.Item2.Select(
         button => button[1..^1].Split(',')
             .Select(int.Parse).ToArray()).ToArray()).ToArray();
-
 var joltages = input.Select(tp => tp.Item3[1..^1].Split(',').Select(int.Parse).ToArray()).ToArray();
 
-var fewest = Enumerable.Range(0, lights.Length).Select(solve).ToArray();
-var p1 = fewest.Sum();
-Console.WriteLine(p1);
+var stopwatch = Stopwatch.StartNew();
+var p1 = Enumerable.Range(0, lights.Length).Select(solveP1).Sum();
+Console.WriteLine($"Part 1: {p1} in {stopwatch.ElapsedMilliseconds}ms");
+stopwatch.Restart();
+var p2 = Enumerable.Range(0, lights.Length).Select(reduceAndBackSubstitute).Sum();
+Console.WriteLine($"Part 2: {p2} in {stopwatch.ElapsedMilliseconds}ms");
 
-//solvep2(131);
-fewest = Enumerable.Range(0, lights.Length).Select(solvep2).ToArray();
-var p2 = fewest.Sum();
-Console.WriteLine(p2);
-
-long solvep2(int index)
+long reduceAndBackSubstitute(int index)
 {
-    int R = joltages[index].Length;
-    int C = buttons[index].Length;
-    double[][] arr = new double[R][];
-    for (int r = 0; r < joltages[index].Length; r++)
+    (int R, int C) = (joltages[index].Length, buttons[index].Length);
+
+    //create our matrix, Ax = b
+    int[][] A = new int[R][];
+    for (int r = 0; r < R; r++)
     {
-        arr[r] = new double[C];
+        //make an extra col. for the b values
+        A[r] = new int[C+1];
+        A[r][C] = joltages[index][r];
     }
     foreach (var button in buttons[index].Index())
-    {
         foreach (var toggle in button.Item)
-        {
-            arr[toggle][button.Index] = 1;
-        }
-    }
+            A[toggle][button.Index] = 1;
 
-    //print(arr);
-    var b = joltages[index].Select(i => (double) i).ToArray();
     //we are going to reduce out matrix as much as possible
     for (int r = 0; r < R && r < C; r++)
     {
-        //first row, we need to reduce the first column if possible
+        //for row i, we need to reduce column i if required
         int pivot = -1;
         for (int p = r; p < R; p++)
-        {
-            if (arr[p][r] != 0)
-            {
+            if (A[p][r] != 0)
                 pivot = p;
-                break;
-            }
-        }
-        if (pivot == -1)
-        {
-            //everything is 0 here so just continue to the next row
-            continue;
-        }
-        else if (pivot == r)
-        {
-            //nothing to do
-        }
-        else
-        {
-            //swap our pivot row into place
-            var tmpA = arr[pivot];
-            arr[pivot] = arr[r];
-            arr[r] = tmpA;
-            //and swap our values
-            var tmpV = b[pivot];
-            b[pivot] = b[r];
-            b[r] = tmpV;
-        }
 
-        //if we got here we can reduce all the remaining rows
-        for (int p = r+1; p < R; p++)
+        if (pivot == -1)
+            //column is all zeroes already so we are done
+            continue;
+
+        //swap our pivot row (and values)
+        swap(A, pivot, r);
+        //and reduce all the rows below our pivot row
+        for (int p = r + 1; p < R; p++)
         {
-            if (arr[p][r] != 0)
+            if (A[p][r] == 0)
+                continue;
+
+            //make sure we keep our maths to integers!
+            (var deltaNom, var deltaDen) = (A[p][r], A[r][r]);
+            for(int c = 0; c < C; c++)
             {
-                var delta = arr[p][r] / arr[r][r];
-                for(int c = 0; c < C; c++)
-                {
-                    arr[p][c] -= delta * arr[r][c];
-                }
-                b[p] -= delta * b[r];
+                A[p][c] = deltaDen * A[p][c] - A[r][c] * deltaNom;
             }
+            A[p][C] = A[p][C] * deltaDen - A[r][C] * deltaNom;
         }
         //we have fully reduced from r+1 to the end
-    }
-
-    //print(arr);
-    //Console.WriteLine($"b: {String.Join(", ", b)}");
-
+    }   
     //now we have to start substituting in
     //and if we get stuck start brute forcing the parameters
-
-    return backSub(index, arr, b);
+    return backSub(index, R, C, A);
 }
 
-int backSub(int index, double[][] arr, double[] b)
+int backSub(int index, int R, int C, int[][] A)
 {
-    if (index == 131)
-    {
-        //62277040675 - TOO HIGH!
-        //must be able to do this one lower
-        //return 275;
-    }
+    var maximums = buttons[index].Select(toggles => toggles.Min(bi => joltages[index][bi])).ToArray();
 
     int best = int.MaxValue;
-    HashSet<string> cache = new HashSet<string>();
-    Stack<(int row, Dictionary<int, int> pressed)> queue = new ();
+    Stack<(int row, Dictionary<int, int> pressed)> stack = new ();
 
-    int bestRow = int.MaxValue;
-    //start on last non 0 row
-    //var si = arr.Index().Last(arr => arr.Item.Any(d => d != 0)).Index;
-    queue.Push((arr.Length - 1, new Dictionary<int, int>()));
-    while (queue.Any())
+    stack.Push((R - 1, new Dictionary<int, int>()));
+    while (stack.Any())
     {
-        (var row, var pressed) = queue.Pop();
-        if (row < 0) continue;
+        (var row, var pressed) = stack.Pop();
 
-        //var cacheKey = $"R:{row}-" + String.Join(",", Enumerable.Range(0, buttons[index].Length).Select(i => pressed.ContainsKey(i) ? pressed[i] : -1).ToArray());
-        //if (cache.Contains(cacheKey))
-        //    continue;
-
-        //cache.Add(cacheKey);
-        //if (row == bestRow)
-        //    Console.WriteLine(cacheKey);
-
-        if (row < bestRow)
+        if (row < 0)
         {
-            bestRow = row;
-            //Console.WriteLine($"Reached row {row}");
-            //Console.WriteLine(cacheKey);
-        }
-
-        
-        //we have a row of the form x0 + x1 + x2 ... = br
-        //we need to sub in any values we know and then start brute forcing the rest
-        bool allKnown = true;
-        double checkSum = b[row];
-        for (int c = 0; c < arr[0].Length; c++)
-        {
-            if (arr[row][c] != 0)
-            {
-                if (pressed.ContainsKey(c))
-                {
-                    checkSum-= arr[row][c] * pressed[c];                    
-                }
-                else
-                {
-                    allKnown = false;
-                }
-            }
-        }
-        //if the whole row is 0, we have two options
-        //1) b[row] = 0, this row has passed, moved onto the next
-        //2) b[row] != 0, this was a failed substitution so stop
-
-        if (allKnown)
-        {
-            if (checkSum == 0)
-            {
-                if (pressed.Count == buttons[index].Length)
-                {
-                    //we are done
-                    var ans = pressed.Sum(kvp => kvp.Value);
-                    best = Math.Min(ans, best);
-                    Console.WriteLine($"#{index} in {best} presses: {String.Join(", ", pressed.Select(kvp => $"key:{kvp.Key}={kvp.Value}").ToArray())}");
-                }
-                queue.Push((row - 1, pressed));
-            }
+            //we've reached the end with no failures, this must be a solution
+            best = Math.Min(pressed.Sum(kvp => kvp.Value), best);
+            Console.WriteLine($"#{index} in {best} total: {String.Join(", ", pressed.Select(kvp => $"b{kvp.Key}={kvp.Value}").ToArray())}");
             continue;
         }
 
-        //we have at least one value left over
-        //so queue up all the options for it
-        var param = arr[row].Index().Where(tp => tp.Item != 0 && !pressed.ContainsKey(tp.Index)).First();
+        //we have a row of the form x0 + x1 + x2 ... = br
+        //we need to sub in any values we know and then start brute forcing the rest
+        int rowTotal = A[row][C];
+        for (int c = 0; c < C; c++)
+            if (A[row][c] != 0)
+                if (pressed.ContainsKey(c))
+                    rowTotal-= A[row][c] * pressed[c];                    
+                else
+                    goto bruteForce;
+
+        //1) rowTotal = 0, the row is consistent and all values are specified, move to next row
+        //2) rowTotal != 0, this was a failed substitution so quit this path
+        if (rowTotal == 0)
+            stack.Push((row - 1, pressed));
+        continue;
+    bruteForce:;
+        //we need to choose our next value
+        //so push all the options to test
+        var param = A[row].Index().Where(tp => tp.Index != C && tp.Item != 0 && !pressed.ContainsKey(tp.Index)).First();
+        //upper bound of our choice is confusing because of -'ve numbers in the matrix
+        //so just use an easily calculated pre-computed max
+        var max = maximums[param.Index];
+        for (int p = max; p >= 0; p--)
         {
-            //max that this parameter can be
-            //var max = Math.Ceiling(checkSum / arr[row][param.Index]);
-            var max = Math.Abs(checkSum);
-            for (int p = (int) max; p >= 0; p--)
-            {
-                var newPressed = pressed.ToDictionary();
-                newPressed[param.Index] = p;                
-                queue.Push((row, newPressed));
-            }
+            var newPressed = pressed.ToDictionary();
+            newPressed[param.Index] = p;
+            stack.Push((row, newPressed));
         }
     }
-
+    Console.WriteLine($"#{index}: {best}");
     return best;
 }
 
-void print(double[][] arr)
+void swap<T>(T[] arr, int i0, int i1)
+{
+    var tmp = arr[i0];
+    arr[i0] = arr[i1];
+    arr[i1] = tmp;
+}
+void print(int[][] arr)
 {
     foreach (var a in arr)
     {
-        Console.WriteLine(String.Join("", a.Select(d => $"{(Math.Sign(d) >= 0 ? "+" : "")}{d}     ").ToArray()));
+        Console.WriteLine(String.Join("", a[0..^1].Select(d => $"{(Math.Sign(d) >= 0 ? "+" : "")}{d}     ").ToArray()) + $" = {a.Last()}");
     }
     Console.WriteLine();
 }
 
-long solve(int index)
+long solveP1(int index)
 {
     Dictionary<string, long> cache = new();
     Queue<(string state, long presses)> queue = new();
@@ -224,7 +158,6 @@ long solve(int index)
         {
             return presses;
         }
-        long best = long.MaxValue;
         foreach (var button in buttons[index])
         {
             var newLights = press(state, button);
@@ -234,18 +167,14 @@ long solve(int index)
     throw new();
 }
 
-
-
 string empty(int length) => new string(Enumerable.Repeat('.', length).ToArray());
 string press(string before, int[] button)
 {
-    var res = new StringBuilder(before);
+    var res = new System.Text.StringBuilder(before);
     foreach(var toggle in button)
     {
         res[toggle] = res[toggle] == '#' ? '.' : '#';
     }
     return res.ToString();
 }
-
-
 record State(string lights, long presses);
