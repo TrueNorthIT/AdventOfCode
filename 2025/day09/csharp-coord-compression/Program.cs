53bec53daa81b1fe80cf51730261d0d175b09a00
Christian Waters <cwaters@truenorthit.co.uk>
2025-12-19 16:52:15 +0000

    day 9 Summed Area Table, fast (~35ms)

--- 2025/day09/csharp-coord-compression/Program.cs
+++ 2025/day09/csharp-coord-compression/Program.cs
@@ -12,7 +12,7 @@ var xMap = redTiles.Select(p => p.x).Distinct().OrderBy(x => x).Index().ToArray(
 var invXMap = xMap.ToDictionary(tp => tp.Item, tp => tp.Index);
 var yMap = redTiles.Select(p => p.y).Distinct().OrderBy(y => y).Index().ToArray();
 var invYMap = yMap.ToDictionary(tp => tp.Item, tp => tp.Index);
-var compressed = redTiles.Select(p => new Point(invXMap[p.x], invYMap[p.y])).ToArray();
+var comp = redTiles.Select(p => new Point(invXMap[p.x], invYMap[p.y])).ToArray();
 
 watch.Stop();
 Console.WriteLine($"Parsing complete by {watch.ElapsedMilliseconds}ms");
@@ -21,24 +21,24 @@ watch.Start();
 var grid = new int[xMap.Length, yMap.Length];
 
 //draw the polygon on our compressed grid
-for (int e = 0; e < compressed.Length; e++)
+for (int e = 0; e < comp.Length; e++)
 {
-    var next = compressed[(e + 1) % compressed.Length];
-    if (next.x == compressed[e].x)
+    var next = comp[(e + 1) % comp.Length];
+    if (next.x == comp[e].x)
     {
-        for (var y = compressed[e].y; y != next.y; y += Math.Sign(next.y - compressed[e].y))
-            grid[compressed[e].x, y] = 1; //green
+        for (var y = comp[e].y; y != next.y; y += Math.Sign(next.y - comp[e].y))
+            grid[comp[e].x, y] = 1; //green
     }
     else
     {
-        for (var x = compressed[e].x; x != next.x; x += Math.Sign(next.x - compressed[e].x))
-            grid[x, compressed[e].y] = 1; //green
+        for (var x = comp[e].x; x != next.x; x += Math.Sign(next.x - comp[e].x))
+            grid[x, comp[e].y] = 1; //green
     }
-    grid[compressed[e].x, compressed[e].y] = 2; //red
+    grid[comp[e].x, comp[e].y] = 2; //red
 }
 
 watch.Stop();
-print(grid);
+//print(grid);
 Console.WriteLine($"Grid compressed by {watch.ElapsedMilliseconds}ms:");
 watch.Start();
 
@@ -60,9 +60,9 @@ for (int y = 0; y < yMap.Length; y++)
     if (grid[xMap.Length - 1, y] == 0)
         queue.Enqueue(new Point(xMap.Length - 1, y));
 }
-while (queue.Any())
+
+while (queue.TryDequeue(out Point curr))
 {
-    var curr = queue.Dequeue();
     grid[curr.x, curr.y] = -1;
     foreach (var neighbour in neighbours(curr))
         if (grid[neighbour.x, neighbour.y] == 0)
@@ -73,42 +73,48 @@ while (queue.Any())
 }
 
 watch.Stop();
-print(grid);
+//print(grid);
 Console.WriteLine($"Grid flood filled by {watch.ElapsedMilliseconds}ms:");
 watch.Start();
 
-var p2 = (
- from c1 in compressed
- from c2 in compressed
- where c1.x < c2.x || (c1.x == c2.x && c1.y < c2.y)
- let c1E = new Point(xMap[c1.x].Item, yMap[c1.y].Item)
- let c2E = new Point(xMap[c2.x].Item, yMap[c2.y].Item)
- select (c1, c2, c1E, c2E, expandedArea: rectArea(c1E, c2E)))
-.OrderByDescending(tp => tp.expandedArea)
-.First(tp =>
-{
-    //4 corner check first speeds things up a little
-    if (grid[tp.c1.x, tp.c1.y] == -1
-        || grid[tp.c1.x, tp.c2.y] == -1
-        || grid[tp.c2.x, tp.c1.y] == -1
-        || grid[tp.c2.x, tp.c2.y] == -1
-    ) return false;
+//Using a Summed Area Table (https://en.wikipedia.org/wiki/Summed-area_table)
+// Fill the grid in one pass using this recursion
+// S(x,y) = i(x,y) + I(x,y-1) + I(x-1,y) - I(x-1,y-1)
+// And calculate total points in a given bounding Box with:
+// I(D) + I(A) - I(B) - I(C)
+// = i(x1,y1) + i(x0 - 1,y0 - 1) - i(x0 - 1,y1) - i(x1, y0 - 1)
 
-    //loop over all rectangle points to find if there are any outside points
-    (var minY, var maxY) = tp.c1.y < tp.c2.y ? (tp.c1.y, tp.c2.y) : (tp.c2.y, tp.c1.y);
-    for (var x = tp.c1.x; x <= tp.c2.x; x++)
-        for (var y = minY; y <= maxY; y++)
-            if (grid[x, y] == -1)
-                return false;
-    return true;
-});
+//start grid at 1 to provide a convenient 0 row / column that is all 0's
+int[,] sums = new int[xMap.Length + 1, yMap.Length + 1];
+for (int y = 1; y < yMap.Length + 1; y++)
+    for (int x = 1; x < xMap.Length + 1; x++)
+    {
+        sums[x, y] = (grid[x - 1, y - 1] == -1) ? 1 : 0;
+        sums[x, y] += sums[x, y - 1];
+        sums[x, y] += sums[x - 1, y];
+        sums[x, y] -= sums[x - 1, y - 1];
+    }
 
 watch.Stop();
-Console.WriteLine($"Best found in {watch.ElapsedMilliseconds}ms");
-Console.WriteLine(((p2.c1, p2.c2),(p2.c1E, p2.c2E), p2.expandedArea));
+Console.WriteLine($"Prefix sums calculated by {watch.ElapsedMilliseconds}ms");
+watch.Start();
+
+long best = 0;
+for (int c1 = 0; c1 < comp.Length; c1++)
+    for (int c2 = c1+1; c2 < comp.Length; c2++)
+    {
+        (var minY, var maxY) = comp[c1].y < comp[c2].y ? (comp[c1].y, comp[c2].y) : (comp[c2].y, comp[c1].y);
+        if ((sums[comp[c2].x + 1, maxY + 1] + sums[comp[c1].x, minY ] 
+            - sums[comp[c2].x + 1, minY] - sums[comp[c1].x, maxY + 1]) == 0)
+        {
+            best = Math.Max(best, rectArea(new Point(xMap[comp[c1].x].Item, yMap[comp[c1].y].Item), new Point(xMap[comp[c2].x].Item, yMap[comp[c2].y].Item)));
+        }
+    }
+
+watch.Stop();
+Console.WriteLine($"Best of {best} found in {watch.ElapsedMilliseconds}ms");
 
 long rectArea(Point p1, Point p2) => (1 + Math.Abs(p2.x - p1.x)) * (1 + Math.Abs(p2.y - p1.y));
-
 IEnumerable<Point> neighbours(Point point)
 {
     for (int x = -1; x <= 1; x++)
